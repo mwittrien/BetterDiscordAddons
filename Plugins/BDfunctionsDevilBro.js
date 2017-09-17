6392194c486c5932d50935a780688ebbdebf0fd8
Mirco Wittrien <mirco.wittrien@gmail.com>
2017-09-17 12:51:21 +0200

    Update BDfunctionsDevilBro.js

--- Plugins/BDfunctionsDevilBro.js
+++ Plugins/BDfunctionsDevilBro.js
@@ -1,13 +1,5 @@
-var BDfunctionsDevilBro = null;
-if (typeof BDfunctionsDevilBroClass === "undefined") {
-	class BDfunctionsDevilBroClass {};
-	BDfunctionsDevilBroClass.test = function () {
-		console.log("HI");
-	};
-	
-	BDfunctionsDevilBro = new BDfunctionsDevilBroClass();
-	console.log(BDfunctionsDevilBro);
-}
-else {
-	alert("Fatal Error: Could not create FunctionClass.");
-}
+var BDfunctionsDevilBro = {};
+BDfunctionsDevilBro.test = function () {
+	console.log("HI");
+};
+console.log(BDfunctionsDevilBro);
