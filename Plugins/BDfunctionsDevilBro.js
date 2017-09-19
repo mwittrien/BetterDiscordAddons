a66b8a9e0a8e7b2a2399d1b6084fa6317bb2c7a3
Mirco Wittrien <mirco.wittrien@gmail.com>
2017-09-19 13:57:17 +0200

    Update BDfunctionsDevilBro.js

--- Plugins/BDfunctionsDevilBro.js
+++ Plugins/BDfunctionsDevilBro.js
@@ -17,6 +17,7 @@ BDfunctionsDevilBro.getReactInstance = function (node) {
 };
 
 BDfunctionsDevilBro.getKeyInformation = function (config) {
+	console.log(config);
 	if (config === undefined) return null;
 	if (config.node === undefined || config.key === undefined) return null;
 	
