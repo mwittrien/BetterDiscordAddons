941ac0388ba6c2b7ea450296c441889165225b5b
Mirco Wittrien <mirco.wittrien@gmail.com>
2017-10-13 19:39:18 +0200

    Update BDfunctionsDevilBro.js

--- Plugins/BDfunctionsDevilBro.js
+++ Plugins/BDfunctionsDevilBro.js
@@ -498,11 +498,11 @@ BDfunctionsDevilBro.color2HSL = function (color) {
 	if (color) {
 		switch (BDfunctionsDevilBro.checkColorType(color)) {
 			case "comp":
-				var r = color[0], g = color[1], b = color[2];
+				var r = parseInt(color[0]), g = parseInt(color[1]), b = parseInt(color[2]);
 				var max = Math.max(r, g, b), min = Math.min(r, g, b), d = max - min, h, s = (max === 0 ? 0 : d / max), l = max / 255;
 				switch (max) {
 					case min: h = 0; break;
-					case r: h = (g - b) + d * (g < b ? 6: 0); h /= 6 * d; break;
+					case r: h = (g - b) + d * (g < b ? 6 : 0); h /= 6 * d; break;
 					case g: h = (b - r) + d * 2; h /= 6 * d; break;
 					case b: h = (r - g) + d * 4; h /= 6 * d; break;
 				}
