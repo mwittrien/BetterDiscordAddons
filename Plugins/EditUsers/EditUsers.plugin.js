88ea608f5ce03b8f4eaa6460e0d92a3cfff32f18
Mirco Wittrien <mirco.wittrien@gmail.com>
2017-10-19 13:36:21 +0200

    Update EditUsers.plugin.js

--- Plugins/EditUsers/EditUsers.plugin.js
+++ Plugins/EditUsers/EditUsers.plugin.js
@@ -349,7 +349,7 @@ class EditUsers {
 
 	getDescription () {return "Allows you to change the icon, name, tag and color of users. Does not work in compact mode.";}
 
-	getVersion () {return "1.4.1";}
+	getVersion () {return "1.4.2";}
 
 	getAuthor () {return "DevilBro";}
 	
@@ -696,7 +696,7 @@ class EditUsers {
 			BDfunctionsDevilBro.setColorSwatches(color2, userSettingsModal.find(".swatches2"), "swatch2");
 			BDfunctionsDevilBro.setColorSwatches(color3, userSettingsModal.find(".swatches3"), "swatch3");
 			BDfunctionsDevilBro.setColorSwatches(color4, userSettingsModal.find(".swatches4"), "swatch4");
-			userSettingsModal.appendTo($(".platform-win > div").last())
+			userSettingsModal.appendTo($("body > div > [class*='platform-'] > div").last())
 				.on("click", ".callout-backdrop,button.btn-cancel", (event) => {
 					userSettingsModal.remove();
 				})
