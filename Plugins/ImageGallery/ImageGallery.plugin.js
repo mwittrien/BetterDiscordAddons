72a9d9c292dbc4dabf211403a05d29f27ec8d99a
Mirco Wittrien <mirco.wittrien@gmail.com>
2017-10-24 22:40:59 +0200

    Update ImageGallery.plugin.js

--- Plugins/ImageGallery/ImageGallery.plugin.js
+++ Plugins/ImageGallery/ImageGallery.plugin.js
@@ -20,6 +20,32 @@ class ImageGallery {
 			
 			.modal-image .image.next {
 				left: 90%;
+			}
+			
+			.modal-image .previewbar {
+				position: absolute;
+				top: 82%;
+				left: 10%;
+				height: 15%;
+				width: 80%;
+				background: red;
+				overflow-x: scroll;
+				overflow-y: hidden;
+				white-space: nowrap;
+			}
+			
+			.modal-image .previewbar [class^="preview-"] {
+				position: relative;
+				top: 0px;
+				bottom: 0px;
+				height: 100%;
+				display: inline-block;
+			}
+			
+			.modal-image .previewbar [class^="previewimage-"] {
+				position: relative;
+				top: 1px;
+				bottom: 0px;
 			}`;
 	}
 
@@ -27,7 +53,7 @@ class ImageGallery {
 
 	getDescription () {return "Allows the user to browse through images sent inside the same message.";}
 
-	getVersion () {return "1.3.1";}
+	getVersion () {return "1.3.2";}
 
 	getAuthor () {return "DevilBro";}
 
@@ -65,7 +91,7 @@ class ImageGallery {
 					}
 				);
 			});
-			if (document.querySelector("body > div > [class*='platform-'] > div")) this.imageModalObserver.observe($("body > div > [class*='platform-'] > div").last()[0], {childList: true});
+			if (document.querySelector("#app-mount")) this.imageModalObserver.observe(document.querySelector("#app-mount"), {childList: true, subtree:true});
 			
 			BDfunctionsDevilBro.appendLocalStyle(this.getName(), this.css);
 		}
@@ -97,7 +123,12 @@ class ImageGallery {
 				var message = this.getMessageGroupOfImage(img);
 				var imgs = $(message).find(".image");
 				
-				this.addImagePreviews(modal, imgs, img);
+				this.addImages(modal, imgs, img);
+				
+				/* $(modal).find(".modal-image").append($("<div/>", { 'class': 'previewbar' }))
+				imgs.each((index, preview) => {
+					this.addImagePreview(modal, preview, index);
+				}); */
 			}
 			else if (performance.now() - start > 10000) {
 				clearInterval(waitForImg);
@@ -120,16 +151,17 @@ class ImageGallery {
 		return null;
 	}
 	
-	addImagePreviews (modal, imgs, img) {
+	addImages (modal, imgs, img) {
 		$(modal).find(".image.prev").remove();
 		$(modal).find(".image.next").remove();
 		var prevImg;
 		var nextImg;
-		for (var i = 0; i < imgs.length; i++) {
-			if (this.getSrcOfImage(img) == this.getSrcOfImage(imgs[i])) {
-				prevImg = 	imgs[i-1];
-				img = 		imgs[i];
-				nextImg = 	imgs[i+1];
+		var index;
+		for (index = 0; index < imgs.length; index++) {
+			if (this.getSrcOfImage(img) == this.getSrcOfImage(imgs[index])) {
+				prevImg = 	imgs[index-1];
+				img = 		imgs[index];
+				nextImg = 	imgs[index+1];
 				break;
 			}
 		}
@@ -138,7 +170,7 @@ class ImageGallery {
 			.attr("placeholder", this.getSrcOfImage(img))
 			.attr("src", this.getSrcOfImage(img));
 			
-		$(modal).find("a")
+		$(modal).find(".download-button")
 			.attr("href", this.getSrcOfImage(img));
 		
 		this.resizeImage(modal, img, modal.querySelector(".image"));
@@ -151,10 +183,17 @@ class ImageGallery {
 			$(modal).find(".modal-image").append($("<video/>", { 'class': 'image next', 'poster': this.getSrcOfImage(nextImg)}));
 			this.resizeImage(modal, nextImg, modal.querySelector(".image.next"));
 		}
-		$(modal).find(".image.prev").off("click").on("click", this.addImagePreviews.bind(this, modal, imgs, prevImg));
-		$(modal).find(".image.next").off("click").on("click", this.addImagePreviews.bind(this, modal, imgs, nextImg));
+		
+		$(modal).find(".image.prev").off("click").on("click", this.addImages.bind(this, modal, imgs, prevImg));
+		$(modal).find(".image.next").off("click").on("click", this.addImages.bind(this, modal, imgs, nextImg));
 		$(document).off("keydown." + this.getName()).on("keydown." + this.getName(), {modal, imgs, prevImg, nextImg}, this.keyPressed.bind(this));
 		$(document).off("keyup." + this.getName()).on("keyup." + this.getName(), () => {this.eventFired = false});
+	}
+	
+	addImagePreview (modal, img, index) {
+		$(modal).find(".previewbar").append($("<div/>", { 'class': 'preview-' + index }));
+		$(modal).find(".preview-" + index).append($("<video/>", { 'class': 'previewimage-' + index, 'poster': this.getSrcOfImage(img)}));
+		this.resizePreview(modal.querySelector(".previewbar"), img, modal.querySelector(".previewimage-" + index));
 	}
 	
 	getSrcOfImage (img) {
@@ -162,13 +201,31 @@ class ImageGallery {
 		return src.split("?width=")[0];
 	}
 	
-	resizeImage (modal, src, img) {
+	resizeImage (container, src, img) {
 		$(img).hide();
 		var temp = new Image();
 		temp.src = src.src.split("?width=")[0];
 		temp.onload = function () {
-			var resizeX = (modal.clientWidth/src.clientWidth) * 0.71;
-			var resizeY = (modal.clientHeight/src.clientHeight) * 0.57;
+			var resizeX = (container.clientWidth/src.clientWidth) * 0.71;
+			var resizeY = (container.clientHeight/src.clientHeight) * 0.57;
+			var resize = resizeX < resizeY ? resizeX : resizeY;
+			var newWidth = src.clientWidth * resize;
+			var newHeight = src.clientHeight * resize;
+			
+			$(img)
+				.attr("width", temp.width > newWidth ? newWidth : temp.width)
+				.attr("height", temp.height > newHeight ? newHeight : temp.height)
+				.show();
+		};
+	}
+	
+	resizePreview (container, src, img) {
+		$(img).hide();
+		var temp = new Image();
+		temp.src = src.src.split("?width=")[0];
+		temp.onload = function () {
+			var resizeX = (container.clientWidth/src.clientWidth) * 0.1;
+			var resizeY = (container.clientHeight/src.clientHeight) * 0.9;
 			var resize = resizeX < resizeY ? resizeX : resizeY;
 			var newWidth = src.clientWidth * resize;
 			var newHeight = src.clientHeight * resize;
@@ -184,10 +241,10 @@ class ImageGallery {
 		if (!this.eventFired) {
 			this.eventFired = true;
 			if (e.keyCode == 37 && e.data.prevImg) {
-				this.addImagePreviews(e.data.modal, e.data.imgs, e.data.prevImg)
+				this.addImages(e.data.modal, e.data.imgs, e.data.prevImg)
 			}
 			else if (e.keyCode == 39 && e.data.nextImg) {
-				this.addImagePreviews(e.data.modal, e.data.imgs, e.data.nextImg)
+				this.addImages(e.data.modal, e.data.imgs, e.data.nextImg)
 			}
 		}
 	}
