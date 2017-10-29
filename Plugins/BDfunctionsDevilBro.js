878119c45dd0eef80111712c338dcf1a25cc8284
Mirco Wittrien <mirco.wittrien@gmail.com>
2017-10-29 08:17:11 +0100

    Update BDfunctionsDevilBro.js

--- Plugins/BDfunctionsDevilBro.js
+++ Plugins/BDfunctionsDevilBro.js
@@ -283,7 +283,7 @@ BDfunctionsDevilBro.getOwnerInstance = function (config) {
 	var upwards = config.up === undefined ? false : config.up;
 	
 	var start = performance.now();
-	var maxTime = config.time === undefined ? 200000 : config.time;
+	var maxTime = config.time === undefined ? 15 : config.time;
 		
 	var keyWhiteList = upwards ? {"return":true,"sibling":true} : {"child":true,"sibling":true};
 	
