b86672846179424cb58c7ade080b462d3accbc45
Mirco Wittrien <mirco.wittrien@gmail.com>
2017-11-10 13:25:33 +0100

    Update ServerFolders.plugin.js

--- Plugins/ServerFolders/ServerFolders.plugin.js
+++ Plugins/ServerFolders/ServerFolders.plugin.js
@@ -246,7 +246,7 @@ class ServerFolders {
 
 	getDescription () {return "Adds the feature to create folders to organize your servers. Right click a server > 'Serverfolders' > 'Create Server' to create a server. To add servers to a folder hold 'Ctrl' and drag the server onto the folder, this will add the server to the folderlist and hide it in the serverlist. To open a folder click the folder. A folder can only be opened when it has at least one server in it. To remove a server from a folder, open the folder and either right click the server > 'Serverfolders' > 'Remove Server from Folder' or hold 'Del' and click the server in the folderlist.";}
 
-	getVersion () {return "5.1.2";}
+	getVersion () {return "5.1.3";}
 
 	getAuthor () {return "DevilBro";}
 	
@@ -875,7 +875,7 @@ class ServerFolders {
 						
 					if (!alreadyOpen) {
 						$(".guilds-wrapper").addClass("folderopen");
-						$(`<div class="foldercontainer"></div>`).insertAfter(".guild.folder:last");
+						$(`<div class="foldercontainer"></div>`).insertAfter(".guilds > .guild:last");
 					}
 					
 					for (var i = 0; i < includedServers.length; i++) {
