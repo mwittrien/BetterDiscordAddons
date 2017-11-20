5bb0b4acfc15ca092a4efa19634ec6d3622995de
Mirco Wittrien <mirco.wittrien@gmail.com>
2017-11-20 12:20:53 +0100

    Add files via upload

---
+++ Plugins/PersonalPins/PersonalPins.plugin.js
@@ -0,0 +1,296 @@
+//META{"name":"PersonalPins"}*//
+
+class PersonalPins {
+	constructor () {
+		this.switchFixObserver = new MutationObserver(() => {});
+		this.chatWindowObserver = new MutationObserver(() => {});
+		this.optionPopoutObserver = new MutationObserver(() => {});
+		
+		this.notesButton =
+			`<svg class="iconInactive-WWHQEI icon-mr9wAc iconMargin-2Js7V9 notesButton" name="Note" width="16" height="16" viewBox="0 0 26 26">
+				<g fill="none" fill-rule="evenodd" transform="translate(3,2)">
+					<path class="iconForeground-2c7s3m" fill="currentColor" d="M 4.618, 0 c -0.316, 0 -0.573, 0.256 -0.573, 0.573 v 1.145 c 0, 0.316, 0.256, 0.573, 0.573, 0.573 s 0.573 -0.256, 0.573 -0.573 V 0.573 C 5.191, 0.256, 4.935, 0, 4.618, 0 z"/>
+					<path class="iconForeground-2c7s3m" fill="currentColor" d="M 8.053, 0 c -0.316, 0 -0.573, 0.256 -0.573, 0.573 v 1.145 c 0, 0.316, 0.256, 0.573, 0.573, 0.573 s 0.573 -0.256, 0.573 -0.573 V 0.573 C 8.626, 0.256, 8.37, 0, 8.053, 0 z"/>
+					<path class="iconForeground-2c7s3m" fill="currentColor" d="M 11.489, 0 c -0.316, 0 -0.573, 0.256 -0.573, 0.573 v 1.145 c 0, 0.316, 0.256, 0.573, 0.573, 0.573 c 0.316, 0, 0.573 -0.256, 0.573 -0.573 V 0.573 C 12.061, 0.256, 11.805, 0, 11.489, 0 z "/>
+					<path class="iconForeground-2c7s3m" fill="currentColor" d="M 14.924, 0 c -0.316, 0 -0.573, 0.256 -0.573, 0.573 v 1.145 c 0, 0.316, 0.256, 0.573, 0.573, 0.573 c 0.316, 0, 0.573 -0.256, 0.573 -0.573 V 0.573 C 15.496, 0.256, 15.24, 0, 14.924, 0 z"/>
+					<path class="iconForeground-2c7s3m" fill="currentColor" d="M 16.641, 1.25 V 1.718 c 0, 0.947 -0.77, 1.718 -1.718, 1.718 c -0.947, 0 -1.718 -0.77 -1.718 -1.718 c 0, 0.947 -0.77, 1.718 -1.718, 1.718 c -0.947, 0 -1.718 -0.77 -1.718 -1.718 c 0, 0.947 -0.77, 1.718 -1.718, 1.718 c -0.947, 0 -1.718 -0.77 -1.718 -1.718 c 0, 0.947 -0.77, 1.718 -1.718, 1.718 c -0.947, 0 -1.718 -0.77 -1.718 -1.718 V 1.25 C 2.236, 1.488, 1.756, 2.117, 1.756, 2.863 v 14.962 c 0, 0.947, 0.77, 1.718, 1.718, 1.718 h 12.595 c 0.947, 0, 1.718 -0.77, 1.718 -1.718 V 2.863 C 17.786, 2.117, 17.306, 1.488, 16.641, 1.25 z M 14.924, 16.679 H 4.618 c -0.316, 0 -0.573 -0.256 -0.573 -0.573 c 0 -0.316, 0.256 -0.573, 0.573 -0.573 h 10.305 c 0.316, 0, 0.573, 0.256, 0.573, 0.573 C 15.496, 16.423, 15.24, 16.679, 14.924, 16.679 z M 14.924, 13.244 H 4.618 c -0.316, 0 -0.573 -0.256 -0.573 -0.573 c 0 -0.316, 0.256 -0.573, 0.573 -0.573 h 10.305 c 0.316, 0, 0.573, 0.256, 0.573, 0.573 C 15.496, 12.988, 15.24, 13.244, 14.924, 13.244 z M 14.924, 9.733 H 4.618 c -0.316, 0 -0.573 -0.256 -0.573 -0.573 s 0.256 -0.573, 0.573 -0.573 h 10.305 c 0.316, 0, 0.573, 0.256, 0.573, 0.573 S 15.24, 9.733, 14.924, 9.733 z M 14.924, 6.298 H 4.618 c -0.316, 0 -0.573 -0.256 -0.573 -0.573 s 0.256 -0.573, 0.573 -0.573 h 10.305 c 0.316, 0, 0.573, 0.256, 0.573, 0.573 S 15.24, 6.298, 14.924, 6.298 z"/>
+				</g>
+			</svg>`;
+			
+		this.notesPopoutMarkup = 
+			`<div class="popout popout-bottom-right no-arrow no-shadow" style="z-index: 1000; visibility: visible; left: 544.844px; top: 35.9896px; transform: translateX(-100%) translateY(0%) translateZ(0px);">
+				<div class="messages-popout-wrap themed-popout" style="max-height: 740px;">
+					<div class="header">
+						<div class="title">Notes</div>
+					</div>
+					<div class="scroller-wrap dark">
+						<div class="messages-popout scroller">
+							<div class="empty-placeholder">
+								<div class="image" style="background-image: url(&quot;/assets/6793e022dc1b065b21f12d6df02f91bd.svg&quot;);"></div>
+								<div class="body"></div>
+							</div>
+						</div>
+					</div>
+					<div class="footer">
+						<div class="protip popout-footer"></div>
+					</div>
+				</div>
+			</div>`;
+		
+		this.optionButtonMarkup = 
+			`<div class="btn-option btn-personalpins"></div>`;
+		
+		this.optionsPopoutMarkup = 
+			`<div class="popout popout-bottom no-arrow popout-personalpins" style="z-index: 1000; visibility: visible;">
+				<div class="option-popout small-popout-box"></div
+			</div>`;
+			
+		this.popoutEntryMarkup = 
+			`<div class="btn-item btn-item-personalpins">Notiz</div>`;
+			
+		this.messageMarkup = 
+			`<div class="message-group hide-overflow">
+				<div class="avatar-large animate"></div>
+				<div class="comment">
+					<div class="message first">
+						<div class="body">
+							<h2 class="old-h2">
+								<span class="username-wrapper"><strong class="user-name"></strong></span>
+								<span class="highlight-separator"> - </span><span class="timestamp"></span>
+							</h2><div class="message-text">
+							<div class="markup"></div>
+						</div>
+					</div>
+					<div class="accessory"></div></div>
+				</div>
+				<div class="sink-interactions clickable"></div>
+				<div class="action-buttons">
+					<div class="close-button"></div>
+				</div>
+			</div>`;
+	}
+
+	getName () {return "PersonalPins";}
+
+	getDescription () {return "Similar to normal pins. Lets you save messages as pins for yourself.";}
+
+	getVersion () {return "1.0.0";}
+
+	getAuthor () {return "DevilBro";}
+	
+    getSettingsPanel () {
+		if (typeof BDfunctionsDevilBro === "object") {
+		}
+	}
+
+	//legacy
+	load () {}
+
+	start () {
+		if (typeof BDfunctionsDevilBro === "object") BDfunctionsDevilBro = "";
+		$('head script[src="https://mwittrien.github.io/BetterDiscordAddons/Plugins/BDfunctionsDevilBro.js"]').remove();
+		$('head').append("<script src='https://mwittrien.github.io/BetterDiscordAddons/Plugins/BDfunctionsDevilBro.js'></script>");
+		if (typeof BDfunctionsDevilBro !== "object") {
+			$('head script[src="https://cors-anywhere.herokuapp.com/https://mwittrien.github.io/BetterDiscordAddons/Plugins/BDfunctionsDevilBro.js"]').remove();
+			$('head').append("<script src='https://cors-anywhere.herokuapp.com/https://mwittrien.github.io/BetterDiscordAddons/Plugins/BDfunctionsDevilBro.js'></script>");
+		}
+		if (typeof BDfunctionsDevilBro === "object") {
+			BDfunctionsDevilBro.loadMessage(this.getName(), this.getVersion());
+			
+			this.chatWindowObserver = new MutationObserver((changes, _) => {
+				changes.forEach(
+					(change, i) => {
+						if (change.addedNodes) {
+							change.addedNodes.forEach((node) => {
+								if (node && node.tagName && node.classList && node.classList.contains("message-group")) {
+									node.querySelectorAll(".message").forEach(message => {this.addOptionButton(message);});
+								}
+								else if (node && node.tagName && node.classList && node.classList.contains("message")) {
+									this.addOptionButton(node);
+								}
+							});
+						}
+					}
+				);
+			});
+			if (document.querySelector(".messages.scroller")) this.chatWindowObserver.observe(document.querySelector(".messages.scroller"), {childList:true, subtree:true});
+			
+			this.optionPopoutObserver = new MutationObserver((changes, _) => {
+				changes.forEach(
+					(change, i) => {
+						if (change.addedNodes) {
+							change.addedNodes.forEach((node) => {
+								if (node && node.tagName && node.querySelector(".option-popout") && !node.querySelector(".btn-item-personalpins")) {
+									$(node).find(".option-popout").append(this.popoutEntryMarkup);
+									this.addClickListener(node);
+								}
+							});
+						}
+					}
+				);
+			});
+			if (document.querySelector(".popouts")) this.optionPopoutObserver.observe(document.querySelector(".popouts"), {childList: true});
+			
+			this.switchFixObserver = BDfunctionsDevilBro.onSwitchFix(this);
+			
+			$(document).off("click." + this.getName(), ".btn-option")
+				.on("click." + this.getName(), ".btn-option", (e) => {
+					var div = $(".message").has(e.currentTarget)[0];
+					this.message = {
+						"div": div,
+						"pos": $(".message-group").has(e.currentTarget).find(".message").index(div)
+					}
+					console.log(this.message);
+				});
+			
+			document.querySelectorAll(".messages-group .message").forEach(message => {this.addOptionButton(message);});
+			
+			this.addNotesButton();
+		}
+		else {
+			console.error(this.getName() + ": Fatal Error: Could not load BD functions!");
+		}
+	}
+
+
+	stop () {
+		if (typeof BDfunctionsDevilBro === "object") {
+			this.switchFixObserver.disconnect();
+			this.chatWindowObserver.disconnect();
+			this.optionPopoutObserver.disconnect();
+			
+			$(document).off("click." + this.getName(), ".btn-option");
+			
+			$(".btn-personalpins, .notesButton").remove();
+			
+			BDfunctionsDevilBro.unloadMessage(this.getName(), this.getVersion());
+		}
+	}
+	
+	onSwitch () {
+		if (typeof BDfunctionsDevilBro === "object") {
+			if (document.querySelector(".messages.scroller")) this.chatWindowObserver.observe(document.querySelector(".messages.scroller"), {childList:true, subtree:true});
+			document.querySelectorAll(".messages .message").forEach(message => {this.addOptionButton(message);});
+			setTimeout(() => {
+				this.addNotesButton();
+			},1);
+		}
+	}
+
+	
+	// begin of own functions
+	
+	addNotesButton () {
+		$(".notesButton").remove();
+		$(this.notesButton)
+			.insertBefore("svg[name='Pin']")
+			.on("click." + this.getName(), (e) => {
+				this.openNotesPopout(e);
+			})
+			.on("mouseenter." + this.getName(), (e) => {
+				BDfunctionsDevilBro.createTooltip("Notes", e.currentTarget, {type:"bottom",selector:"reload-button-tooltip"});
+			});
+	}
+	
+	addOptionButton (message) {
+		if (!message.querySelector(".btn-option")) {
+			$(message).find(".markup").before(this.optionButtonMarkup);
+			$(message).off("click." + this.getName()).on("click." + this.getName(), ".btn-personalpins", (e) => {
+				this.openOptionPopout(e);
+			});
+		}
+	}
+	
+	openNotesPopout (e) {
+		var popout = $(this.notesPopoutMarkup);
+		$(".popouts").append(popout);
+		popout
+			.css("left", $(e.currentTarget).outerWidth()/2 + $(e.currentTarget).offset().left + "px")
+			.css("top", $(e.currentTarget).outerHeight() + $(e.currentTarget).offset().top + "px");
+			
+		$(document).on("mousedown." + this.getName(), (e2) => {
+			if (popout.has(e2.target).length == 0) {
+				$(document).off("mousedown." + this.getName());
+				popout.remove();
+			}
+		});
+		
+		this.addNotes(popout[0]);
+	}
+	
+	openOptionPopout (e) {
+		var popout = $(this.optionsPopoutMarkup);
+		$(".popouts").append(popout);
+		$(popout).find(".option-popout").append(this.popoutEntryMarkup);
+		this.addClickListener(popout);
+		
+		popout
+			.css("left", e.pageX - ($(popout).outerWidth() / 2) + "px")
+			.css("top", e.pageY + "px");
+			
+		$(document).on("mousedown." + this.getName(), (e2) => {
+			if (popout.has(e2.target).length == 0) {
+				$(document).off("mousedown." + this.getName());
+				popout.remove();
+			}
+		});
+	}
+	
+	addClickListener (popout) {
+		$(popout)
+			.off("click." + this.getName(), ".btn-item-personalpins")
+			.on("click." + this.getName(), ".btn-item-personalpins", (e) => {
+				$(".popout").has(".option-popout").hide();
+				if (!this.message) return;
+				var info = BDfunctionsDevilBro.getKeyInformation({"node":this.message.div.parentElement,"key":"message"});
+				if (info) {
+					var channelID = info.channel_id;
+					var data = BDfunctionsDevilBro.loadAllData(this.getName(), channelID);
+					var timestamp = info.timestamp._i.getTime();
+					var messageID = info.id;
+					var position = this.message.pos;
+					var message = {
+						"channel": channelID,
+						"id": messageID,
+						"pos": position,
+						"timestamp": timestamp,
+						"color": info.colorString,
+						"author": info.author.username,
+						"avatar": "url('https://cdn.discordapp.com/avatars/" + info.author.id + "/" + info.author.avatar + ".webp')",
+						"markup": this.message.div.querySelector(".markup").innerHTML,
+						"accessory": this.message.div.querySelector(".accessory").innerHTML
+					};
+					data[timestamp + "_" + messageID + "_" + position] = message;
+					BDfunctionsDevilBro.saveAllData(data, this.getName(), channelID);
+				}
+				this.message = null;
+			});
+	}
+	
+	addNotes (popout) {
+		var info = BDfunctionsDevilBro.getKeyInformation({"node":document.querySelector(".chat"),"key":"channel"});
+		if (info) {
+			var messages = BDfunctionsDevilBro.loadAllData(this.getName(), info.id);
+			var language = BDfunctionsDevilBro.getDiscordLanguage().id;
+			var container = popout.querySelector(".messages-popout");
+			var placeholder = popout.querySelector(".empty-placeholder")
+			if (!BDfunctionsDevilBro.isObjectEmpty(messages)) $(placeholder).hide();
+			for (let id in messages) {
+				let data = messages[id];
+				let message = $(this.messageMarkup)[0];
+				container.insertBefore(message, container.firstChild);
+				message.querySelector(".avatar-large").style.backgroundImage = data.avatar;
+				message.querySelector(".user-name").innerText = data.author;
+				message.querySelector(".user-name").style.color = data.color;
+				message.querySelector(".timestamp").innerText = new Date(data.timestamp).toLocaleDateString(language);
+				message.querySelector(".markup").innerHTML = data.markup;
+				message.querySelector(".accessory").innerHTML = data.accessory;
+				$(message).on("click." + this.getName(), ".close-button", (e) => {
+					message.remove();
+					BDfunctionsDevilBro.removeData(id, this.getName(), info.id);
+					if (!container.querySelector(".message-group")) $(placeholder).show();
+				});
+			}
+		}
+	}
+}
