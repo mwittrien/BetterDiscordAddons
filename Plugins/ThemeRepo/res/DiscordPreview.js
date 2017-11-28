e8a15be58a1808b270a1e03e531107a199eb47c0
Mirco Wittrien <mirco.wittrien@gmail.com>
2017-11-28 21:09:32 +0100

    Update DiscordPreview.js

--- Plugins/ThemeRepo/res/DiscordPreview.js
+++ Plugins/ThemeRepo/res/DiscordPreview.js
@@ -19,7 +19,6 @@ window.onmessage = function (e) {
 				theme = document.createElement("style");
 				theme.classList.add("previewTheme");
 				theme.innerText = e.data.css;
-					
 				head.appendChild(theme);
 				break;
 			case "DarkLight":
@@ -28,12 +27,21 @@ window.onmessage = function (e) {
 				else 
 					body.innerHTML = body.innerHTML.replace(new RegExp("theme-light", "g"), "theme-dark");
 				break;
+			case "CustomCSS":
+				if (e.data.custom)
+					theme = document.createElement("style");
+					theme.classList.add("customCSS");
+					theme.innerText = e.data.css;
+					head.appendChild(theme);
+				else {
+					document.querySelectorAll("style.customCSS").forEach(theme => theme.remove());
+				}
+				break;
 			case "ThemeFixer":		
 				if (e.data.fixer) {
 					theme = document.createElement("style");
 					theme.classList.add("themeFixer");
 					theme.innerText =`#voice-connection,#friends,.friends-header,.friends-table,.guilds-wrapper,.guild-headerheader,.channels-wrap,.private-channels.search-bar,.private-channels,.guild-channels,.account,.friend-table-add-header,.chat,.content,.layers,.title-wrap:not(.search-bar),.messages-wrapper,.messages.dividerspan,.messages.divider:before,.content,.message-group-blocked,.is-local-bot-message,.channel-members-loading,.channel-members-loading.heading,.channel-members-loading.member,.typing,.layer,.layers,.container-RYiLUQ,.theme-dark.ui-standard-sidebar-view,.theme-dark.ui-standard-sidebar-view.sidebar-region,.theme-dark.ui-standard-sidebar-view.content-region,.theme-dark.channel-members,.layer,.layers,.container-2OU7Cz,.theme-dark.title-qAcLxz,.theme-dark.chatform,.channels-3g2vYe,.theme-dark.friends-table,.theme-dark.messages-wrapper,.content.flex-spacer,.theme-dark.chat>.content,.theme-dark.chat,.container-2OU7Cz,.theme-dark.channel-members,.channel-members,.channels-3g2vYe,.guilds-wrapper,.search.search-bar,.theme-dark.chatform,.container-iksrDt,.container-3lnMWU,.theme-dark.title-qAcLxz{background:transparent!important;}.theme-dark.layer,.theme-dark.layers,.typeWindows-15E0Ys{background:rgba(0,0,0,0.18)!important;}`
-						
 					head.appendChild(theme);
 				}
 				else {
