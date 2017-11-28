3bcc8fe336343a3e584e692eecbcccf602441266
Mirco Wittrien <mirco.wittrien@gmail.com>
2017-11-28 19:54:12 +0100

    Update DiscordPreview.js

--- Plugins/ThemeRepo/res/DiscordPreview.js
+++ Plugins/ThemeRepo/res/DiscordPreview.js
@@ -7,8 +7,8 @@ window.onkeyup = function (e) {
 window.onmessage = function (e) {
 	if (typeof e.data === "object" && e.data.origin == "ThemeRepo") {
 		switch (e.data.reason) {
+			var body = document.querySelector("body"), head = document.querySelector("head"), theme;
 			case "OnLoad":
-				var body = document.querySelector("body");
 				body.innerHTML = body.innerHTML.replace(new RegExp("REPLACE_USERNAME", "g"), e.data.username);
 				body.innerHTML = body.innerHTML.replace(new RegExp("REPLACE_AVATAR", "g"), e.data.avatar.split('"').join(''));
 				body.innerHTML = body.innerHTML.replace(new RegExp("REPLACE_DISCRIMINATOR", "g"), e.data.discriminator);
@@ -16,18 +16,29 @@ window.onmessage = function (e) {
 			case "NewTheme":
 				document.querySelectorAll("style.previewTheme").forEach(theme => theme.remove());
 				
-				var theme = document.createElement("style");
+				theme = document.createElement("style");
 				theme.classList.add("previewTheme");
 				theme.innerText = e.data.css;
 					
-				document.querySelector("head").appendChild(theme);
+				head.appendChild(theme);
 				break;
 			case "DarkLight":
-				var body = document.querySelector("body");
 				if (e.data.light)
 					body.innerHTML = body.innerHTML.replace(new RegExp("theme-dark", "g"), "theme-light");
 				else 
 					body.innerHTML = body.innerHTML.replace(new RegExp("theme-light", "g"), "theme-dark");
+				break;
+			case "ThemeFixer":		
+				if (e.data.fixer) {
+					theme = document.createElement("style");
+					theme.classList.add("themeFixer");
+					theme.innerText =`#voice-connection,#friends,.friends-header,.friends-table,.guilds-wrapper,.guild-headerheader,.channels-wrap,.private-channels.search-bar,.private-channels,.guild-channels,.account,.friend-table-add-header,.chat,.content,.layers,.title-wrap:not(.search-bar),.messages-wrapper,.messages.dividerspan,.messages.divider:before,.content,.message-group-blocked,.is-local-bot-message,.channel-members-loading,.channel-members-loading.heading,.channel-members-loading.member,.typing,.layer,.layers,.container-RYiLUQ,.theme-dark.ui-standard-sidebar-view,.theme-dark.ui-standard-sidebar-view.sidebar-region,.theme-dark.ui-standard-sidebar-view.content-region,.theme-dark.channel-members,.layer,.layers,.container-2OU7Cz,.theme-dark.title-qAcLxz,.theme-dark.chatform,.channels-3g2vYe,.theme-dark.friends-table,.theme-dark.messages-wrapper,.content.flex-spacer,.theme-dark.chat>.content,.theme-dark.chat,.container-2OU7Cz,.theme-dark.channel-members,.channel-members,.channels-3g2vYe,.guilds-wrapper,.search.search-bar,.theme-dark.chatform,.container-iksrDt,.container-3lnMWU,.theme-dark.title-qAcLxz{background:transparent!important;}.theme-dark.layer,.theme-dark.layers,.typeWindows-15E0Ys{background:rgba(0,0,0,0.18)!important;}`
+						
+					head.appendChild(theme);
+				}
+				else {
+					document.querySelectorAll("style.themeFixer").forEach(theme => theme.remove());
+				}
 				break;
 		}
 	}
