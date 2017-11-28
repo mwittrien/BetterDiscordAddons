4dd0b142b416cbac50592248efa730ba523c95f7
Mirco Wittrien <mirco.wittrien@gmail.com>
2017-11-28 17:59:41 +0100

    Update DiscordPreview.js

--- Plugins/ThemeRepo/res/DiscordPreview.js
+++ Plugins/ThemeRepo/res/DiscordPreview.js
@@ -2,13 +2,11 @@ function receiveMessage(e) {
 	if (typeof e.data === "object" && e.data.origin == "ThemeRepo") {
 		switch (e.data.reason) {
 			case "NewTheme":
-				document.querySelectorAll("link.previewTheme").forEach(theme => theme.remove());
+				document.querySelectorAll("style.previewTheme").forEach(theme => theme.remove());
 				
-				var theme = document.createElement("link");
+				var theme = document.createElement("style");
 				theme.classList.add("previewTheme");
-				theme.type = "text/css";
-				theme.rel = "Stylesheet";
-				theme.href = e.data.url;
+				theme.innerText = e.data.css;
 					
 				document.querySelector("head").appendChild(theme);
 				break;
