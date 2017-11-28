59accfe2767e73489eaf242edcaa80c2e7e2df65
Mirco Wittrien <mirco.wittrien@gmail.com>
2017-11-28 17:53:16 +0100

    Update DiscordPreview.js

--- Plugins/ThemeRepo/res/DiscordPreview.js
+++ Plugins/ThemeRepo/res/DiscordPreview.js
@@ -1,14 +1,18 @@
 function receiveMessage(e) {
-	if (typeof e.data === "object" && e.data.origin == "ThemeRepo" && e.data.reason == "NewTheme") {
-		document.querySelectorAll("link.previewTheme").forEach(theme => theme.remove());
-		
-		var theme = document.createElement("link");
-		theme.classList.add("previewTheme");
-		theme.type = "text/css";
-		theme.rel = "Stylesheet";
-		theme.href = e.data.url;
-			
-		document.querySelector("head").appendChild(theme);
+	if (typeof e.data === "object" && e.data.origin == "ThemeRepo") {
+		switch (e.data.reason) {
+			case "NewTheme":
+				document.querySelectorAll("link.previewTheme").forEach(theme => theme.remove());
+				
+				var theme = document.createElement("link");
+				theme.classList.add("previewTheme");
+				theme.type = "text/css";
+				theme.rel = "Stylesheet";
+				theme.href = e.data.url;
+					
+				document.querySelector("head").appendChild(theme);
+				break;
+		}
 	}
 }
 
