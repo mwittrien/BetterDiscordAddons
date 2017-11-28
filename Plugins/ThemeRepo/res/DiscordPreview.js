c1ca5e235c9ae6b937415684c80f9ead2045eab6
Mirco Wittrien <mirco.wittrien@gmail.com>
2017-11-28 18:54:09 +0100

    Update DiscordPreview.js

--- Plugins/ThemeRepo/res/DiscordPreview.js
+++ Plugins/ThemeRepo/res/DiscordPreview.js
@@ -1,11 +1,11 @@
 function receiveMessage(e) {
 	if (typeof e.data === "object" && e.data.origin == "ThemeRepo") {
-		console.log(e);
 		switch (e.data.reason) {
 			case "OnLoad":
-				document.innerHTML = document.innerHTML.replace(new RegExp("REPLACE_USERNAME", "g"), e.data.username);
-				document.innerHTML = document.innerHTML.replace(new RegExp("REPLACE_AVATAR", "g"), e.data.avatar);
-				document.innerHTML = document.innerHTML.replace(new RegExp("REPLACE_DISCRIMINATOR", "g"), e.data.discriminator);
+				var body = document.querySelector("body");
+				body.innerHTML = body.innerHTML.replace(new RegExp("REPLACE_USERNAME", "g"), e.data.username);
+				body.innerHTML = body.innerHTML.replace(new RegExp("REPLACE_AVATAR", "g"), e.data.avatar);
+				body.innerHTML = body.innerHTML.replace(new RegExp("REPLACE_DISCRIMINATOR", "g"), e.data.discriminator);
 				break;
 			case "NewTheme":
 				document.querySelectorAll("style.previewTheme").forEach(theme => theme.remove());
@@ -25,6 +25,5 @@ function receiveMessage(e) {
 window.addEventListener("message", receiveMessage, false);
 
 window.onload = function () {
-	console.log("hi");
 	window.parent.postMessage({origin:"DiscordPreview",reason:"OnLoad"},"*");
 };
