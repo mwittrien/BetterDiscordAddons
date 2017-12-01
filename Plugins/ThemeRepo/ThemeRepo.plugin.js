75ae385e6355a0fbad6e3f2a83f3390902b8de45
Mirco Wittrien <mirco.wittrien@gmail.com>
2017-12-01 10:22:37 +0100

    Update ThemeRepo.plugin.js

--- Plugins/ThemeRepo/ThemeRepo.plugin.js
+++ Plugins/ThemeRepo/ThemeRepo.plugin.js
@@ -3,6 +3,7 @@
 class ThemeRepo {
 	constructor () {
 		this.settingsWindowObserver = new MutationObserver(() => {});
+		this.innerSettingsWindowObserver = new MutationObserver(() => {});
 		
 		this.loading = false;
 		
@@ -28,7 +29,8 @@ class ThemeRepo {
 					<div class="flexChild-1KGW5q switchEnabled-3CPlLV switch-3lyafC value-kmHGfs sizeDefault-rZbSBU size-yI1KRe themeDefault-3M0dJU previewCheckboxWrapper" style="flex: 0 0 auto;">
 						<input type="checkbox" class="checkboxEnabled-4QfryV checkbox-1KYsPm previewCheckbox">
 					</div>
-					<svg class="favStar" version="1.1" xmlns="http://www.w3.org/2000/svg" width="20px" height="20px"><path fill="none" stroke="black" d="M 19.542, 9.092 c 0.393 -0.383, 0.532 -0.946, 0.362 -1.468 c -0.17 -0.523 -0.613 -0.896 -1.157 -0.975 l -4.837 -0.703 c -0.206 -0.03 -0.384 -0.159 -0.476 -0.346 L 11.273, 1.217 c -0.243 -0.492 -0.736 -0.798 -1.285 -0.798 c -0.549, 0 -1.042, 0.306 -1.284, 0.798 L 6.541, 5.6 c -0.092, 0.187 -0.27, 0.316 -0.476, 0.346 L 1.228, 6.649 c -0.544, 0.079 -0.987, 0.452 -1.157, 0.975 c -0.17, 0.523 -0.031, 1.085, 0.362, 1.468 l 3.5, 3.411 c 0.149, 0.146, 0.218, 0.355, 0.182, 0.56 L 3.29, 17.88 c -0.073, 0.424, 0.038, 0.836, 0.312, 1.162 c 0.426, 0.507, 1.171, 0.661, 1.766, 0.348 l 4.326 -2.274 c 0.181 -0.095, 0.408 -0.094, 0.589, 0 l 4.326, 2.274 c 0.21, 0.111, 0.435, 0.167, 0.666, 0.167 c 0.423, 0, 0.824 -0.188, 1.099 -0.515 c 0.275 -0.325, 0.386 -0.738, 0.312 -1.162 l -0.826 -4.817 c -0.035 -0.205, 0.033 -0.414, 0.182 -0.56 L 19.542, 9.092 z"/></svg>
+					<svg class="favIcon" version="1.1" xmlns="http://www.w3.org/2000/svg" width="20px" height="20px"><path fill="none" stroke="black" d="M 19.542, 9.092 c 0.393 -0.383, 0.532 -0.946, 0.362 -1.468 c -0.17 -0.523 -0.613 -0.896 -1.157 -0.975 l -4.837 -0.703 c -0.206 -0.03 -0.384 -0.159 -0.476 -0.346 L 11.273, 1.217 c -0.243 -0.492 -0.736 -0.798 -1.285 -0.798 c -0.549, 0 -1.042, 0.306 -1.284, 0.798 L 6.541, 5.6 c -0.092, 0.187 -0.27, 0.316 -0.476, 0.346 L 1.228, 6.649 c -0.544, 0.079 -0.987, 0.452 -1.157, 0.975 c -0.17, 0.523 -0.031, 1.085, 0.362, 1.468 l 3.5, 3.411 c 0.149, 0.146, 0.218, 0.355, 0.182, 0.56 L 3.29, 17.88 c -0.073, 0.424, 0.038, 0.836, 0.312, 1.162 c 0.426, 0.507, 1.171, 0.661, 1.766, 0.348 l 4.326 -2.274 c 0.181 -0.095, 0.408 -0.094, 0.589, 0 l 4.326, 2.274 c 0.21, 0.111, 0.435, 0.167, 0.666, 0.167 c 0.423, 0, 0.824 -0.188, 1.099 -0.515 c 0.275 -0.325, 0.386 -0.738, 0.312 -1.162 l -0.826 -4.817 c -0.035 -0.205, 0.033 -0.414, 0.182 -0.56 L 19.542, 9.092 z"/></svg>
+					<svg class="gitIcon" version="1.1" xmlns="http://www.w3.org/2000/svg" width="20px" height="20px"><g fill="white"><path d="M 7.19, 16.027 c -0.139, 0.026 -0.199, 0.091 -0.182, 0.195 c 0.017, 0.104, 0.095, 0.138, 0.234, 0.104 c 0.139 -0.035, 0.199 -0.095, 0.182 -0.182 C 7.406, 16.049, 7.328, 16.01, 7.19, 16.027 z"/><path d="M 6.45, 16.131 c -0.138, 0 -0.208, 0.047 -0.208, 0.143 c 0, 0.112, 0.074, 0.16, 0.221, 0.143 c 0.138, 0, 0.208 -0.048, 0.208 -0.143 C 6.671, 16.162, 6.597, 16.114, 6.45, 16.131 z"/><path d="M 5.438, 16.092 c -0.035, 0.095, 0.022, 0.16, 0.169, 0.195 c 0.13, 0.052, 0.212, 0.026, 0.247 -0.078 c 0.026 -0.095 -0.03 -0.164 -0.169 -0.208 C 5.554, 15.967, 5.472, 15.996, 5.438, 16.092 z"/><path d="M 18.837, 1.097 C 18.106, 0.366, 17.226, 0, 16.196, 0 H 3.738 C 2.708, 0, 1.828, 0.366, 1.097, 1.097 C 0.366, 1.828, 0, 2.708, 0, 3.738 v 12.459 c 0, 1.03, 0.366, 1.91, 1.097, 2.641 c 0.731, 0.731, 1.612, 1.097, 2.641, 1.097 h 2.907 c 0.19, 0, 0.333 -0.007, 0.428 -0.019 c 0.095 -0.013, 0.19 -0.069, 0.285 -0.169 c 0.095 -0.099, 0.143 -0.244, 0.143 -0.435 c 0 -0.026 -0.002 -0.32 -0.007 -0.883 c -0.004 -0.562 -0.007 -1.008 -0.007 -1.337 l -0.298, 0.052 c -0.19, 0.035 -0.43, 0.05 -0.72, 0.045 c -0.29 -0.004 -0.59 -0.035 -0.902 -0.091 c -0.312 -0.056 -0.601 -0.186 -0.87 -0.389 c -0.268 -0.203 -0.458 -0.469 -0.571 -0.798 l -0.13 -0.299 c -0.086 -0.199 -0.223 -0.419 -0.409 -0.662 c -0.186 -0.242 -0.374 -0.407 -0.564 -0.493 l -0.091 -0.065 c -0.06 -0.043 -0.117 -0.095 -0.169 -0.156 c -0.052 -0.061 -0.091 -0.121 -0.117 -0.182 c -0.026 -0.061 -0.004 -0.11, 0.065 -0.149 c 0.069 -0.039, 0.195 -0.058, 0.376 -0.058 l 0.259, 0.039 c 0.173, 0.035, 0.387, 0.138, 0.642, 0.311 c 0.255, 0.173, 0.465, 0.398, 0.629, 0.675 c 0.199, 0.355, 0.439, 0.625, 0.72, 0.811 c 0.281, 0.186, 0.565, 0.279, 0.85, 0.279 s 0.532 -0.022, 0.74 -0.065 c 0.208 -0.043, 0.402 -0.108, 0.584 -0.195 c 0.078 -0.58, 0.29 -1.025, 0.636 -1.337 c -0.493 -0.052 -0.936 -0.13 -1.33 -0.234 c -0.394 -0.104 -0.8 -0.272 -1.22 -0.506 c -0.42 -0.234 -0.768 -0.523 -1.045 -0.87 c -0.277 -0.346 -0.504 -0.8 -0.681 -1.363 c -0.177 -0.562 -0.266 -1.211 -0.266 -1.947 c 0 -1.047, 0.342 -1.938, 1.025 -2.673 c -0.32 -0.787 -0.29 -1.67, 0.091 -2.647 c 0.251 -0.078, 0.623 -0.019, 1.116, 0.175 c 0.493, 0.195, 0.854, 0.361, 1.084, 0.5 c 0.229, 0.138, 0.413, 0.255, 0.552, 0.35 c 0.805 -0.225, 1.635 -0.337, 2.492 -0.337 c 0.856, 0, 1.687, 0.112, 2.492, 0.337 l 0.493 -0.311 c 0.338 -0.208, 0.735 -0.398, 1.194 -0.571 c 0.459 -0.173, 0.809 -0.221, 1.051 -0.143 c 0.389, 0.978, 0.424, 1.86, 0.104, 2.647 c 0.683, 0.735, 1.025, 1.627, 1.025, 2.673 c 0, 0.735 -0.089, 1.387 -0.266, 1.953 c -0.177, 0.567 -0.406, 1.021 -0.688, 1.363 c -0.281, 0.342 -0.632, 0.629 -1.051, 0.863 c -0.42, 0.234 -0.826, 0.402 -1.22, 0.506 c -0.394, 0.104 -0.837, 0.182 -1.33, 0.234 c 0.45, 0.389, 0.675, 1.003, 0.675, 1.843 v 3.102 c 0, 0.147, 0.021, 0.266, 0.065, 0.357 c 0.044, 0.091, 0.113, 0.153, 0.208, 0.188 c 0.096, 0.035, 0.18, 0.056, 0.253, 0.065 c 0.074, 0.009, 0.18, 0.013, 0.318, 0.013 h 2.907 c 1.029, 0, 1.91 -0.366, 2.641 -1.097 c 0.731 -0.731, 1.097 -1.612, 1.097 -2.641 V 3.738 C 19.933, 2.708, 19.568, 1.827, 18.837, 1.097 z"/><path d="M 3.945, 14.509 c -0.06, 0.043 -0.052, 0.112, 0.026, 0.208 c 0.087, 0.086, 0.156, 0.1, 0.208, 0.039 c 0.061 -0.043, 0.052 -0.112 -0.026 -0.208 C 4.066, 14.47, 3.997, 14.457, 3.945, 14.509 z"/><path d="M 3.517, 14.184 c -0.026, 0.061, 0.004, 0.113, 0.091, 0.156 c 0.069, 0.043, 0.126, 0.035, 0.169 -0.026 c 0.026 -0.061 -0.004 -0.113 -0.091 -0.156 C 3.599, 14.132, 3.543, 14.141, 3.517, 14.184 z"/><path d="M 4.348, 15.015 c -0.078, 0.043 -0.078, 0.121, 0, 0.234 c 0.078, 0.113, 0.151, 0.143, 0.221, 0.091 c 0.078 -0.061, 0.078 -0.143, 0 -0.247 C 4.499, 14.981, 4.425, 14.954, 4.348, 15.015 z"/><path d="M 4.802, 15.599 c -0.078, 0.069 -0.061, 0.151, 0.052, 0.247 c 0.104, 0.104, 0.19, 0.117, 0.259, 0.039 c 0.069 -0.069, 0.052 -0.151 -0.052 -0.246 C 4.958, 15.534, 4.871, 15.521, 4.802, 15.599 z"/></g></svg>
 					<button type="button" class="btn-download buttonBrandFilledDefault-2Rs6u5 buttonFilledDefault-AELjWf buttonDefault-2OLW-v button-2t3of8 buttonFilled-29g7b5 buttonBrandFilled-3Mv0Ra mediumGrow-uovsMu">
 						<div class="contentsDefault-nt2Ym5 contents-4L4hQM contentsFilled-3M8HCx contents-4L4hQM">Download</div>
 					</button>
@@ -54,10 +56,13 @@ class ThemeRepo {
 								</svg>
 							</div>
 							<div class="header inner-tqJwAU">
-								<div class="header-tab-bar-wrapper" style="margin-top:0;">
+								<div class="header-tab-bar-wrapper">
 									<div class="tab-bar TOP">
 										<div tab="themes" class="tab-bar-item selected">Themes</div>
 										<div tab="settings" class="tab-bar-item">Settings</div>
+									</div>
+									<div class="inputWrapper-3xoRWR vertical-3X17r5 directionColumn-2h-LPR searchWrapper">
+										<input type="text" class="input-2YozMi size16-3IvaX_" id="input-search" placeholder="Search for...">
 									</div>
 									<div class="mention-filter sort-filter">
 										<div class="label">Sort by:</div>
@@ -148,21 +153,40 @@ class ThemeRepo {
 			.themerepo-modal .inner-1_1f7b {
 				min-height: 100%;
 			}
+			.themerepo-modal .header {
+				overflow: visible !important;
+			}
+			.themerepo-modal .header-tab-bar-wrapper {
+				margin-top: 0 !important;
+			}
+			.themerepo-modal .searchWrapper {
+				margin-top: -5px !important;
+			}
+			.themerepo-modal .searchWrapper #input-search {
+				padding: 1px 8px !important;
+			}
 			.themerepo-modal .previewCheckboxWrapper {
 				position: absolute;
 				right: 10px;
 				top: 10px;
 			}
-			.themerepo-modal .favStar {
+			.themerepo-modal .gitIcon,
+			.themerepo-modal .favIcon {
 				position: absolute;
-				right: 60px;
-				top: 11px;
+				cursor: pointer;
+				top: 12px;
 			}
-			.themerepo-modal .favStar path {
+			.themerepo-modal .gitIcon {
+				right: 60px;
+			}
+			.themerepo-modal .favIcon {
+				right: 90px;
+			}
+			.themerepo-modal .favIcon path {
 				stroke: yellow;
 				fill: none;
 			}
-			.themerepo-modal .favStar.favorized path {
+			.themerepo-modal .favIcon.favorized path {
 				stroke: yellow;
 				fill: yellow;
 			}
@@ -175,9 +199,9 @@ class ThemeRepo {
 
 	getName () {return "ThemeRepo";}
 
-	getDescription () {return "Allows you to preview all themes from the themerepo and download them on the fly. Repo button is in the theme settings.";}
+	getDescription () {return "Allows you to preview all themes from the theme repo and download them on the fly. Repo button is in the theme settings.";}
 
-	getVersion () {return "1.0.3";}
+	getVersion () {return "1.0.4";}
 
 	getAuthor () {return "DevilBro";}
 	
@@ -206,20 +230,7 @@ class ThemeRepo {
 						if (change.addedNodes) {
 							change.addedNodes.forEach((node) => {
 								if (node && node.tagName && node.getAttribute("layer-id") == "user-settings") {
-									var settingsObserver = new MutationObserver((changes2, _) => {
-										changes2.forEach(
-											(change2, j) => {
-												if (change2.addedNodes) {
-													change2.addedNodes.forEach((node2) => {
-														if (node2 && node2.tagName && node2.querySelector(".bd-pfbtn") && node2.querySelector("h2") && node2.querySelector("h2").innerText.toLowerCase() === "themes") {
-															this.addThemeRepoButton(node2);
-														}
-													});
-												}
-											}
-										);
-									});
-									settingsObserver.observe(node, {childList:true, subtree:true});
+									this.innerSettingsWindowObserver.observe(node, {childList:true, subtree:true});
 								}
 							});
 						}
@@ -227,6 +238,26 @@ class ThemeRepo {
 				);
 			});
 			this.settingsWindowObserver.observe(document.querySelector(".layers"), {childList:true});
+			
+			this.innerSettingsWindowObserver =  new MutationObserver((changes2, _) => {
+				changes2.forEach(
+					(change2, j) => {
+						if (change2.addedNodes) {
+							change2.addedNodes.forEach((node2) => {
+								if (node2 && node2.tagName && node2.querySelector(".bd-pfbtn") && node2.querySelector("h2") && node2.querySelector("h2").innerText.toLowerCase() === "themes") {
+									this.addThemeRepoButton(node2);
+								}
+							});
+						}
+					}
+				);
+			});
+			if (document.querySelector(".layer[layer-id='user-settings']")) this.innerSettingsWindowObserver.observe(document.querySelector(".layer[layer-id='user-settings']"), {childList:true, subtree:true});
+			
+			var bdbutton = document.querySelector(".bd-pfbtn");
+			if (bdbutton && bdbutton.parentElement.querySelector("h2") && bdbutton.parentElement.querySelector("h2").innerText.toLowerCase() === "themes") {
+				this.addThemeRepoButton(bdbutton.parentElement);
+			}
 			
 			BDfunctionsDevilBro.appendLocalStyle(this.getName(), this.css);
 			
@@ -243,6 +274,7 @@ class ThemeRepo {
 			BDfunctionsDevilBro.unloadMessage(this.getName(), this.getVersion());
 			
 			this.settingsWindowObserver.disconnect();
+			this.innerSettingsWindowObserver.disconnect();
 			
 			BDfunctionsDevilBro.removeLocalStyle(this.getName());
 			
@@ -261,8 +293,8 @@ class ThemeRepo {
 					if (!this.loading) this.openDiscordPreview(); 
 					else BDfunctionsDevilBro.showToast(`Themes are still being fetched. Try again in some seconds.`, {type:"danger"});
 				})
-				.on("onmouseenter", (e) => {
-					BDfunctionsDevilBro.createTooltip("Open Theme Repo", e.currentTarget, {type:"right",selector:"update-button-tooltip"});
+				.on("mouseenter", (e) => {
+					BDfunctionsDevilBro.createTooltip("Open Theme Repo", e.currentTarget, {type:"right",selector:"themerepo-button-tooltip"});
 				});
 		}
 	}
@@ -271,11 +303,17 @@ class ThemeRepo {
 		var frame = $(this.frameMarkup)[0];
 		var lightTheme = BDfunctionsDevilBro.getDiscordTheme() == "theme-light";
 		var themeRepoModal = $(this.themeRepoModalMarkup);
-		themeRepoModal.find(".themeAmount").text(themeRepoModal.find(".themeAmount").text() + " " + Object.keys(this.loadedThemes).length + " Themes");
+		themeRepoModal.enableSearch = false;
 		themeRepoModal.find("#input-darklight").prop("checked", lightTheme);
 		themeRepoModal.find("#input-customcss").prop("checked", false);
 		themeRepoModal.find("#input-themefixer").prop("checked", false);
 		themeRepoModal
+			.on("keyup." + this.getName(), "#input-search", (e) => {
+				if (e.which == 13) {
+					themeRepoModal.enableSearch = true;
+					this.addThemeEntries(themeRepoModal);	
+				}
+			})
 			.on("click." + this.getName(), ".btn-cancel, .backdrop-2ohBEd", () => {
 				frame.remove();
 				$(document).off("keyup." + this.getName());
@@ -378,14 +416,24 @@ class ThemeRepo {
 			this.loadedThemes[url].fav = favorites[url] ? false : true;
 			themes.push(this.loadedThemes[url]);
 		}
+		if (modal.enableSearch) {
+			var searchInput = modal.find("#input-search");
+			themes = themes.filter((theme) => {
+				var themeText = theme.name + " " + theme.version + " " + theme.author + " " + theme.description;
+				return themeText.toLowerCase().indexOf(searchInput.val().toLowerCase()) > -1 ? theme : null;
+			});
+			modal.enableSearch = false;
+			searchInput.val("");
+		}
 		themes = BDfunctionsDevilBro.sortArrayByKey(themes, modal.find(".sort-filter .value").attr("option"));
 		if (modal.find(".order-filter .value").attr("option") == "desc") themes.reverse();
+		modal.find(".themeAmount").text("Theme Repository " + themes.length + "/" + Object.keys(this.loadedThemes).length + " Themes");
 		for (let theme of themes) {
 			let entry = $(this.themeEntryMarkup);
 			modal.find(".themes").append(entry);
 			entry.find(".bda-name").html(theme.name + (theme.version ? " v" + theme.version : "") + (theme.author ? " by " + theme.author : ""));
 			entry.find(".bda-description").html(theme.description);
-			if (!theme.fav) entry.find(".favStar")[0].classList.add("favorized");
+			if (!theme.fav) entry.find(".favIcon")[0].classList.add("favorized");
 			entry
 				.on("change." + this.getName(), ".previewCheckbox", (e) => {
 					modal.find(".previewCheckbox").not(e.target).prop("checked", false);
@@ -396,11 +444,23 @@ class ThemeRepo {
 					});
 					frame.contentWindow.postMessage({origin:"ThemeRepo",reason:"NewTheme",checked:$(e.target).prop("checked"),css:theme.css},"*");
 				})
-				.on("click." + this.getName(), ".favStar", (e) => {
+				.on("click." + this.getName(), ".favIcon", (e) => {
 					e.currentTarget.classList.toggle("favorized");
 					if (e.currentTarget.classList.contains("favorized")) favorites[theme.url] = true;
 					else delete favorites[theme.url];
 					BDfunctionsDevilBro.saveAllData(favorites, this.getName(), "favorites");
+				})
+				.on("click." + this.getName(), ".gitIcon", (e) => {
+					var giturl = null;
+					if (theme.url.indexOf("https://raw.githubusercontent.com") == 0) {
+						giturl = theme.url.replace("//raw.githubusercontent", "//github").replace("/master/", "/blob/master/");
+					}
+					else if (theme.url.indexOf("https://gist.githubusercontent.com/") == 0) {
+						giturl = theme.url.replace("//gist.githubusercontent", "//gist.github").split("/raw/")[0];
+					}
+					if (giturl) {
+						window.open(giturl, "_blank");
+					}
 				})
 				.on("click." + this.getName(), ".btn-download", () => {
 					this.downloadTheme(theme);
@@ -436,10 +496,15 @@ class ThemeRepo {
 					let text = body;
 					if (text.split("*//").length > 1 && text.split("\n").length > 1) {
 						for (let tag of tags) {
-							let temp = text.replace(new RegExp("\\s*\:\\s*", "g"), ":").split('"' + tag + '":"');
+							if (theme.name == "Rainbow_Kitten") console.log(tag);
+							let temp = text.replace(new RegExp("\\s*\:\\s*", "g"), ":").replace(new RegExp("\\s*\}\\s*", "g"), "}").split('"' + tag + '":"');
+							if (theme.name == "Rainbow_Kitten") console.log(temp);
 							temp = temp.length > 1 ? temp[1].split('",')[0].split('"}')[0] : null;
+							if (theme.name == "Rainbow_Kitten") console.log(temp);
 							temp = temp && tag != "version" ? temp.charAt(0).toUpperCase() + temp.slice(1) : temp;
+							if (theme.name == "Rainbow_Kitten") console.log(temp);
 							theme[tag] = temp;
+							if (theme.name == "Rainbow_Kitten") console.log(temp);
 						}
 						theme.css = text.split("\n").slice(1).join("\n").replace(new RegExp("[\\r|\\n|\\t]", "g"), "");
 						theme.url = url;
