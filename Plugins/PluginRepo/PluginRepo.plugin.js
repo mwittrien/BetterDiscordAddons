7f6c94a29bc2f60542689f704389767abfbf405e
Mirco Wittrien <mirco.wittrien@gmail.com>
2017-12-02 11:37:24 +0100

    Update PluginRepo.plugin.js

--- Plugins/PluginRepo/PluginRepo.plugin.js
+++ Plugins/PluginRepo/PluginRepo.plugin.js
@@ -26,6 +26,7 @@ class PluginRepo {
 				<div class="bda-right">
 					<svg class="favIcon" version="1.1" xmlns="http://www.w3.org/2000/svg" width="20px" height="20px"><path fill="none" stroke="black" d="M 19.542, 9.092 c 0.393 -0.383, 0.532 -0.946, 0.362 -1.468 c -0.17 -0.523 -0.613 -0.896 -1.157 -0.975 l -4.837 -0.703 c -0.206 -0.03 -0.384 -0.159 -0.476 -0.346 L 11.273, 1.217 c -0.243 -0.492 -0.736 -0.798 -1.285 -0.798 c -0.549, 0 -1.042, 0.306 -1.284, 0.798 L 6.541, 5.6 c -0.092, 0.187 -0.27, 0.316 -0.476, 0.346 L 1.228, 6.649 c -0.544, 0.079 -0.987, 0.452 -1.157, 0.975 c -0.17, 0.523 -0.031, 1.085, 0.362, 1.468 l 3.5, 3.411 c 0.149, 0.146, 0.218, 0.355, 0.182, 0.56 L 3.29, 17.88 c -0.073, 0.424, 0.038, 0.836, 0.312, 1.162 c 0.426, 0.507, 1.171, 0.661, 1.766, 0.348 l 4.326 -2.274 c 0.181 -0.095, 0.408 -0.094, 0.589, 0 l 4.326, 2.274 c 0.21, 0.111, 0.435, 0.167, 0.666, 0.167 c 0.423, 0, 0.824 -0.188, 1.099 -0.515 c 0.275 -0.325, 0.386 -0.738, 0.312 -1.162 l -0.826 -4.817 c -0.035 -0.205, 0.033 -0.414, 0.182 -0.56 L 19.542, 9.092 z"/></svg>
 					<svg class="gitIcon" version="1.1" xmlns="http://www.w3.org/2000/svg" width="20px" height="20px"><g fill="white"><path d="M 7.19, 16.027 c -0.139, 0.026 -0.199, 0.091 -0.182, 0.195 c 0.017, 0.104, 0.095, 0.138, 0.234, 0.104 c 0.139 -0.035, 0.199 -0.095, 0.182 -0.182 C 7.406, 16.049, 7.328, 16.01, 7.19, 16.027 z"/><path d="M 6.45, 16.131 c -0.138, 0 -0.208, 0.047 -0.208, 0.143 c 0, 0.112, 0.074, 0.16, 0.221, 0.143 c 0.138, 0, 0.208 -0.048, 0.208 -0.143 C 6.671, 16.162, 6.597, 16.114, 6.45, 16.131 z"/><path d="M 5.438, 16.092 c -0.035, 0.095, 0.022, 0.16, 0.169, 0.195 c 0.13, 0.052, 0.212, 0.026, 0.247 -0.078 c 0.026 -0.095 -0.03 -0.164 -0.169 -0.208 C 5.554, 15.967, 5.472, 15.996, 5.438, 16.092 z"/><path d="M 18.837, 1.097 C 18.106, 0.366, 17.226, 0, 16.196, 0 H 3.738 C 2.708, 0, 1.828, 0.366, 1.097, 1.097 C 0.366, 1.828, 0, 2.708, 0, 3.738 v 12.459 c 0, 1.03, 0.366, 1.91, 1.097, 2.641 c 0.731, 0.731, 1.612, 1.097, 2.641, 1.097 h 2.907 c 0.19, 0, 0.333 -0.007, 0.428 -0.019 c 0.095 -0.013, 0.19 -0.069, 0.285 -0.169 c 0.095 -0.099, 0.143 -0.244, 0.143 -0.435 c 0 -0.026 -0.002 -0.32 -0.007 -0.883 c -0.004 -0.562 -0.007 -1.008 -0.007 -1.337 l -0.298, 0.052 c -0.19, 0.035 -0.43, 0.05 -0.72, 0.045 c -0.29 -0.004 -0.59 -0.035 -0.902 -0.091 c -0.312 -0.056 -0.601 -0.186 -0.87 -0.389 c -0.268 -0.203 -0.458 -0.469 -0.571 -0.798 l -0.13 -0.299 c -0.086 -0.199 -0.223 -0.419 -0.409 -0.662 c -0.186 -0.242 -0.374 -0.407 -0.564 -0.493 l -0.091 -0.065 c -0.06 -0.043 -0.117 -0.095 -0.169 -0.156 c -0.052 -0.061 -0.091 -0.121 -0.117 -0.182 c -0.026 -0.061 -0.004 -0.11, 0.065 -0.149 c 0.069 -0.039, 0.195 -0.058, 0.376 -0.058 l 0.259, 0.039 c 0.173, 0.035, 0.387, 0.138, 0.642, 0.311 c 0.255, 0.173, 0.465, 0.398, 0.629, 0.675 c 0.199, 0.355, 0.439, 0.625, 0.72, 0.811 c 0.281, 0.186, 0.565, 0.279, 0.85, 0.279 s 0.532 -0.022, 0.74 -0.065 c 0.208 -0.043, 0.402 -0.108, 0.584 -0.195 c 0.078 -0.58, 0.29 -1.025, 0.636 -1.337 c -0.493 -0.052 -0.936 -0.13 -1.33 -0.234 c -0.394 -0.104 -0.8 -0.272 -1.22 -0.506 c -0.42 -0.234 -0.768 -0.523 -1.045 -0.87 c -0.277 -0.346 -0.504 -0.8 -0.681 -1.363 c -0.177 -0.562 -0.266 -1.211 -0.266 -1.947 c 0 -1.047, 0.342 -1.938, 1.025 -2.673 c -0.32 -0.787 -0.29 -1.67, 0.091 -2.647 c 0.251 -0.078, 0.623 -0.019, 1.116, 0.175 c 0.493, 0.195, 0.854, 0.361, 1.084, 0.5 c 0.229, 0.138, 0.413, 0.255, 0.552, 0.35 c 0.805 -0.225, 1.635 -0.337, 2.492 -0.337 c 0.856, 0, 1.687, 0.112, 2.492, 0.337 l 0.493 -0.311 c 0.338 -0.208, 0.735 -0.398, 1.194 -0.571 c 0.459 -0.173, 0.809 -0.221, 1.051 -0.143 c 0.389, 0.978, 0.424, 1.86, 0.104, 2.647 c 0.683, 0.735, 1.025, 1.627, 1.025, 2.673 c 0, 0.735 -0.089, 1.387 -0.266, 1.953 c -0.177, 0.567 -0.406, 1.021 -0.688, 1.363 c -0.281, 0.342 -0.632, 0.629 -1.051, 0.863 c -0.42, 0.234 -0.826, 0.402 -1.22, 0.506 c -0.394, 0.104 -0.837, 0.182 -1.33, 0.234 c 0.45, 0.389, 0.675, 1.003, 0.675, 1.843 v 3.102 c 0, 0.147, 0.021, 0.266, 0.065, 0.357 c 0.044, 0.091, 0.113, 0.153, 0.208, 0.188 c 0.096, 0.035, 0.18, 0.056, 0.253, 0.065 c 0.074, 0.009, 0.18, 0.013, 0.318, 0.013 h 2.907 c 1.029, 0, 1.91 -0.366, 2.641 -1.097 c 0.731 -0.731, 1.097 -1.612, 1.097 -2.641 V 3.738 C 19.933, 2.708, 19.568, 1.827, 18.837, 1.097 z"/><path d="M 3.945, 14.509 c -0.06, 0.043 -0.052, 0.112, 0.026, 0.208 c 0.087, 0.086, 0.156, 0.1, 0.208, 0.039 c 0.061 -0.043, 0.052 -0.112 -0.026 -0.208 C 4.066, 14.47, 3.997, 14.457, 3.945, 14.509 z"/><path d="M 3.517, 14.184 c -0.026, 0.061, 0.004, 0.113, 0.091, 0.156 c 0.069, 0.043, 0.126, 0.035, 0.169 -0.026 c 0.026 -0.061 -0.004 -0.113 -0.091 -0.156 C 3.599, 14.132, 3.543, 14.141, 3.517, 14.184 z"/><path d="M 4.348, 15.015 c -0.078, 0.043 -0.078, 0.121, 0, 0.234 c 0.078, 0.113, 0.151, 0.143, 0.221, 0.091 c 0.078 -0.061, 0.078 -0.143, 0 -0.247 C 4.499, 14.981, 4.425, 14.954, 4.348, 15.015 z"/><path d="M 4.802, 15.599 c -0.078, 0.069 -0.061, 0.151, 0.052, 0.247 c 0.104, 0.104, 0.19, 0.117, 0.259, 0.039 c 0.069 -0.069, 0.052 -0.151 -0.052 -0.246 C 4.958, 15.534, 4.871, 15.521, 4.802, 15.599 z"/></g></svg>
+					<svg class="trashIcon" version="1.1" xmlns="http://www.w3.org/2000/svg" width="20px" height="20px"><g fill="white"><path d="M 18.012, 0.648 H 12.98 C 12.944, 0.284, 12.637, 0, 12.264, 0 H 8.136 c -0.373, 0 -0.68, 0.284 -0.716, 0.648 H 2.389 c -0.398, 0 -0.72, 0.322 -0.72, 0.72 v 1.368 c 0, 0.398, 0.322, 0.72, 0.72, 0.72 h 15.623 c 0.398, 0, 0.72 -0.322, 0.72 -0.72 V 1.368 C 18.731, 0.97, 18.409, 0.648, 18.012, 0.648 z"/><path d="M 3.178, 4.839 v 14.841 c 0, 0.397, 0.322, 0.72, 0.72, 0.72 h 12.604 c 0.398, 0, 0.72 -0.322, 0.72 -0.72 V 4.839 H 3.178 z M 8.449, 15.978 c 0, 0.438 -0.355, 0.794 -0.794, 0.794 c -0.438, 0 -0.794 -0.355 -0.794 -0.794 V 8.109 c 0 -0.438, 0.355 -0.794, 0.794 -0.794 c 0.438, 0, 0.794, 0.355, 0.794, 0.794 V 15.978 z M 13.538, 15.978 c 0, 0.438 -0.355, 0.794 -0.794, 0.794 s -0.794 -0.355 -0.794 -0.794 V 8.109 c 0 -0.438, 0.355 -0.794, 0.794 -0.794 c 0.438, 0, 0.794, 0.355, 0.794, 0.794 V 15.978 z"/></g></svg>
 					<button type="button" class="btn-download buttonBrandFilledDefault-2Rs6u5 buttonFilledDefault-AELjWf buttonDefault-2OLW-v button-2t3of8 buttonFilled-29g7b5 buttonBrandFilled-3Mv0Ra mediumGrow-uovsMu">
 						<div class="contentsDefault-nt2Ym5 contents-4L4hQM contentsFilled-3M8HCx contents-4L4hQM">Download</div>
 					</button>
@@ -81,19 +82,25 @@ class PluginRepo {
 								<div class="flex-lFgbSz flex-3B1Tl4 horizontal-2BEEBe horizontal-2VE-Fw flex-3B1Tl4 directionRow-yNbSvJ justifyStart-2yIZo0 alignStart-pnSyE6 noWrap-v6g9vO marginBottom20-2Ifj-2" style="flex: 0 0 auto;">
 									<h3 class="titleDefault-1CWM9y title-3i-5G_ marginReset-3hwONl weightMedium-13x9Y8 size16-3IvaX_ height24-2pMcnc flexChild-1KGW5q" style="flex: 1 1 auto;">Hide updated Plugins.</h3>
 									<div class="flexChild-1KGW5q switchEnabled-3CPlLV switch-3lyafC value-kmHGfs sizeDefault-rZbSBU size-yI1KRe themeDefault-3M0dJU" style="flex: 0 0 auto;">
-										<input type="checkbox" value="updated" class="checkboxEnabled-4QfryV checkbox-1KYsPm hide-checkbox" id="hideUpdated">
+										<input type="checkbox" value="updated" class="checkboxEnabled-4QfryV checkbox-1KYsPm hide-checkbox" id="input-hideupdated">
 									</div>
 								</div>
 								<div class="flex-lFgbSz flex-3B1Tl4 horizontal-2BEEBe horizontal-2VE-Fw flex-3B1Tl4 directionRow-yNbSvJ justifyStart-2yIZo0 alignStart-pnSyE6 noWrap-v6g9vO marginBottom20-2Ifj-2" style="flex: 0 0 auto;">
 									<h3 class="titleDefault-1CWM9y title-3i-5G_ marginReset-3hwONl weightMedium-13x9Y8 size16-3IvaX_ height24-2pMcnc flexChild-1KGW5q" style="flex: 1 1 auto;">Hide outdated Plugins.</h3>
 									<div class="flexChild-1KGW5q switchEnabled-3CPlLV switch-3lyafC value-kmHGfs sizeDefault-rZbSBU size-yI1KRe themeDefault-3M0dJU" style="flex: 0 0 auto;">
-										<input type="checkbox" value="outdated" class="checkboxEnabled-4QfryV checkbox-1KYsPm hide-checkbox" id="hideOutdated">
+										<input type="checkbox" value="outdated" class="checkboxEnabled-4QfryV checkbox-1KYsPm hide-checkbox" id="input-hideoutdated">
 									</div>
 								</div>
 								<div class="flex-lFgbSz flex-3B1Tl4 horizontal-2BEEBe horizontal-2VE-Fw flex-3B1Tl4 directionRow-yNbSvJ justifyStart-2yIZo0 alignStart-pnSyE6 noWrap-v6g9vO marginBottom20-2Ifj-2" style="flex: 0 0 auto;">
 									<h3 class="titleDefault-1CWM9y title-3i-5G_ marginReset-3hwONl weightMedium-13x9Y8 size16-3IvaX_ height24-2pMcnc flexChild-1KGW5q" style="flex: 1 1 auto;">Hide downloadable Plugins.</h3>
 									<div class="flexChild-1KGW5q switchEnabled-3CPlLV switch-3lyafC value-kmHGfs sizeDefault-rZbSBU size-yI1KRe themeDefault-3M0dJU" style="flex: 0 0 auto;">
-										<input type="checkbox" value="downloadable" class="checkboxEnabled-4QfryV checkbox-1KYsPm hide-checkbox" id="hideDownloadable">
+										<input type="checkbox" value="downloadable" class="checkboxEnabled-4QfryV checkbox-1KYsPm hide-checkbox" id="input-hidedownloadable">
+									</div>
+								</div>
+								<div id="RNMoption" class="flex-lFgbSz flex-3B1Tl4 horizontal-2BEEBe horizontal-2VE-Fw flex-3B1Tl4 directionRow-yNbSvJ justifyStart-2yIZo0 alignStart-pnSyE6 noWrap-v6g9vO marginBottom20-2Ifj-2" style="flex: 0 0 auto;">
+									<h3 class="titleDefault-1CWM9y title-3i-5G_ marginReset-3hwONl weightMedium-13x9Y8 size16-3IvaX_ height24-2pMcnc flexChild-1KGW5q" style="flex: 1 1 auto;">Start Plugin after Download (Restart-No-More needed)</h3>
+									<div class="flexChild-1KGW5q switchEnabled-3CPlLV switch-3lyafC value-kmHGfs sizeDefault-rZbSBU size-yI1KRe themeDefault-3M0dJU" style="flex: 0 0 auto;">
+										<input type="checkbox" class="checkboxEnabled-4QfryV checkbox-1KYsPm" id="input-rnmstart">
 									</div>
 								</div>
 							</div>
@@ -149,16 +156,14 @@ class PluginRepo {
 				overflow: visible !important;
 			}
 			.pluginrepo-modal .pluginEntry .gitIcon,
-			.pluginrepo-modal .pluginEntry .favIcon {
+			.pluginrepo-modal .pluginEntry .favIcon,
+			.pluginrepo-modal .pluginEntry .trashIcon {
 				position: absolute;
 				cursor: pointer;
-				top: 12px;
-			}
-			.pluginrepo-modal .pluginEntry .gitIcon {
-				right: 10px;
 			}
 			.pluginrepo-modal .pluginEntry .favIcon {
 				right: 40px;
+				top: 12px;
 			}
 			.pluginrepo-modal .pluginEntry .favIcon path {
 				stroke: yellow;
@@ -167,6 +172,19 @@ class PluginRepo {
 			.pluginrepo-modal .pluginEntry .favIcon.favorized path {
 				stroke: yellow;
 				fill: yellow;
+			}
+			.pluginrepo-modal .pluginEntry .gitIcon {
+				right: 10px;
+				top: 12px;
+			}
+			.pluginrepo-modal .pluginEntry .trashIcon {
+				right: 120px;
+				bottom: 25px;
+				display: none;
+			}
+			.pluginrepo-modal .pluginEntry.outdated .trashIcon,
+			.pluginrepo-modal .pluginEntry.updated .trashIcon {
+				display: block;
 			}
 			.pluginrepo-modal .pluginEntry .btn-download {
 				position: absolute;
@@ -185,7 +203,7 @@ class PluginRepo {
 
 	getDescription () {return "Allows you to look at all plugins from the plugin repo and download them on the fly. Repo button is in the plugins settings.";}
 
-	getVersion () {return "1.0.6";}
+	getVersion () {return "1.0.7";}
 
 	getAuthor () {return "DevilBro";}
 	
@@ -292,9 +310,15 @@ class PluginRepo {
 		pluginRepoModal.updateModal = true;
 		pluginRepoModal.enableSearch = false;
 		var hiddenSettings = BDfunctionsDevilBro.loadAllData(this.getName(), "hidden");
-		pluginRepoModal.find("#hideUpdated").prop("checked", hiddenSettings.updated);
-		pluginRepoModal.find("#hideOutdated").prop("checked", hiddenSettings.outdated);
-		pluginRepoModal.find("#hideDownloadable").prop("checked", hiddenSettings.downloadable);
+		pluginRepoModal.find("#input-hideupdated").prop("checked", hiddenSettings.updated);
+		pluginRepoModal.find("#input-hideoutdated").prop("checked", hiddenSettings.outdated);
+		pluginRepoModal.find("#input-hidedownloadable").prop("checked", hiddenSettings.downloadable);
+		if (!(window.bdplugins["Restart-No-More"] && window.pluginCookie["Restart-No-More"] || window.bdplugins["Restart No More"] && window.pluginCookie["Restart No More"])) {
+			pluginRepoModal.find("#RNMoption").remove();
+		}
+		else {
+			pluginRepoModal.find("#input-rnmstart").prop("checked", BDfunctionsDevilBro.loadData("RNMstart", this.getName(), "settings"));
+		}
 		pluginRepoModal
 			.on("keyup." + this.getName(), "#input-search", (e) => {
 				if (e.which == 13) {
@@ -308,6 +332,9 @@ class PluginRepo {
 				hiddenSettings[hideButton.val()] = hideButton.prop("checked");
 				BDfunctionsDevilBro.saveAllData(hiddenSettings, this.getName(), "hidden");
 				pluginRepoModal.updateModal = true;
+			})
+			.on("change." + this.getName(), "#input-rnmstart", (e) => {
+				BDfunctionsDevilBro.saveData("RNMstart", $(e.currentTarget).prop("checked"), this.getName(), "settings");
 			})
 			.on("click." + this.getName(), ".sort-filter", (e) => {
 				this.openSortPopout(e, this.sortPopoutMarkup, pluginRepoModal);
@@ -364,7 +391,7 @@ class PluginRepo {
 		var plugins = [];
 		for (var url in this.loadedPlugins) {
 			this.loadedPlugins[url].getFav = favorites[url] ? 0 : 1;
-			var installedPlugin = BdApi.getPlugin(this.loadedPlugins[url].getName);
+			var installedPlugin = window.bdplugins[this.loadedPlugins[url].getName] ? window.bdplugins[this.loadedPlugins[url].getName].plugin : null;
 			if (installedPlugin) {
 				if (installedPlugin.getVersion() != this.loadedPlugins[url].getVersion) {
 					this.loadedPlugins[url].getState = 1;
@@ -378,9 +405,9 @@ class PluginRepo {
 			}
 			plugins.push(this.loadedPlugins[url]);
 		}
-		if (modal.find("#hideUpdated").prop("checked")) 		plugins = plugins.filter((plugin) => {return plugin.getState != 0 ? plugin : null;});
-		if (modal.find("#hideOutdated").prop("checked")) 		plugins = plugins.filter((plugin) => {return plugin.getState != 1 ? plugin : null;});
-		if (modal.find("#hideDownloadable").prop("checked")) 	plugins = plugins.filter((plugin) => {return plugin.getState != 2 ? plugin : null;});
+		if (modal.find("#input-hideupdated").prop("checked")) 		plugins = plugins.filter((plugin) => {return plugin.getState != 0 ? plugin : null;});
+		if (modal.find("#input-hideoutdated").prop("checked")) 		plugins = plugins.filter((plugin) => {return plugin.getState != 1 ? plugin : null;});
+		if (modal.find("#input-hidedownloadable").prop("checked")) 	plugins = plugins.filter((plugin) => {return plugin.getState != 2 ? plugin : null;});
 		if (modal.enableSearch) {
 			var searchInput = modal.find("#input-search");
 			plugins = plugins.filter((plugin) => {
@@ -428,16 +455,26 @@ class PluginRepo {
 						window.open(giturl, "_blank");
 					}
 				})
+				.on("click." + this.getName(), ".trashIcon", () => {
+					entry.removeClass("outdated").removeClass("updated")
+						.find(".btn-download div").text("Download");
+					this.deletePluginFile(plugin);
+					if (!(window.bdplugins["Restart-No-More"] && window.pluginCookie["Restart-No-More"] || window.bdplugins["Restart No More"] && window.pluginCookie["Restart No More"])) stopPlugin(plugin);
+				})
 				.on("mouseenter." + this.getName(), ".favIcon", (e) => {
 					BDfunctionsDevilBro.createTooltip("Favorize", e.currentTarget, {type:"top",selector:"pluginrepo-favicon-tooltip"});
 				})
 				.on("mouseenter." + this.getName(), ".gitIcon", (e) => {
 					BDfunctionsDevilBro.createTooltip("Go to Git", e.currentTarget, {type:"top",selector:"pluginrepo-giticon-tooltip"});
 				})
+				.on("mouseenter." + this.getName(), ".trashIcon", (e) => {
+					BDfunctionsDevilBro.createTooltip("Delete Pluginfile", e.currentTarget, {type:"top",selector:"pluginrepo-trashIcon-tooltip"});
+				})
 				.on("click." + this.getName(), ".btn-download", () => {
-					this.downloadPlugin(plugin);
 					entry.removeClass("outdated").addClass("updated")
 						.find(".btn-download div").text("Updated");
+					this.downloadPlugin(plugin);
+					if (modal.find("#input-rnmstart").prop("checked")) setTimeout(() => {this.startPlugin(plugin);},3000);
 				});
 		}
 	}
@@ -456,6 +493,7 @@ class PluginRepo {
 					getPluginInfo(webview, this.grabbedPlugins, this.loadedPlugins, () => {
 						this.loading = false;
 						webview.remove();
+						console.log("PluginRepo: Finished fetching Plugins.");
 						if (document.querySelector(".bd-pluginrepobutton")) BDfunctionsDevilBro.showToast(`Finished fetching Plugins.`, {type:"success"});
 					});
 				});
@@ -574,5 +612,40 @@ class PluginRepo {
 				BDfunctionsDevilBro.showToast(`Successfully saved Plugin "${filename}".`, {type:"success"});
 			}
 		});
+	}
+	
+	startPlugin (plugin) {
+		var name = plugin.getName;
+		if (typeof window.bdplugins[name] == "object" && window.pluginCookie[name] == false) {
+			window.bdplugins[name].plugin.start();
+			window.pluginCookie[name] = true;
+			console.log("PluginRepo: started Plugin " + name);
+		}
+	}
+	
+	deletePluginFile (plugin) {
+		let fileSystem = require("fs");
+		let path = require("path");
+		let filename = plugin.url.split("/");
+		filename = filename[filename.length - 1];
+		var file = path.join(BDfunctionsDevilBro.getPluginsFolder(), filename);
+		fileSystem.unlink(file, (error) => {
+			if (error) {
+				BDfunctionsDevilBro.showToast(`Unable to delete Plugin "${filename}".`, {type:"danger"});
+			}
+			else {
+				BDfunctionsDevilBro.showToast(`Successfully deleted Plugin "${filename}".`, {type:"success"});
+			}
+		});
+	}
+	
+	stopPlugin (plugin) {
+		var name = plugin.getName;
+		if (typeof window.bdplugins[name] == "object" && window.pluginCookie[name] == true) {
+			window.bdplugins[name].plugin.stop();
+			window.pluginCookie[name] = false;
+			delete window.bdplugins[name];
+			console.log("PluginRepo: stopped Plugin " + name);
+		}
 	}
 }
