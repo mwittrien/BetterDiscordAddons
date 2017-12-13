a616d96a3d674d2199decdf69eb74a136d04b2ee
Mirco Wittrien <mirco.wittrien@gmail.com>
2017-12-13 18:30:09 +0100

    Update PluginRepo.plugin.js

--- Plugins/PluginRepo/PluginRepo.plugin.js
+++ Plugins/PluginRepo/PluginRepo.plugin.js
@@ -222,8 +222,8 @@ class PluginRepo {
 	start () {
 		if (typeof BDfunctionsDevilBro !== "object" || BDfunctionsDevilBro.isLibraryOutdated()) {
 			if (typeof BDfunctionsDevilBro === "object") BDfunctionsDevilBro = "";
-			$('head script[src="https://mwittrien.github.io/BetterDiscordAddons/Plugins/BDfunctionsDevilBroBeta.js"]').remove();
-			$('head').append('<script src="https://mwittrien.github.io/BetterDiscordAddons/Plugins/BDfunctionsDevilBroBeta.js"></script>');
+			$('head script[src="https://mwittrien.github.io/BetterDiscordAddons/Plugins/BDfunctionsDevilBro.js"]').remove();
+			$('head').append('<script src="https://mwittrien.github.io/BetterDiscordAddons/Plugins/BDfunctionsDevilBro.js"></script>');
 		}
 		if (typeof BDfunctionsDevilBro === "object") {
 			BDfunctionsDevilBro.loadMessage(this);
