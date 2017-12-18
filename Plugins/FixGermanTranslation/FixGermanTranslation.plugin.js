6bd4464274adba75742b139e4879748f200cc7a5
Mirco Wittrien <mirco.wittrien@gmail.com>
2017-12-18 18:37:53 +0100

    Update FixGermanTranslation.plugin.js

--- Plugins/FixGermanTranslation/FixGermanTranslation.plugin.js
+++ Plugins/FixGermanTranslation/FixGermanTranslation.plugin.js
@@ -2,14 +2,14 @@
 
 class FixGermanTranslation {
 	constructor () {
-		this.serverContextObserver = new MutationObserver(() => {});
+		this.oldStringTable;
 	}
 
 	getName () {return "FixGermanTranslation";}
 
-	getDescription () {return "Fixes some german translation errors.";}
+	getDescription () {return "Fixes the german translation.";}
 
-	getVersion () {return "1.1.6";}
+	getVersion () {return "2.0.0";}
 
 	getAuthor () {return "DevilBro";}
 
@@ -19,34 +19,16 @@ class FixGermanTranslation {
 	unload () {}
 
 	start () {
-		if (typeof BDfunctionsDevilBro === "object") BDfunctionsDevilBro = "";
-		$('head script[src="https://mwittrien.github.io/BetterDiscordAddons/Plugins/BDfunctionsDevilBro.js"]').remove();
-		$('head').append("<script src='https://mwittrien.github.io/BetterDiscordAddons/Plugins/BDfunctionsDevilBro.js'></script>");
-		if (typeof BDfunctionsDevilBro !== "object") {
-			$('head script[src="https://cors-anywhere.herokuapp.com/https://mwittrien.github.io/BetterDiscordAddons/Plugins/BDfunctionsDevilBro.js"]').remove();
-			$('head').append("<script src='https://cors-anywhere.herokuapp.com/https://mwittrien.github.io/BetterDiscordAddons/Plugins/BDfunctionsDevilBro.js'></script>");
+		if (typeof BDfunctionsDevilBro !== "object" || BDfunctionsDevilBro.isLibraryOutdated()) {
+			if (typeof BDfunctionsDevilBro === "object") BDfunctionsDevilBro = "";
+			$('head script[src="https://mwittrien.github.io/BetterDiscordAddons/Plugins/BDfunctionsDevilBro.js"]').remove();
+			$('head').append('<script src="https://mwittrien.github.io/BetterDiscordAddons/Plugins/BDfunctionsDevilBro.js"></script>');
 		}
 		if (typeof BDfunctionsDevilBro === "object") {
-			BDfunctionsDevilBro.loadMessage(this.getName(), this.getVersion());
+			BDfunctionsDevilBro.loadMessage(this);
 			
-			setTimeout(() => {
-				if (BDfunctionsDevilBro.getDiscordLanguage().id == "de") {
-					this.serverContextObserver = new MutationObserver((changes, _) => {
-						changes.forEach(
-							(change, i) => {
-								if (change.addedNodes) {
-									change.addedNodes.forEach((node) => {
-										if (node.nodeType == 1 && node.className.includes("context-menu")) {
-											this.onContextMenu(node);
-										}
-									});
-								}
-							}
-						);
-					});
-					if (document.querySelector(".app")) this.serverContextObserver.observe(document.querySelector(".app"), {childList: true});
-				}
-			},5000);
+			var strings = BDfunctionsDevilBro.getLanguageTable("de");
+			this.oldStringTable = Object.assign({}, strings);
 		}
 		else {
 			console.error(this.getName() + ": Fatal Error: Could not load BD functions!");
@@ -55,31 +37,10 @@ class FixGermanTranslation {
 
 	stop () {
 		if (typeof BDfunctionsDevilBro === "object") {
-			this.serverContextObserver.disconnect();
+			BDfunctionsDevilBro.unloadMessage(this);
 			
-			BDfunctionsDevilBro.unloadMessage(this.getName(), this.getVersion());
-		}
-	}
-
-	
-	// begin of own functions
-	
-	onContextMenu (context) {
-		var serverData = BDfunctionsDevilBro.getKeyInformation({"node":context, "key":"guild"});
-		var contextType = BDfunctionsDevilBro.getKeyInformation({"node":context, "key":"displayName", "value":"GuildLeaveGroup"});
-		
-		if (serverData && contextType) {
-			var allLabels = Array.from(context.getElementsByClassName("label"));
-			allLabels.forEach(
-				(label) => {
-					if (label.innerText.indexOf("Serverweit Mikrofone deaktivieren") != -1) {
-						label.innerText = "Server stummschalten";
-					}
-					if (label.innerText.indexOf("Hide Muted Channels") != -1) {
-						label.innerText = "Verstecke stumme Kanäle";
-					}
-				}
-			);
+			var strings = BDfunctionsDevilBro.getLanguageTable("de");
+			strings = this.oldStringTable;
 		}
 	}
 }
