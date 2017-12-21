c19a66d353b0768cc59da2146ca2783a112f38b2
Mirco Wittrien <mirco.wittrien@gmail.com>
2017-12-21 22:04:37 +0100

    Update CreationDate.plugin.js

--- Plugins/CreationDate/CreationDate.plugin.js
+++ Plugins/CreationDate/CreationDate.plugin.js
@@ -7,7 +7,81 @@ class CreationDate {
 		this.userPopoutObserver = new MutationObserver(() => {});
 		this.userProfilModalObserver = new MutationObserver(() => {});
 		
-		this.creationDateMarkup = `<div class="creationDate nameNormal-1LgIgN textRow-1_JoJf"></div>`
+		this.creationDateMarkup = `<div class="creationDate nameNormal-1LgIgN textRow-1_JoJf"></div>`;
+		
+		this.languages = {
+			_Discord:		{name:"Discord",					id:BDfunctionsDevilBro.getDiscordLanguage().id},
+			Arabic:			{name:"Arabic",						id:"ar"},
+			Afrikaans:		{name:"Afrikaans",					id:"af"},
+			Amharic:		{name:"Amharic",					id:"am"},
+			Armenian:		{name:"Armenian",					id:"hy"},
+			Azerbaijani:	{name:"Azerbaijani",				id:"az"},
+			Basque:			{name:"Basque",						id:"eu"},
+			Bengali:		{name:"Bengali",					id:"bn"},
+			Bulgarian:		{name:"Bulgarian",					id:"bg"},
+			Catalan:		{name:"Catalan",					id:"ca"},
+			ChineseHK:		{name:"Chinese (Hong Kong)",		id:"zh-HK"},
+			ChineseS:		{name:"Chinese (Simplified)",		id:"zh-CN"},
+			ChineseT:		{name:"Chinese (Traditional)",		id:"zh-TW"},
+			Croatian:		{name:"Croatian",					id:"hr"},
+			Czech:			{name:"Czech",						id:"cs"},
+			Danish:			{name:"Danish",						id:"da"},
+			Dutch:			{name:"Dutch",						id:"nl"},
+			EnglishUK:		{name:"English (UK)",				id:"en-GB"},
+			EnglishUS:		{name:"English (US)",				id:"en"},
+			Estonian:		{name:"Estonian",					id:"et"},
+			Filipino:		{name:"Filipino",					id:"fil"},
+			Finnish:		{name:"Finnish",					id:"fi"},
+			French:			{name:"French",						id:"fr"},
+			FrenchC:		{name:"French (Canadian)",			id:"fr-CA"},
+			Galician:		{name:"Galician",					id:"gl"},
+			Georgian:		{name:"Georgian",					id:"ka"},
+			German:			{name:"German",						id:"de"},
+			GermanA:		{name:"German (Austria)",			id:"de-AT"},
+			GermanS:		{name:"German (Switzerland)",		id:"de-CH"},
+			Greek:			{name:"Greek",						id:"el"},
+			Gujarati:		{name:"Gujarati",					id:"gu"},
+			Hebrew:			{name:"Hebrew",						id:"iw"},
+			Hindi:			{name:"Hindi",						id:"hi"},
+			Hungarain:		{name:"Hungarain",					id:"hu"},
+			Icelandic:		{name:"Icelandic",					id:"is"},
+			Indonesian:		{name:"Indonesian",					id:"id"},
+			Italian:		{name:"Italian",					id:"it"},
+			Japanese:		{name:"Japanese",					id:"ja"},
+			Kannada:		{name:"Kannada",					id:"kn"},
+			Korean:			{name:"Korean",						id:"ko"},
+			Laothian:		{name:"Laothian",					id:"lo"},
+			Latvian:		{name:"Latvian",					id:"lv"},
+			Lithuanian:		{name:"Lithuanian",					id:"lt"},
+			Malay:			{name:"Malay",						id:"ms"},
+			Malayalam:		{name:"Malayalam",					id:"ml"},
+			Marathi:		{name:"Marathi",					id:"mr"},
+			Mongolian:		{name:"Mongolian",					id:"mn"},
+			Norwegian:		{name:"Norwegian",					id:"no"},
+			Persian:		{name:"Persian",					id:"fa"},
+			Polish:			{name:"Polish",						id:"pl"},
+			Portuguese:		{name:"Portuguese",					id:"pt"},
+			PortugueseB:	{name:"Portuguese (Brazil)",		id:"pt-BR"},
+			PortugueseP:	{name:"Portuguese (Portugal)",		id:"pt-PT"},
+			Romanian:		{name:"Romanian",					id:"ro"},
+			Russian:		{name:"Russian",					id:"ru"},
+			Serbian:		{name:"Serbian",					id:"sr"},
+			Sinhalese:		{name:"Sinhalese",					id:"si"},
+			Slovak:			{name:"Slovak",						id:"sk"},
+			Slovenian:		{name:"Slovenian",					id:"sl"},
+			Spanish:		{name:"Spanish",					id:"es"},
+			SpanishL:		{name:"Spanish (Latin America)",	id:"es-419"},
+			Swahili:		{name:"Swahili",					id:"sw"},
+			Swedish:		{name:"Swedish",					id:"sv"},
+			Tamil:			{name:"Tamil",						id:"ta"},
+			Telugu:			{name:"Telugu",						id:"te"},
+			Thai:			{name:"Thai",						id:"th"},
+			Turkish:		{name:"Turkish",					id:"tr"},
+			Ukrainian:		{name:"Ukrainian",					id:"uk"},
+			Urdu:			{name:"Urdu",						id:"ur"},
+			Vietnamese:		{name:"Vietnamese",					id:"vi"},
+			Zulu:			{name:"Zulu",						id:"zu"}
+		};
 		
 		this.css = `
 			.theme-light .headerNormal-1cioxU .creationDate {
@@ -33,7 +107,9 @@ class CreationDate {
 			}
 			.theme-dark .topSectionPlaying-3jAH9b .creationDate {
 				color: hsla(0,0%,100%,.6);
-			}`;
+			}
+			
+			.CreationDate-settings div {margin-top:0 !important;}`;
 			
 			
 		this.defaultSettings = {
@@ -46,7 +122,7 @@ class CreationDate {
 
 	getDescription () {return "Displays the Creation Date of an Account in the UserPopout and UserModal.";}
 
-	getVersion () {return "1.0.2";}
+	getVersion () {return "1.0.3";}
 
 	getAuthor () {return "DevilBro";}
 	
@@ -58,11 +134,14 @@ class CreationDate {
 				settingshtml += `<div class="flex-lFgbSz flex-3B1Tl4 horizontal-2BEEBe horizontal-2VE-Fw flex-3B1Tl4 directionRow-yNbSvJ justifyStart-2yIZo0 alignStart-pnSyE6 noWrap-v6g9vO marginBottom8-1mABJ4" style="flex: 1 1 auto; margin-top: 0;"><h3 class="titleDefault-1CWM9y title-3i-5G_ marginReset-3hwONl weightMedium-13x9Y8 size16-3IvaX_ height24-2pMcnc flexChild-1KGW5q" style="flex: 1 1 auto;">${this.defaultSettings[key].description}</h3><div class="flexChild-1KGW5q switchEnabled-3CPlLV switch-3lyafC value-kmHGfs sizeDefault-rZbSBU size-yI1KRe themeDefault-3M0dJU ${settings[key] ? "valueChecked-3Bzkbm" : "valueUnchecked-XR6AOk"}" style="flex: 0 0 auto;"><input type="checkbox" value="${key}" class="checkboxEnabled-4QfryV checkbox-1KYsPm"${settings[key] ? " checked" : ""}></div></div>`;
 			}
 			settingshtml += `</div>`;
+			var language = BDfunctionsDevilBro.loadData("language", this.getName(), "language") || "_Discord";
+			settingshtml += `<div class="ui-form-item flexChild-1KGW5q" style="flex: 1 1 20%;"><h3 class="titleDefault-1CWM9y title-3i-5G_ marginBottom8-1mABJ4 weightMedium-13x9Y8 size16-3IvaX_ height24-2pMcnc flexChild-1KGW5q" style="flex: 1 1 auto;">Creation Date Format:</h3><div class="ui-select format-select-wrapper"><div value="${language}" class="Select Select--single has-value"><div class="Select-control"><div class="flex-lFgbSz flex-3B1Tl4 horizontal-2BEEBe horizontal-2VE-Fw flex-3B1Tl4 directionRow-yNbSvJ justifyStart-2yIZo0 alignBaseline-4enZzv noWrap-v6g9vO wrapper-1v8p8a Select-value" style="flex: 1 1 auto;"><div class="title-3I2bY1 medium-2KnC-N size16-3IvaX_ height20-165WbF primary-2giqSn weightNormal-3gw0Lm languageName" style="flex: 1 1 50%;">${this.languages[language].name}</div><div class="title-3I2bY1 medium-2KnC-N size16-3IvaX_ height20-165WbF primary-2giqSn weightNormal-3gw0Lm languageTimestamp" style="flex: 1 1 50%;">${new Date().toLocaleString(this.languages[language].id)}</div></div><span class="Select-arrow-zone"><span class="Select-arrow"></span></span></div></div></div></div>`;
 			settingshtml += `</div>`;
 			
 			var settingspanel = $(settingshtml)[0];
 			$(settingspanel)
-				.on("click", ".checkbox-1KYsPm", () => {this.updateSettings(settingspanel);});
+				.on("click", ".checkbox-1KYsPm", () => {this.updateSettings(settingspanel);})
+				.on("click", ".Select-control", (e) => {this.openDropdownMenu(settingspanel, e);});
 			return settingspanel;
 		}
 	}
@@ -156,13 +235,50 @@ class CreationDate {
 		BDfunctionsDevilBro.saveAllData(settings, this.getName(), "settings");
 	}
 	
+	openDropdownMenu (settingspanel, e) {
+		var selectControl = e.currentTarget;
+		var selectWrap = selectControl.parentElement;
+		
+		if (selectWrap.classList.contains("is-open")) return;
+		
+		selectWrap.classList.add("is-open");
+		$("li").has(settingspanel).css("overflow", "visible");
+		
+		var selectMenu = this.createDropdownMenu(selectWrap.getAttribute("value"));
+		selectWrap.appendChild(selectMenu);
+		
+		$(selectMenu).on("mousedown." + this.getName(), ".Select-option", (e2) => {
+			var language = e2.currentTarget.getAttribute("value");
+			selectWrap.setAttribute("value", language);
+			selectControl.querySelector(".languageName").innerText = this.languages[language].name;
+			selectControl.querySelector(".languageTimestamp").innerText = new Date().toLocaleString(this.languages[language].id);
+			BDfunctionsDevilBro.saveData("language", language, this.getName(), "language");
+		});
+		$(document).on("mousedown.select" + this.getName(), () => {
+			$(document).off("mousedown.select" + this.getName());
+			selectMenu.remove()
+			$("li").has(settingspanel).css("overflow", "auto");
+			setTimeout(() => {selectWrap.classList.remove("is-open");},100);
+		});
+	}
+	
+	createDropdownMenu (choice) {
+		var menuhtml = `<div class="Select-menu-outer"><div class="Select-menu">`;
+		for (var language in this.languages) {
+			var isSelected = language == choice ? " is-selected" : "";
+			menuhtml += `<div value="${language}" class="flex-lFgbSz flex-3B1Tl4 horizontal-2BEEBe horizontal-2VE-Fw flex-3B1Tl4 directionRow-yNbSvJ justifyStart-2yIZo0 alignBaseline-4enZzv noWrap-v6g9vO wrapper-1v8p8a Select-option ${isSelected}" style="flex: 1 1 auto; display:flex;"><div class="title-3I2bY1 medium-2KnC-N size16-3IvaX_ height20-165WbF primary-2giqSn weightNormal-3gw0Lm languageName" style="flex: 1 1 50%;">${this.languages[language].name}</div><div class="title-3I2bY1 medium-2KnC-N size16-3IvaX_ height20-165WbF primary-2giqSn weightNormal-3gw0Lm languageTimestamp" style="flex: 1 1 50%;">${new Date().toLocaleString(this.languages[language].id)}</div></div>`
+		}
+		menuhtml += `</div></div>`;
+		return $(menuhtml)[0];
+	}
+	
 	addCreationDate (container) {
 		if (!container) return;
 		var info = BDfunctionsDevilBro.getKeyInformation({"node":container,"key":"user"});
 		if (info) {
 			var creationDate = $(this.creationDateMarkup);
-			var language = BDfunctionsDevilBro.getDiscordLanguage().id;
-			creationDate.text(this.labels.createdat_text + " " + info.createdAt.toLocaleString(language)).appendTo(container);
+			var language = BDfunctionsDevilBro.loadData("language", this.getName(), "language") || "_Discord";
+			creationDate.text(this.labels.createdat_text + " " + info.createdAt.toLocaleString(this.languages[language].id)).appendTo(container);
 		}
 	}
 	
