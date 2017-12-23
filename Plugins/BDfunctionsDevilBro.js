fc2a41f0a4f278d15169742bc21dd0811280f0fa
Mirco Wittrien <mirco.wittrien@gmail.com>
2017-12-23 10:03:37 +0100

    Update BDfunctionsDevilBro.js

--- Plugins/BDfunctionsDevilBro.js
+++ Plugins/BDfunctionsDevilBro.js
@@ -31,7 +31,7 @@ BDfunctionsDevilBro.loadMessage = function (plugin, oldVersionRemove) {
 		},7200000);
 	}
 	
-	if (typeof window.PluginUpdates.observer === "undefined" && document.querySelector(".layers")) {
+	if (typeof window.PluginUpdates.observer === "undefined" && document.querySelector(".app")) {
 		window.PluginUpdates.observer = new MutationObserver((changes, _) => {
 			changes.forEach(
 				(change, i) => {
@@ -58,7 +58,7 @@ BDfunctionsDevilBro.loadMessage = function (plugin, oldVersionRemove) {
 				}
 			);
 		});
-		window.PluginUpdates.observer.observe(document.querySelector(".layers"), {childList:true});
+		window.PluginUpdates.observer.observe(document.querySelector(".app"), {childList:true, subtree:true});
 	}
 	
 	var bdbutton = document.querySelector(".bd-pfbtn");
