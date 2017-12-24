b9eaaa765dab81161e745f38c569457a8f88b047
Mirco Wittrien <mirco.wittrien@gmail.com>
2017-12-24 15:32:26 +0100

    Update WriteUpperCase.plugin.js

--- Plugins/WriteUpperCase/WriteUpperCase.plugin.js
+++ Plugins/WriteUpperCase/WriteUpperCase.plugin.js
@@ -8,7 +8,7 @@ class WriteUpperCase {
 
 	getDescription () {return "Change input to uppercase.";}
 
-	getVersion () {return "1.0.7";}
+	getVersion () {return "1.0.8";}
 
 	getAuthor () {return "DevilBro";}
 
@@ -23,7 +23,15 @@ class WriteUpperCase {
 		if (typeof BDfunctionsDevilBro === "object") {
 			BDfunctionsDevilBro.loadMessage(this);
 			
-			this.bindEventToTextArea();
+			this.TextArea = BDfunctionsDevilBro.WebModules.findByPrototypes(["saveCurrentText"]);
+			this.patchCancel = BDfunctionsDevilBro.WebModules.monkeyPatch(this.TextArea.prototype, "componentDidMount", {after: (e) => {
+				if (e && e.thisObject && e.thisObject._ref && e.thisObject._ref._textArea && e.thisObject.props && e.thisObject.props.type) {
+					this.bindEventToTextArea(e.thisObject._ref._textArea, e.thisObject.props.type);
+				}
+			}});
+			
+			
+			this.bindEventToTextArea(document.querySelector("form .channelTextArea-os01xC textarea"), "normal");
 		}
 		else {
 			console.error(this.getName() + ": Fatal Error: Could not load BD functions!");
@@ -32,42 +40,43 @@ class WriteUpperCase {
 
 	stop () {
 		if (typeof BDfunctionsDevilBro === "object") {
-			$(".channelTextArea-1HTP3C").find("textarea").off("keyup." + this.getName());
+			if (typeof this.patchCancel === "function") this.patchCancel();
+			
+			$("textarea").off("keyup." + this.getName());
 			
 			BDfunctionsDevilBro.unloadMessage(this);
-		}
-	}
-	
-	onSwitch () {
-		if (typeof BDfunctionsDevilBro === "object") {
-			this.bindEventToTextArea();
 		}
 	}
 
 	
 	// begin of own functions
 	
-	bindEventToTextArea () {
-		$(".channelTextArea-1HTP3C").find("textarea")
-			.off("keyup." + this.getName())
-			.on("keyup." + this.getName(), e => {
-				var textarea = e.target;
-				var string = textarea.value;
-				if (string.length > 0) {
-					var textareaInstance = BDfunctionsDevilBro.getOwnerInstance({"node":textarea, "name":"ChannelTextAreaForm", "up":true});
-					var first = string.charAt(0);
-					var position = e.target.selectionStart;
-					if (first === first.toUpperCase() && string.toLowerCase().indexOf("http") == 0) {
-						textareaInstance.setState({textValue:string.charAt(0).toLowerCase() + string.slice(1)});
-						textarea.selectionStart = position;
-						textarea.selectionEnd = position;
+	bindEventToTextArea (textarea, type) {
+		if (!textarea || !type) return;
+		var textareaInstance = BDfunctionsDevilBro.getOwnerInstance({"node":textarea, "props":["handlePaste","saveCurrentText"], "up":true});
+		if (textarea && textareaInstance) {
+			$(textarea)
+				.off("keyup." + this.getName())
+				.on("keyup." + this.getName(), () => {
+					var string = textarea.value;
+					if (string.length > 0) {
+						var newstring = string;
+						var first = string.charAt(0);
+						var position = textarea.selectionStart;
+						if (first === first.toUpperCase() && string.toLowerCase().indexOf("http") == 0) {
+							newstring = string.charAt(0).toLowerCase() + string.slice(1);
+						}
+						else if (first === first.toLowerCase() && first !== first.toUpperCase() && string.toLowerCase().indexOf("http") != 0) {
+							newstring = string.charAt(0).toUpperCase() + string.slice(1);
+						}
+						if (string != newstring) {
+							textarea.value = newstring;
+							textareaInstance.saveCurrentText();
+							textarea.selectionStart = position;
+							textarea.selectionEnd = position;
+						}
 					}
-					else if (first === first.toLowerCase() && first !== first.toUpperCase() && string.toLowerCase().indexOf("http") != 0) {
-						textareaInstance.setState({textValue:string.charAt(0).toUpperCase() + string.slice(1)});
-						textarea.selectionStart = position;
-						textarea.selectionEnd = position;
-					}
-				}
-			});
+				});
+		}
 	}
 }
