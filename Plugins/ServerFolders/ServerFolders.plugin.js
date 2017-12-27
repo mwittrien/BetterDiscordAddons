8701b98b83efc0fcffed7ab31ab490df46ff8221
Alex Haber <AlexHaber@users.noreply.github.com>
2017-12-27 19:32:14 +1100

    Fix ServerFolders description

--- Plugins/ServerFolders/ServerFolders.plugin.js
+++ Plugins/ServerFolders/ServerFolders.plugin.js
@@ -247,7 +247,7 @@ class ServerFolders {
 		
 	getName () {return "ServerFolders";}
 
-	getDescription () {return "Adds the feature to create folders to organize your servers. Right click a server > 'Serverfolders' > 'Create Server' to create a server. To add servers to a folder hold 'Ctrl' and drag the server onto the folder, this will add the server to the folderlist and hide it in the serverlist. To open a folder click the folder. A folder can only be opened when it has at least one server in it. To remove a server from a folder, open the folder and either right click the server > 'Serverfolders' > 'Remove Server from Folder' or hold 'Del' and click the server in the folderlist.";}
+	getDescription () {return "Adds the feature to create folders to organize your servers. Right click a server > 'Serverfolders' > 'Create Folder' to create a folder. To add servers to a folder hold 'Ctrl' and drag the server onto the folder, this will add the server to the folderlist and hide it in the serverlist. To open a folder click the folder. A folder can only be opened when it has at least one server in it. To remove a server from a folder, open the folder and either right click the server > 'Serverfolders' > 'Remove Server from Folder' or hold 'Del' and click the server in the folderlist.";}
 
 	getVersion () {return "5.4.4";}
 
