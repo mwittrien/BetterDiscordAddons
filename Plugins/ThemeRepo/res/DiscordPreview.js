20a904c3c8e8d5bfb0f36e51af0e1972f44f8950
Mirco Wittrien <mirco.wittrien@gmail.com>
2018-02-02 12:00:15 +0100

    Update DiscordPreview.js

--- Plugins/ThemeRepo/res/DiscordPreview.js
+++ Plugins/ThemeRepo/res/DiscordPreview.js
@@ -9,9 +9,17 @@ window.onmessage = function (e) {
 		var body = document.querySelector("body"), head = document.querySelector("head"), theme;
 		switch (e.data.reason) {
 			case "OnLoad":
+				head.appendChild(theme);
 				body.innerHTML = body.innerHTML.replace(new RegExp("REPLACE_USERNAME", "g"), e.data.username);
 				body.innerHTML = body.innerHTML.replace(new RegExp("REPLACE_AVATAR", "g"), e.data.avatar.split('"').join(''));
 				body.innerHTML = body.innerHTML.replace(new RegExp("REPLACE_DISCRIMINATOR", "g"), e.data.discriminator);
+				if (e.data.nativecss) {
+					theme = document.createElement("link");
+					theme.classList.add(e.data.reason);
+					theme.rel = "stylesheet";
+					theme.href = e.data.nativecss;
+					head.appendChild(theme);
+				}
 				break;
 			case "NewTheme":
 			case "CustomCSS":
