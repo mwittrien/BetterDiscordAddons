9d7a4090f58c69b1e17080e3821db655a5b44ee1
Mirco Wittrien <mirco.wittrien@gmail.com>
2018-03-08 09:06:16 +0100

    Update ReadAllNotificationsButton.plugin.js

--- Plugins/ReadAllNotificationsButton/ReadAllNotificationsButton.plugin.js
+++ Plugins/ReadAllNotificationsButton/ReadAllNotificationsButton.plugin.js
@@ -41,6 +41,7 @@ class ReadAllNotificationsButton {
 		}
 		if (typeof BDfunctionsDevilBro === "object") this.initialize();
 		else libraryScript.addEventListener("load", () => {this.initialize();});
+		this.startTimeout = setTimeout(() => {this.initialize();}, 30000);
 	}
 
 	initialize () {
