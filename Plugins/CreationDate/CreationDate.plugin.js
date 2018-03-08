f45a146582aa39229d4da49c18f3a0a5b84ed772
Mirco Wittrien <mirco.wittrien@gmail.com>
2018-03-08 09:03:10 +0100

    Update CreationDate.plugin.js

--- Plugins/CreationDate/CreationDate.plugin.js
+++ Plugins/CreationDate/CreationDate.plugin.js
@@ -91,6 +91,7 @@ class CreationDate {
 		}
 		if (typeof BDfunctionsDevilBro === "object") this.initialize();
 		else libraryScript.addEventListener("load", () => {this.initialize();});
+		this.startTimeout = setTimeout(() => {this.initialize();}, 30000);
 	}
 
 	initialize () {
