cbca3611606bcea806052a78d2993ae9cb27f401
Mirco Wittrien <mirco.wittrien@gmail.com>
2018-03-08 09:17:05 +0100

    Update ImageGallery.plugin.js

--- Plugins/ImageGallery/ImageGallery.plugin.js
+++ Plugins/ImageGallery/ImageGallery.plugin.js
@@ -43,9 +43,9 @@ class ImageGallery {
 			libraryScript.setAttribute("src", "https://mwittrien.github.io/BetterDiscordAddons/Plugins/BDfunctionsDevilBro.js");
 			document.head.appendChild(libraryScript);
 		}
+		this.startTimeout = setTimeout(() => {this.initialize();}, 30000);
 		if (typeof BDfunctionsDevilBro === "object") this.initialize();
 		else libraryScript.addEventListener("load", () => {this.initialize();});
-		this.startTimeout = setTimeout(() => {this.initialize();}, 30000);
 	}
 
 	initialize () {
