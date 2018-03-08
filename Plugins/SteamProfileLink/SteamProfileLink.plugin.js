c4ded35fdf289ae59bc3d3887084bc4ed05a4529
Mirco Wittrien <mirco.wittrien@gmail.com>
2018-03-08 09:20:02 +0100

    Update SteamProfileLink.plugin.js

--- Plugins/SteamProfileLink/SteamProfileLink.plugin.js
+++ Plugins/SteamProfileLink/SteamProfileLink.plugin.js
@@ -26,9 +26,9 @@ class SteamProfileLink {
 			libraryScript.setAttribute("src", "https://mwittrien.github.io/BetterDiscordAddons/Plugins/BDfunctionsDevilBro.js");
 			document.head.appendChild(libraryScript);
 		}
+		this.startTimeout = setTimeout(() => {this.initialize();}, 30000);
 		if (typeof BDfunctionsDevilBro === "object") this.initialize();
 		else libraryScript.addEventListener("load", () => {this.initialize();});
-		this.startTimeout = setTimeout(() => {this.initialize();}, 30000);
 	}
 
 	initialize () {
