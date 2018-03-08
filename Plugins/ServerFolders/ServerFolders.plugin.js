72db697bb97a5dda09c80bc47f3915ae8383eab7
Mirco Wittrien <mirco.wittrien@gmail.com>
2018-03-08 09:06:59 +0100

    Update ServerFolders.plugin.js

--- Plugins/ServerFolders/ServerFolders.plugin.js
+++ Plugins/ServerFolders/ServerFolders.plugin.js
@@ -368,6 +368,7 @@ class ServerFolders {
 		}
 		if (typeof BDfunctionsDevilBro === "object") this.initialize();
 		else libraryScript.addEventListener("load", () => {this.initialize();});
+		this.startTimeout = setTimeout(() => {this.initialize();}, 30000);
 	}
 
 	initialize () {
