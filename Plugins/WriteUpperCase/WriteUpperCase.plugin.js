f8d0d00889eea76899fdd450a647b88e238b93db
Mirco Wittrien <mirco.wittrien@gmail.com>
2018-03-08 09:20:43 +0100

    Update WriteUpperCase.plugin.js

--- Plugins/WriteUpperCase/WriteUpperCase.plugin.js
+++ Plugins/WriteUpperCase/WriteUpperCase.plugin.js
@@ -25,9 +25,9 @@ class WriteUpperCase {
 			libraryScript.setAttribute("src", "https://mwittrien.github.io/BetterDiscordAddons/Plugins/BDfunctionsDevilBro.js");
 			document.head.appendChild(libraryScript);
 		}
+		this.startTimeout = setTimeout(() => {this.initialize();}, 30000);
 		if (typeof BDfunctionsDevilBro === "object") this.initialize();
 		else libraryScript.addEventListener("load", () => {this.initialize();});
-		this.startTimeout = setTimeout(() => {this.initialize();}, 30000);
 	}
 
 	initialize () {
