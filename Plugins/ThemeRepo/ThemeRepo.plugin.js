076656888b45496a4b922da7d3c48b73c2317d63
Mirco Wittrien <mirco.wittrien@gmail.com>
2018-03-08 09:07:39 +0100

    Update ThemeRepo.plugin.js

--- Plugins/ThemeRepo/ThemeRepo.plugin.js
+++ Plugins/ThemeRepo/ThemeRepo.plugin.js
@@ -343,6 +343,7 @@ class ThemeRepo {
 		}
 		if (typeof BDfunctionsDevilBro === "object") this.initialize();
 		else libraryScript.addEventListener("load", () => {this.initialize();});
+		this.startTimeout = setTimeout(() => {this.initialize();}, 30000);
 	}
 
 	initialize () {
