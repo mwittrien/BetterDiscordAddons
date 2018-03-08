3add548f33ad8629b44a35f74b690b24b8b2d598
Mirco Wittrien <mirco.wittrien@gmail.com>
2018-03-08 09:07:16 +0100

    Update ShowImageDetails.plugin.js

--- Plugins/ShowImageDetails/ShowImageDetails.plugin.js
+++ Plugins/ShowImageDetails/ShowImageDetails.plugin.js
@@ -65,6 +65,7 @@ class ShowImageDetails {
 		}
 		if (typeof BDfunctionsDevilBro === "object") this.initialize();
 		else libraryScript.addEventListener("load", () => {this.initialize();});
+		this.startTimeout = setTimeout(() => {this.initialize();}, 30000);
 	}
 
 	initialize () {
