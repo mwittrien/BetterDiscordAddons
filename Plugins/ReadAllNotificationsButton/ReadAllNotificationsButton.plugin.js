ade27bee505ca1aa5d3f657aaa0b21330191ffd6
Mirco Wittrien <mirco.wittrien@gmail.com>
2018-03-08 09:18:02 +0100

    Update ReadAllNotificationsButton.plugin.js

--- Plugins/ReadAllNotificationsButton/ReadAllNotificationsButton.plugin.js
+++ Plugins/ReadAllNotificationsButton/ReadAllNotificationsButton.plugin.js
@@ -39,9 +39,9 @@ class ReadAllNotificationsButton {
 			libraryScript.setAttribute("src", "https://mwittrien.github.io/BetterDiscordAddons/Plugins/BDfunctionsDevilBro.js");
 			document.head.appendChild(libraryScript);
 		}
+		this.startTimeout = setTimeout(() => {this.initialize();}, 30000);
 		if (typeof BDfunctionsDevilBro === "object") this.initialize();
 		else libraryScript.addEventListener("load", () => {this.initialize();});
-		this.startTimeout = setTimeout(() => {this.initialize();}, 30000);
 	}
 
 	initialize () {
