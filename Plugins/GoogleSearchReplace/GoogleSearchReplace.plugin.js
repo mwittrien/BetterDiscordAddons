098ba10af57a6976e02cacf5405fac37071788e9
Mirco Wittrien <mirco.wittrien@gmail.com>
2018-03-08 09:05:20 +0100

    Update GoogleSearchReplace.plugin.js

--- Plugins/GoogleSearchReplace/GoogleSearchReplace.plugin.js
+++ Plugins/GoogleSearchReplace/GoogleSearchReplace.plugin.js
@@ -87,6 +87,7 @@ class GoogleSearchReplace {
 		}
 		if (typeof BDfunctionsDevilBro === "object") this.initialize();
 		else libraryScript.addEventListener("load", () => {this.initialize();});
+		this.startTimeout = setTimeout(() => {this.initialize();}, 30000);
 	}
 
 	initialize () {
