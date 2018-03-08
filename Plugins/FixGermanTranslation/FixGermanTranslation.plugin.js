f1d16598e70bd250580563e0cfe7223460b09299
Mirco Wittrien <mirco.wittrien@gmail.com>
2018-03-08 09:16:38 +0100

    Update FixGermanTranslation.plugin.js

--- Plugins/FixGermanTranslation/FixGermanTranslation.plugin.js
+++ Plugins/FixGermanTranslation/FixGermanTranslation.plugin.js
@@ -219,9 +219,9 @@ class FixGermanTranslation {
 			libraryScript.setAttribute("src", "https://mwittrien.github.io/BetterDiscordAddons/Plugins/BDfunctionsDevilBro.js");
 			document.head.appendChild(libraryScript);
 		}
+		this.startTimeout = setTimeout(() => {this.initialize();}, 30000);
 		if (typeof BDfunctionsDevilBro === "object") this.initialize();
 		else libraryScript.addEventListener("load", () => {this.initialize();});
-		this.startTimeout = setTimeout(() => {this.initialize();}, 30000);
 	}
 
 	initialize () {
