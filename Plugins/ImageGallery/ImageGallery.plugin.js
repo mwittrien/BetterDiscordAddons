b5ba7ae0262ea35fdf385f3b114c5fa7ad9be3fa
Mirco Wittrien <mirco.wittrien@gmail.com>
2018-03-08 09:05:33 +0100

    Update ImageGallery.plugin.js

--- Plugins/ImageGallery/ImageGallery.plugin.js
+++ Plugins/ImageGallery/ImageGallery.plugin.js
@@ -45,6 +45,7 @@ class ImageGallery {
 		}
 		if (typeof BDfunctionsDevilBro === "object") this.initialize();
 		else libraryScript.addEventListener("load", () => {this.initialize();});
+		this.startTimeout = setTimeout(() => {this.initialize();}, 30000);
 	}
 
 	initialize () {
