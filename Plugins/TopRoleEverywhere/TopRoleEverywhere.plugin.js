d83b414ddc919dc94bdc557bb4f327e44abe53e0
Mirco Wittrien <mirco.wittrien@gmail.com>
2018-03-08 09:20:32 +0100

    Update TopRoleEverywhere.plugin.js

--- Plugins/TopRoleEverywhere/TopRoleEverywhere.plugin.js
+++ Plugins/TopRoleEverywhere/TopRoleEverywhere.plugin.js
@@ -79,9 +79,9 @@ class TopRoleEverywhere {
 			libraryScript.setAttribute("src", "https://mwittrien.github.io/BetterDiscordAddons/Plugins/BDfunctionsDevilBro.js");
 			document.head.appendChild(libraryScript);
 		}
+		this.startTimeout = setTimeout(() => {this.initialize();}, 30000);
 		if (typeof BDfunctionsDevilBro === "object") this.initialize();
 		else libraryScript.addEventListener("load", () => {this.initialize();});
-		this.startTimeout = setTimeout(() => {this.initialize();}, 30000);
 	}
 
 	initialize () {
