f05ac2b501173d66304f53ac2b950e1d185a015d
Mirco Wittrien <mirco.wittrien@gmail.com>
2018-03-08 09:03:51 +0100

    Update DisplayServersAsChannels.plugin.js

--- Plugins/DisplayServersAsChannels/DisplayServersAsChannels.plugin.js
+++ Plugins/DisplayServersAsChannels/DisplayServersAsChannels.plugin.js
@@ -173,6 +173,7 @@ class DisplayServersAsChannels {
 		}
 		if (typeof BDfunctionsDevilBro === "object") this.initialize();
 		else libraryScript.addEventListener("load", () => {this.initialize();});
+		this.startTimeout = setTimeout(() => {this.initialize();}, 30000);
 	}
 
 	initialize () {
