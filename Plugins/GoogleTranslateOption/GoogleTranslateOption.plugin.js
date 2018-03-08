fea9f67c5b7c0d97cf10fef66a5572381c7d5fa6
Mirco Wittrien <mirco.wittrien@gmail.com>
2018-03-08 09:05:26 +0100

    Update GoogleTranslateOption.plugin.js

--- Plugins/GoogleTranslateOption/GoogleTranslateOption.plugin.js
+++ Plugins/GoogleTranslateOption/GoogleTranslateOption.plugin.js
@@ -403,6 +403,7 @@ class GoogleTranslateOption {
 		}
 		if (typeof BDfunctionsDevilBro === "object") this.initialize();
 		else libraryScript.addEventListener("load", () => {this.initialize();});
+		this.startTimeout = setTimeout(() => {this.initialize();}, 30000);
 	}
 
 	initialize () {
