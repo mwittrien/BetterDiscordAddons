35ec8566c629caf4dda6e2b20c1e32aa92545f69
Mirco Wittrien <mirco.wittrien@gmail.com>
2018-03-08 09:05:38 +0100

    Update MessageUtilities.plugin.js

--- Plugins/MessageUtilities/MessageUtilities.plugin.js
+++ Plugins/MessageUtilities/MessageUtilities.plugin.js
@@ -102,6 +102,7 @@ class MessageUtilities {
 		}
 		if (typeof BDfunctionsDevilBro === "object") this.initialize();
 		else libraryScript.addEventListener("load", () => {this.initialize();});
+		this.startTimeout = setTimeout(() => {this.initialize();}, 30000);
 	}
 
 	initialize () {
