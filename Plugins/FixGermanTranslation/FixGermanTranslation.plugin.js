e4e1cd33373ae858613057a51fc9bc5b8c51f532
Mirco Wittrien <mirco.wittrien@gmail.com>
2018-03-08 09:05:08 +0100

    Update FixGermanTranslation.plugin.js

--- Plugins/FixGermanTranslation/FixGermanTranslation.plugin.js
+++ Plugins/FixGermanTranslation/FixGermanTranslation.plugin.js
@@ -221,6 +221,7 @@ class FixGermanTranslation {
 		}
 		if (typeof BDfunctionsDevilBro === "object") this.initialize();
 		else libraryScript.addEventListener("load", () => {this.initialize();});
+		this.startTimeout = setTimeout(() => {this.initialize();}, 30000);
 	}
 
 	initialize () {
