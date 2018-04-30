e5dcc3da5de0f63b927817a75f856d6e867609d7
Mirco Wittrien <mirco.wittrien@gmail.com>
2018-04-30 12:11:43 +0200

    ForceImagePreviews fixed

--- Plugins/ForceImagePreviews/ForceImagePreviews.plugin.js
+++ Plugins/ForceImagePreviews/ForceImagePreviews.plugin.js
@@ -1,7 +1,7 @@
 //META{"name":"ForceImagePreviews"}*//
 
 class ForceImagePreviews {
-	constructor () {
+	initConstructor () {
 		this.waitTime = 3000;
 	}
 
@@ -9,12 +9,12 @@ class ForceImagePreviews {
 
 	getDescription () {return "Forces embedded Image Previews, if Discord doesn't do it itself.";}
 
-	getVersion () {return "1.0.2";}
+	getVersion () {return "1.0.3";}
 
 	getAuthor () {return "DevilBro";}
 	
 	getSettingsPanel () {
-		if (!this.started || typeof BDfunctionsDevilBro !== "object") return;
+		if (!this.started || typeof BDFDB !== "object") return;
 	}
 
 	//legacy
@@ -22,23 +22,23 @@ class ForceImagePreviews {
 
 	start () {
 		var libraryScript = null;
-		if (typeof BDfunctionsDevilBro !== "object" || BDfunctionsDevilBro.isLibraryOutdated()) {
-			if (typeof BDfunctionsDevilBro === "object") BDfunctionsDevilBro = "";
-			libraryScript = document.querySelector('head script[src="https://mwittrien.github.io/BetterDiscordAddons/Plugins/BDfunctionsDevilBro.js"]');
+		if (typeof BDFDB !== "object" || BDFDB.isLibraryOutdated()) {
+			if (typeof BDFDB === "object") BDFDB = "";
+			libraryScript = document.querySelector('head script[src="https://mwittrien.github.io/BetterDiscordAddons/Plugins/BDFDB.js"]');
 			if (libraryScript) libraryScript.remove();
 			libraryScript = document.createElement("script");
 			libraryScript.setAttribute("type", "text/javascript");
-			libraryScript.setAttribute("src", "https://mwittrien.github.io/BetterDiscordAddons/Plugins/BDfunctionsDevilBro.js");
+			libraryScript.setAttribute("src", "https://mwittrien.github.io/BetterDiscordAddons/Plugins/BDFDB.js");
 			document.head.appendChild(libraryScript);
 		}
 		this.startTimeout = setTimeout(() => {this.initialize();}, 30000);
-		if (typeof BDfunctionsDevilBro === "object") this.initialize();
+		if (typeof BDFDB === "object") this.initialize();
 		else libraryScript.addEventListener("load", () => {this.initialize();});
 	}
 
 	initialize () {
-		if (typeof BDfunctionsDevilBro === "object") {
-			BDfunctionsDevilBro.loadMessage(this);
+		if (typeof BDFDB === "object") {
+			BDFDB.loadMessage(this);
 			
 			var observer = null;
 
@@ -50,22 +50,22 @@ class ForceImagePreviews {
 						}
 						if (change.addedNodes) {
 							change.addedNodes.forEach((node) => {
-								if ($(node).attr("class") == "message") setTimeout(() => {this.addPreviews($(node).find(".markup")[0]);},this.waitTime);
+								if ($(node).attr("class") == BDFDB.disCN.message) setTimeout(() => {this.addPreviews($(node).find(BDFDB.dotCN.messagemarkup)[0]);},this.waitTime);
 							});
 						}
 					}
 				);
 			});
-			BDfunctionsDevilBro.addObserver(this, null, {name:"messageChangeObserver",instance:observer,multi:true}, {childList:true, characterData:true, subtree:true});
+			BDFDB.addObserver(this, null, {name:"messageChangeObserver",instance:observer,multi:true}, {childList:true, characterData:true, subtree:true});
 			
 			observer = new MutationObserver((changes, _) => {
 				changes.forEach(
 					(change, i) => {
 						if (change.addedNodes) {
 							change.addedNodes.forEach((node) => {
-								if (node && node.tagName && node.querySelector(".message")) {
-									BDfunctionsDevilBro.addObserver(this, node, {name:"messageChangeObserver",multi:true}, {childList:true, characterData:true, subtree:true});
-									node.querySelectorAll(".markup").forEach(message => {
+								if (node && node.tagName && node.querySelector(BDFDB.dotCN.message)) {
+									BDFDB.addObserver(this, node, {name:"messageChangeObserver",multi:true}, {childList:true, characterData:true, subtree:true});
+									node.querySelectorAll(BDFDB.dotCN.messagemarkup).forEach(message => {
 										setTimeout(() => {this.addPreviews(message);},this.waitTime);
 									});
 								}
@@ -74,7 +74,7 @@ class ForceImagePreviews {
 					}
 				);
 			});
-			BDfunctionsDevilBro.addObserver(this, ".messages.scroller", {name:"chatWindowObserver",instance:observer}, {childList:true});
+			BDFDB.addObserver(this, BDFDB.dotCN.messages, {name:"chatWindowObserver",instance:observer}, {childList:true});
 			
 			this.addAllPreviews();
 		}
@@ -84,27 +84,28 @@ class ForceImagePreviews {
 	}
 
 	stop () {
-		if (typeof BDfunctionsDevilBro === "object") {
+		if (typeof BDFDB === "object") {
 			document.querySelectorAll(".FIP-embed").forEach(embed => {embed.remove();});
 			
-			BDfunctionsDevilBro.unloadMessage(this);
+			BDFDB.unloadMessage(this);
 		}
 	}
 	
 	onSwitch () {
-		if (typeof BDfunctionsDevilBro === "object") {
+		if (typeof BDFDB === "object") {
 			this.addAllPreviews();
-			BDfunctionsDevilBro.addObserver(this, ".messages.scroller", {name:"chatWindowObserver"}, {childList:true, subtree:true});
+			BDFDB.addObserver(this, BDFDB.dotCN.messages, {name:"chatWindowObserver"}, {childList:true, subtree:true});
 		}
 	}
+	
 	
 	// begin of own functions
 	
 	addAllPreviews () {
 		document.querySelectorAll(".FIP-embed").forEach(embed => {embed.remove();});
-		document.querySelectorAll(".message-group").forEach(messageContainer => {
-			BDfunctionsDevilBro.addObserver(this, messageContainer, {name:"messageChangeObserver",multi:true}, {childList:true, characterData:true, subtree:true});
-			messageContainer.querySelectorAll(".markup").forEach(message => {
+		document.querySelectorAll(BDFDB.dotCN.messagegroup).forEach(messageContainer => {
+			BDFDB.addObserver(this, messageContainer, {name:"messageChangeObserver",multi:true}, {childList:true, characterData:true, subtree:true});
+			messageContainer.querySelectorAll(BDFDB.dotCN.messagemarkup).forEach(message => {
 				this.addPreviews(message);
 			});
 		});
@@ -112,7 +113,7 @@ class ForceImagePreviews {
 	
 	addPreviews (message) {
 		if (!message) return;
-		var messageData = BDfunctionsDevilBro.getKeyInformation({node:message,key:"message",up:true});
+		var messageData = BDFDB.getKeyInformation({node:message,key:"message",up:true});
 		if (!messageData) return;
 		
 		let accessory = this.getAccessoryOfMessage(message);
@@ -121,7 +122,7 @@ class ForceImagePreviews {
 			for (let word of messageData.content.split(new RegExp("\\n|\\s|\\r|\\t|\\0"))) {
 				if (word.indexOf("https://") > -1 || word.indexOf("http://") > -1) {
 					if (word.indexOf("<") == 0 && word.indexOf(">") == word.length-1) links.push({src:word.slice(1,-1),embedded:false});
-					else if (!accessory.querySelector(`.embedImage-1JnXMa[href="${this.parseSrc(word)}"]`)) links.push({src:word,embedded:false});
+					else if (!accessory.querySelector(`${BDFDB.dotCN.embedimage}[href="${this.parseSrc(word)}"]`)) links.push({src:word,embedded:false});
 					else links.push({src:word,embedded:true});
 				}
 			}
@@ -141,10 +142,10 @@ class ForceImagePreviews {
 					imagethrowaway.src = imagesrc;
 					let width = 400;
 					let height = Math.round(width*(imagethrowaway.naturalHeight/imagethrowaway.naturalWidth));
-					let embed = $(`<div class="FIP-embed embed-2diOCQ flex-3B1Tl4 embed"><a class="imageWrapper-38T7d9 imageZoom-2suFUV embedImage-1JnXMa" href="${imagesrc}" rel="noreferrer noopener" target="_blank" style="width: ${width}px; height: ${height}px;"><img src="${imagesrc}" style="width: ${width}px; height: ${height}px;"></a></div>`)[0];
-					let prevembed = accessory.querySelector(`.embedImage-1JnXMa[href="${previmage ? this.parseSrc(previmage.src) : void 0}"]`);
-					let nextembed = accessory.querySelector(`.embedImage-1JnXMa[href="${links[0] ? this.parseSrc(links[0].src) : void 0}"]`);
-					if (!accessory.querySelector(`.embedImage-1JnXMa[href="${imagesrc}"]`)) {
+					let embed = $(`<div class="FIP-embed ${BDFDB.disCNS.embed + BDFDB.disCNS.flex + BDFDB.disCN.embedold}"><a class="${BDFDB.disCNS.imagewrapper + BDFDB.disCNS.imagezoom + BDFDB.disCN.embedimage}" href="${imagesrc}" rel="noreferrer noopener" target="_blank" style="width: ${width}px; height: ${height}px;"><img src="${imagesrc}" style="width: ${width}px; height: ${height}px;"></a></div>`)[0];
+					let prevembed = accessory.querySelector(`${BDFDB.dotCN.embedimage}[href="${previmage ? this.parseSrc(previmage.src) : void 0}"]`);
+					let nextembed = accessory.querySelector(`${BDFDB.dotCN.embedimage}[href="${links[0] ? this.parseSrc(links[0].src) : void 0}"]`);
+					if (!accessory.querySelector(`${BDFDB.dotCN.embedimage}[href="${imagesrc}"]`)) {
 						accessory.insertBefore(embed, prevembed ? prevembed.parentElement.nextSibling : (nextembed ? nextembed.parentElement : null));
 					}
 					this.addImageToAccessory(image, links, accessory);
@@ -156,8 +157,8 @@ class ForceImagePreviews {
 	
 	getAccessoryOfMessage (message) {
 		var accessory = null;
-		while (message && !message.querySelector(".message-group") && !accessory) {
-			accessory = message.querySelector(".accessory");
+		while (message && !message.querySelector(BDFDB.dotCN.messagegroup) && !accessory) {
+			accessory = message.querySelector(BDFDB.dotCN.messageaccessory);
 			message = message.parentElement;
 		}
 		return accessory;
