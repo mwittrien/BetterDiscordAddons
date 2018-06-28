3e0674f3475e25b8ef6db508d30d1661f59a1299
Mirco Wittrien <mirco.wittrien@gmail.com>
2018-06-28 19:20:19 +0200

    Fixed ServerFolders <-> Theme incompatiblity

--- Plugins/ServerFolders/ServerFolders.plugin.js
+++ Plugins/ServerFolders/ServerFolders.plugin.js
@@ -332,7 +332,7 @@ class ServerFolders {
 
 	getDescription () {return "Adds the feature to create folders to organize your servers. Right click a server > 'Serverfolders' > 'Create Server' to create a server. To add servers to a folder hold 'Ctrl' and drag the server onto the folder, this will add the server to the folderlist and hide it in the serverlist. To open a folder click the folder. A folder can only be opened when it has at least one server in it. To remove a server from a folder, open the folder and either right click the server > 'Serverfolders' > 'Remove Server from Folder' or hold 'Del' and click the server in the folderlist.";}
 
-	getVersion () {return "5.6.9";}
+	getVersion () {return "5.7.0";}
 
 	getAuthor () {return "DevilBro";}
 	
@@ -1165,7 +1165,9 @@ class ServerFolders {
 									overflow-y: scroll !important;
 								}
 								
-								${BDFDB.dotCN.guildswrapper}.folderopen {
+								${BDFDB.dotCN.guildswrapper}.folderopen,
+								${BDFDB.dotCN.guildswrapper}.folderopen ${BDFDB.dotCN.scrollerwrapold},
+								${BDFDB.dotCN.guildswrapper}.folderopen ${BDFDB.dotCN.guilds}{
 									overflow: visible !important;
 									width: ${guildswrapper.outerWidth() + (guildswrapper.outerWidth() / columnamount)}px !important;
 								}`;
@@ -1184,7 +1186,9 @@ class ServerFolders {
 									overflow-y: hidden !important;
 								}
 								
-								${BDFDB.dotCN.guildswrapper}.folderopen {
+								${BDFDB.dotCN.guildswrapper}.folderopen,
+								${BDFDB.dotCN.guildswrapper}.folderopen ${BDFDB.dotCN.scrollerwrapold},
+								${BDFDB.dotCN.guildswrapper}.folderopen ${BDFDB.dotCN.guilds} {
 									overflow: visible !important;
 									height: ${guildswrapper.outerouterHeightWidth() + (guildswrapper.outerHeight() / rowamount)}px !important;
 								}`;
