e3507381ef9238463e85496fd9a98f3cc6dc401c
Mirco Wittrien <mirco.wittrien@gmail.com>
2018-11-26 23:36:18 +0100

    PinDMs no longer has any scrolling bugs

--- Plugins/PinDMs/PinDMs.plugin.js
+++ Plugins/PinDMs/PinDMs.plugin.js
@@ -13,38 +13,13 @@ class PinDMs {
 				<span>REPLACE_context_unpindm_text</span>
 				<div class="${BDFDB.disCN.contextmenuhint}"></div>
 			</div>`;
-			
-		this.pinDMsHeaderMarkup =
-			`<header class="pinneddms-header">REPLACE_header_pinneddms_text</header>`;
-			
-		this.pinnedDMMarkup =
-			`<div class="${BDFDB.disCN.dmchannel} pinned" style="height: 42px; opacity: 1;">
-				<a>
-					<div class="${BDFDB.disCNS.avatarwrapper + BDFDB.disCNS.avatarsmall + BDFDB.disCNS.forcedarktheme + BDFDB.disCN.avatarsmallold}">
-						<div class="${BDFDB.disCN.avatarsmallold} stop-animation"></div>
-						<div class="${BDFDB.disCNS.status + BDFDB.disCNS.statusold + BDFDB.disCN.avatarsmall}"></div>
-					</div> 
-					<div class="${BDFDB.disCN.dmchannelname}">
-						<label style="cursor: pointer;"></label>
-						<div class="${BDFDB.disCNS.flex + BDFDB.disCNS.flex2 + BDFDB.disCNS.horizontal + BDFDB.disCNS.horizontal2 + BDFDB.disCNS.directionrow + BDFDB.disCNS.justifystart + BDFDB.disCNS.aligncenter + BDFDB.disCNS.nowrap + BDFDB.disCN.dmchannelactivity}" style="flex: 1 1 auto;">
-							<div class="${BDFDB.disCN.dmchannelactivitytext}"></div>
-						</div>
-					</div>
-					<button class="${BDFDB.disCN.dmchannelclose}"></button>
-				</a>
-			</div>`;
-		
-		this.richActivityMarkup = 
-			`<svg name="RichActivity" class="${BDFDB.disCN.dmchannelactivityicon}" width="16" height="16" viewBox="0 0 16 16">
-				<path class="${BDFDB.disCN.dmchannelactivityiconforeground}" fill="currentColor" d="M6,7 L2,7 L2,6 L6,6 L6,7 Z M8,5 L2,5 L2,4 L8,4 L8,5 Z M8,3 L2,3 L2,2 L8,2 L8,3 Z M8.88888889,0 L1.11111111,0 C0.494444444,0 0,0.494444444 0,1.11111111 L0,8.88888889 C0,9.50253861 0.497461389,10 1.11111111,10 L8.88888889,10 C9.50253861,10 10,9.50253861 10,8.88888889 L10,1.11111111 C10,0.494444444 9.5,0 8.88888889,0 Z" transform="translate(3 3)"></path>
-			</svg>`;
 	}
 
 	getName () {return "PinDMs";}
 
 	getDescription () {return "Allows you to pin DMs, making them appear at the top of your DM-list.";}
 
-	getVersion () {return "1.1.6";}
+	getVersion () {return "1.1.7";}
 
 	getAuthor () {return "DevilBro";}
 
@@ -94,21 +69,21 @@ class PinDMs {
 			});
 			BDFDB.addObserver(this, BDFDB.dotCN.appmount, {name:"dmContextObserver",instance:observer}, {childList: true});
 			
-			observer = new MutationObserver((changes, _) => {
-				changes.forEach(
-					(change, i) => {
-						if (change.addedNodes) {
-							change.addedNodes.forEach((node) => {
-								if (node.tagName && node.querySelector(`a[href="/channels/@me"]`)) {
-									$(BDFDB.dotCN.dmchannel + ".pinned, header.pinneddms-header").remove();
-									this.addAllPinnedDMs();
-								}
-							});
-						}
+			$(BDFDB.dotCN.appmount).on("click." + this.getName(), BDFDB.dotCNS.dmchannels + BDFDB.dotCNS.dmchannel + BDFDB.dotCN.dmchannelclose, (e) => {
+				let instance = BDFDB.getReactInstance(e.currentTarget);
+				if (instance.return.return.return.return.return.memoizedProps.ispin) {
+					e.stopPropagation();
+					e.preventDefault();
+					let dmsscroller = document.querySelector(BDFDB.dotCNS.dmchannels + BDFDB.dotCN.scroller);
+					if (dmsscroller) {
+						let channel = instance.return.return.return.return.return.memoizedProps.channel;
+						let dms = BDFDB.getReactInstance(dmsscroller).return.return.return.memoizedProps.children;
+						this.removePinnedDM(channel.type == 1 ? channel.recipients[0] : channel.id, dms); 
+						this.forceUpdateScroller(dmsscroller);
 					}
-				);
+				}
+				else setImmediate(() => {this.addAllPinnedDMs();});
 			});
-			BDFDB.addObserver(this, BDFDB.dotCNS.dmchannels + BDFDB.dotCN.scroller, {name:"friendButtonObserver",instance:observer}, {childList: true});
 			
 			setTimeout(() => {this.onSwitch();},1000);
 		}
@@ -119,29 +94,38 @@ class PinDMs {
 
 	stop () {
 		if (typeof BDFDB === "object") {
-			$(BDFDB.dotCN.dmchannel + ".pinned, header.pinneddms-header").remove();
-			
-			clearInterval(this.statusInterval);
-			
+			let dmsscroller = document.querySelector(BDFDB.dotCNS.dmchannels + BDFDB.dotCN.scroller);
+			if (dmsscroller) {
+				let dms = BDFDB.getReactInstance(dmsscroller).return.return.return.memoizedProps.children;
+				let amount = 0;
+				let insertpoint = null;
+				for (let i in dms) {
+					let ele = dms[i];
+					if (ele && ele.pinned) {
+						delete ele.pinned;
+						if (ele.props.ispin) {
+							if (ele.type == "header") insertpoint = i;
+							amount++;
+						}
+					}
+				}
+				dms.splice(insertpoint, amount);
+				this.forceUpdateScroller(dmsscroller);
+			}
 			BDFDB.unloadMessage(this);
 		}
 	}
 	
 	onSwitch () {
-		if (BDFDB.getSelectedServer()) clearInterval(this.statusInterval);
 		if (!document.querySelector(BDFDB.dotCNS.guildselected + BDFDB.dotCN.friendsicon) || document.querySelector(BDFDB.dotCN.dmchannel + ".pinned")) return;
 		
 		this.addAllPinnedDMs();
-		
-		BDFDB.addObserver(this, BDFDB.dotCNS.dmchannels + BDFDB.dotCN.scroller, {name:"friendButtonObserver"}, {childList: true});
 	}
 	
 	
 	// begin of own functions
 	
-	changeLanguageStrings () {
-		this.pinDMsHeaderMarkup = 	this.pinDMsHeaderMarkup.replace("REPLACE_header_pinneddms_text", this.labels.header_pinneddms_text);
-		
+	changeLanguageStrings () {		
 		this.pinDMEntryMarkup = 	this.pinDMEntryMarkup.replace("REPLACE_context_pindm_text", this.labels.context_pindm_text);
 		
 		this.unpinDMEntryMarkup = 	this.unpinDMEntryMarkup.replace("REPLACE_context_unpindm_text", this.labels.context_unpindm_text);
@@ -160,16 +144,19 @@ class PinDMs {
 			}
 		}
 		if (ele) {
-			if (BDFDB.loadData(info.id, this, "pinnedDMs") == null) {
+			let pinnedDMs = BDFDB.loadAllData(this, "pinnedDMs");
+			if (typeof pinnedDMs[info.id] == "undefined") {
 				$(this.pinDMEntryMarkup).insertBefore(ele)
 					.on("click", (e) => {
 						$(context).hide();
-						var pinnedDMs = BDFDB.loadAllData(this, "pinnedDMs");
-						if (typeof pinnedDMs[info.id] == "undefined") {
-							var pos = Object.keys(pinnedDMs).length;
-							pinnedDMs[info.id] = pos;
+						let dmsscroller = document.querySelector(BDFDB.dotCNS.dmchannels + BDFDB.dotCN.scroller);
+						if (dmsscroller) {
+							let dms = BDFDB.getReactInstance(dmsscroller).return.return.return.memoizedProps.children;
+							let insertpoint = this.getInsertPoint(dms);
+							this.addPinnedDM(info.id, dms, insertpoint); 
+							this.forceUpdateScroller(dmsscroller);
+							pinnedDMs[info.id] = Object.keys(pinnedDMs).length;
 							BDFDB.saveAllData(pinnedDMs, this, "pinnedDMs");
-							this.addPinnedDM(info.id, pos); 
 						}
 					});
 			}
@@ -177,159 +164,98 @@ class PinDMs {
 				$(this.unpinDMEntryMarkup).insertBefore(ele)
 					.on("click", (e) => {
 						$(context).hide();
-						$(`${BDFDB.dotCN.dmchannel}.pinned[user-id="${info.id}"]`).remove();
-						$(`${BDFDB.dotCN.dmchannel}.pinned[channel-id="${info.id}"]`).remove(); 
-						BDFDB.removeData(info.id, this, "pinnedDMs");
-						this.updatePinnedDMPositions();
+						let dmsscroller = document.querySelector(BDFDB.dotCNS.dmchannels + BDFDB.dotCN.scroller);
+						if (dmsscroller) {
+							let dms = BDFDB.getReactInstance(dmsscroller).return.return.return.memoizedProps.children;
+							this.removePinnedDM(info.id, dms); 
+							this.forceUpdateScroller(dmsscroller);
+						}
 					});
 			}
-				
+			
 			BDFDB.updateContextPosition(context);
 		}
 	}
 	
 	addAllPinnedDMs () {
+		let dmsscroller = document.querySelector(BDFDB.dotCNS.dmchannels + BDFDB.dotCN.scroller);
+		if (dmsscroller) {
+			let dms = BDFDB.getReactInstance(dmsscroller).return.return.return.memoizedProps.children;
+			let insertpoint = this.getInsertPoint(dms);
+			let sortedDMs = this.sortAndUpdate();
+			for (let pos in sortedDMs) this.addPinnedDM(sortedDMs[pos], dms, insertpoint);
+			this.forceUpdateScroller(dmsscroller);
+		}
+	}
+	
+	getInsertPoint (dms) {
+		let insertpoint = null;
+		for (let i in dms) {
+			let ele = dms[i];
+			if (ele && ele.type == "header") {
+				insertpoint = i;
+				if (!ele.pinned && !ele.props.ispin) {
+					ele.pinned = true;
+					let headerpin = Object.assign({},ele);
+					headerpin.key = "pin" + headerpin.key;
+					headerpin.props = {children:this.labels.header_pinneddms_text,ispin:true};
+					dms.splice(insertpoint,0,headerpin);
+				}
+				insertpoint++;
+				break;
+			}
+		}
+		return insertpoint;
+	}
+	
+	addPinnedDM (id, dms, insertpoint) {
+		id = this.ChannelStore.getDMFromUserId(id) || id;
+		for (let ele of dms) {
+			if (ele && !ele.pinned && id == ele.key) {
+				ele.pinned = true;
+				let dmpin = Object.assign({ispin:true},ele);
+				dmpin.key = "pin" + ele.key;
+				dmpin.props = {channel:ele.props.channel,selected:ele.props.selected,ispin:true};
+				dms.splice(insertpoint,0,dmpin);
+			}
+		}
+	}
+	
+	removePinnedDM (id, dms) {
+		BDFDB.removeData(id, this, "pinnedDMs");
+		this.sortAndUpdate();
+		id = this.ChannelStore.getDMFromUserId(id) || id;
+		let removepoint = null;
+		for (let i in dms) {
+			let ele = dms[i];
+			if (ele && ele.pinned && (id == ele.key || ("pin" + id) == ele.key)) {
+				delete ele.pinned;
+				if (ele.props.ispin) removepoint = i;
+			}
+		}
+		if (removepoint) {
+			dms.splice(removepoint,1);
+		}
+	}
+	
+	sortAndUpdate () {
 		let pinnedDMs = BDFDB.loadAllData(this, "pinnedDMs");
-		let sortedDMs = [];
 		delete pinnedDMs[""];
-		for (let id in pinnedDMs) this.sortDM(sortedDMs, pinnedDMs, id);
+		let sortedDMs = [], sortDM = (id) => {
+			if (!this.UserStore.getUser(id) && !this.ChannelStore.getChannel(id)) delete pinnedDMs[id];
+			else if (typeof sortedDMs[pinnedDMs[id]] == "undefined") sortedDMs[pinnedDMs[id]] = id;
+			else sortDM(sortedDMs, pinnedDMs[id]+1, id);
+		};
+		for (let id in pinnedDMs) sortDM(id);
 		sortedDMs = sortedDMs.filter(n => n);
-		for (let pos in sortedDMs) {
-			pinnedDMs[sortedDMs[pos]] = parseInt(pos);
-			this.addPinnedDM(sortedDMs[pos], pos);
-		}
+		for (let pos in sortedDMs) pinnedDMs[sortedDMs[pos]] = parseInt(pos);
 		BDFDB.saveAllData(pinnedDMs, this, "pinnedDMs");
+		return sortedDMs;
 	}
 	
-	sortDM (sortedDMs, pinnedDMs, id) {
-		if (!this.UserStore.getUser(id) && !this.ChannelStore.getChannel(id)) delete pinnedDMs[id];
-		else if (typeof sortedDMs[pinnedDMs[id]] == "undefined") sortedDMs[pinnedDMs[id]] = id;
-		else this.sortDM(sortedDMs, pinnedDMs[id]+1, id);
-	}
-	
-	addPinnedDM (id, pos) {
-		if (!document.querySelector(BDFDB.dotCN.dmchannel + " + header.pinneddms-header")) {
-			$(this.pinDMsHeaderMarkup).insertBefore(document.querySelector(BDFDB.dotCN.dmchannel + " + header"));
-			this.startUpdateInterval();
-		}
-		let user = this.UserStore.getUser(id);
-		let channel = this.ChannelStore.getChannel(id);
-		if (user || channel) {
-			let pinnedDM = $(this.pinnedDMMarkup);
-			pinnedDM.attr("user-id", user ? user.id : null).attr("channel-id", channel ? channel.id : null).insertAfter(BDFDB.dotCN.dmchannel + " + header.pinneddms-header")
-				.on("contextmenu." + this.getName(), (e) => {
-					if (user) {
-						let DMid = this.ChannelStore.getDMFromUserId(user.id)
-						if (DMid) {
-							this.UserContextMenuUtils.openUserContextMenu(e, user, this.ChannelStore.getChannel(DMid));
-						}
-						else {
-							this.PrivateChannelUtils.ensurePrivateChannel(BDFDB.myData.id, user.id).then(DMid => {
-								this.UserContextMenuUtils.openUserContextMenu(e, user, this.ChannelStore.getChannel(DMid));
-							});
-						}
-					}
-					else if (channel) {
-						var channelObj = BDFDB.getDivOfChannel(channel.id);
-						if (channelObj && channelObj.div) BDFDB.getKeyInformation({"node":channelObj.div,"key":"onContextMenu"})(e);
-						else BDFDB.showToast("Could not open ContextMenu, make sure the DM exists, Group DMs have to be loaded in the list.", {type:"error"});
-					}
-				})
-				.on("click." + this.getName(), (e) => {
-					if (e.target.classList && e.target.classList.contains(BDFDB.disCN.dmchannelclose)) return;
-					else if (channel) this.ChannelSwitchUtils.selectPrivateChannel(channel.id);
-					else if (user) {
-						let DMid = this.ChannelStore.getDMFromUserId(user.id)
-						if (DMid) this.ChannelSwitchUtils.selectPrivateChannel(DMid);
-						else this.PrivateChannelUtils.openPrivateChannel(BDFDB.myData.id, user.id);
-					}
-				})
-				.on("click." + this.getName(), BDFDB.dotCN.dmchannelclose, () => {
-					pinnedDM.remove();
-					BDFDB.removeData(user ? user.id : channel.id, this, "pinnedDMs");
-					this.updatePinnedDMPositions();
-				});
-				
-			this.setPinnedDM(pinnedDM[0]);
-		}
-	}
-	
-	setPinnedDM (pinnedDM) {
-		if (pinnedDM && pinnedDM.parentElement) {
-			let id = pinnedDM.getAttribute("user-id");
-			let user = this.UserStore.getUser(id);
-			if (user) {
-				let data = BDFDB.loadData(user.id, "EditUsers", "users") || {};
-				let activity = this.ActivityModule.getApplicationActivity(id);
-				pinnedDM.querySelector(BDFDB.dotCN.avatarsmallold + ":not(" + BDFDB.dotCN.avatarwrapper + ")").style.backgroundImage = `url(${data.removeIcon ? "" : (data.url ? data.url : BDFDB.getUserAvatar(user.id))})`; 
-				pinnedDM.querySelector(BDFDB.dotCN.status).classList.add(BDFDB.disCN[`status${BDFDB.getUserStatus(user.id)}`]);
-				pinnedDM.querySelector(BDFDB.dotCN.dmchannelname + " > label").textContent = data.name ? data.name : user.username;
-				pinnedDM.querySelector(BDFDB.dotCN.dmchannelname).style.color = data.color1 ? BDFDB.color2RGB(data.color1) : "";
-				pinnedDM.querySelector(BDFDB.dotCN.dmchannelname).style.background = data.color2 ? BDFDB.color2RGB(data.color2) : "";
-				pinnedDM.querySelector(BDFDB.dotCN.dmchannelactivitytext).innerHTML = activity ? this.getActivityString(activity.type, activity.name) : "";
-				if (activity && activity.application_id && activity.session_id) {
-					if (!pinnedDM.querySelector(BDFDB.dotCN.dmchannelactivityicon)) $(BDFDB.dotCN.dmchannelactivity, pinnedDM).append(this.richActivityMarkup);
-				}
-				else $(BDFDB.dotCN.dmchannelactivityicon, pinnedDM).remove();
-			}
-			else {
-				id = pinnedDM.getAttribute("channel-id")
-				let channel = this.ChannelStore.getChannel(id);
-				if (channel) {
-					pinnedDM.querySelector(BDFDB.dotCN.avatarsmallold + ":not(" + BDFDB.dotCN.avatarwrapper + ")").style.backgroundImage = `url(${BDFDB.getChannelAvatar(channel.id)})`;
-					var channelname = channel.name;
-					if (!channelname && channel.recipients.length > 0) {
-						for (let dmmemberID of channel.recipients) {
-							channelname = channelname ? channelname + ", " : channelname;
-							channelname = channelname + this.UserStore.getUser(dmmemberID).username;
-						}
-					}
-					pinnedDM.querySelector(BDFDB.dotCN.dmchannelname + " > label").textContent = channelname ? channelname : BDFDB.LanguageStrings.UNNAMED;
-					pinnedDM.querySelectorAll(BDFDB.dotCNC.status + BDFDB.dotCN.dmchannelactivitytext).forEach(ele => {ele.remove();});
-					pinnedDM.querySelector(BDFDB.dotCN.dmchannelactivity).innerHTML = channel.recipients.length+1 + " " + (channel.recipients.length+1 == 1 ? BDFDB.LanguageStrings.MEMBER : BDFDB.LanguageStrings.MEMBERS);
-				}
-			}
-		}
-	}
-	
-	updatePinnedDMPositions () {
-		let pinnedDMs = BDFDB.loadAllData(this, "pinnedDMs");
-		let pinnedDMEles =  Array.from(document.querySelectorAll(BDFDB.dotCN.dmchannel + ".pinned")).reverse();
-		for (let i = 0; i < pinnedDMEles.length; i++) {
-			pinnedDMs[pinnedDMEles[i].getAttribute("user-id") ? pinnedDMEles[i].getAttribute("user-id") : pinnedDMEles[i].getAttribute("channel-id")] = i;
-		}
-		if (pinnedDMEles.length == 0) $(BDFDB.dotCN.dmchannel + " + header.pinneddms-header").remove();
-		BDFDB.saveAllData(pinnedDMs, this, "pinnedDMs")
-	}
-	
-	startUpdateInterval () {
-		clearInterval(this.statusInterval);
-		this.statusInterval = setInterval(() => {
-			for (let pinnedDM of document.querySelectorAll(BDFDB.dotCN.dmchannel + ".pinned")) this.setPinnedDM(pinnedDM);
-			if (!document.querySelector(BDFDB.dotCN.dmchannel + " + header.pinneddms-header")) clearInterval(this.statusInterval); 
-		},10000);
-	}
-	
-	getActivityString (type, name) {
-		let stringname = "";
-		switch (type) {
-			case 0:
-				stringname = "PLAYING_GAME";
-				break;
-			case 1:
-				stringname = "STREAMING";
-				break;
-			case 2:
-				stringname = "LISTENING_TO";
-				break;
-			case 3:
-				stringname = "WATCHING";
-				break;
-		}
-		
-		let string = BDFDB.LanguageStrings[stringname] || "";
-		
-		return string.replace("**!!{name}!!**", `<strong>${name}</strong>`).replace("**!!{game}!!**", `<strong>${name}</strong>`);
+	forceUpdateScroller (scroller) {
+		scroller.scrollTop += 10;
+		scroller.scrollTop -= 10;
 	}
 	
 	setLabelsByLanguage () {
