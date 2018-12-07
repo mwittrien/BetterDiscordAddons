c64ef662a7a25171c8e890458b8ec2018289ca66
Mirco Wittrien <mirco.wittrien@gmail.com>
2018-12-07 13:19:22 +0100

    Update ServerFolders.plugin.js

--- Plugins/ServerFolders/ServerFolders.plugin.js
+++ Plugins/ServerFolders/ServerFolders.plugin.js
@@ -313,7 +313,7 @@ class ServerFolders {
 
 	getDescription () {return "Adds the feature to create folders to organize your servers. Right click a server > 'Serverfolders' > 'Create Server' to create a server. To add servers to a folder hold 'Ctrl' and drag the server onto the folder, this will add the server to the folderlist and hide it in the serverlist. To open a folder click the folder. A folder can only be opened when it has at least one server in it. To remove a server from a folder, open the folder and either right click the server > 'Serverfolders' > 'Remove Server from Folder' or hold 'Del' and click the server in the folderlist.";}
 
-	getVersion () {return "5.8.6";}
+	getVersion () {return "5.8.5";}
 
 	getAuthor () {return "DevilBro";}
 	
