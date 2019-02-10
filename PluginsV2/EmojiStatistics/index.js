74fafb7062d6f0831923ec871cd9ff1c5870f8ea
Mirco Wittrien <mirco.wittrien@gmail.com>
2019-02-10 19:40:46 +0100

    stuff

--- PluginsV2/EmojiStatistics/index.js
+++ PluginsV2/EmojiStatistics/index.js
@@ -159,7 +159,7 @@ module.exports = (Plugin, Api, Vendor) => {
 		onStart () {
 			if (global.BDFDB && global.BDFDB.myPlugins && typeof global.BDFDB.myPlugins == "object") global.BDFDB.myPlugins[this.name] = this;
 			var libraryScript = document.querySelector('head script[src="https://mwittrien.github.io/BetterDiscordAddons/Plugins/BDFDB.js"]');
-			if (!libraryScript || performance.now() - libraryScript.getAttribute("date") > 600000) {
+			if (!global.BDFDB || typeof BDFDB != "object" || performance.now() - BDFDB.creationTime > 600000e.now() - BDFDB.creationTime > 600000) {
 				if (libraryScript) libraryScript.remove();
 				libraryScript = document.createElement("script");
 				libraryScript.setAttribute("type", "text/javascript");
