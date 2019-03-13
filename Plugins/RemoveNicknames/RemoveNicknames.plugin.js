4217344f9ef44a40e4bc9b0b8aeed02ea7997b47
Mirco Wittrien <mirco.wittrien@gmail.com>
2019-03-13 14:40:38 +0100

    Update RemoveNicknames.plugin.js

--- Plugins/RemoveNicknames/RemoveNicknames.plugin.js
+++ Plugins/RemoveNicknames/RemoveNicknames.plugin.js
@@ -3,7 +3,7 @@
 class RemoveNicknames {
 	getName () {return "RemoveNicknames";}
 
-	getVersion () {return "1.1.6";}
+	getVersion () {return "1.1.7";}
 
 	getAuthor () {return "DevilBro";}
 
@@ -11,7 +11,7 @@ class RemoveNicknames {
 
 	initConstructor () {
 		this.changelog = {
-			"fixed":[["Replace Own","Fixed the issue where it would replace your own nick even with the option disabled"]]
+			"fixed":[["Little Bug","Fixed a little bug that caused some console error output and could mess up names sometimes"]]
 		};
 		
 		this.patchModules = {
@@ -107,7 +107,7 @@ class RemoveNicknames {
 		if (EditUsersData && EditUsersData.name) return EditUsersData.name;
 		let settings = BDFDB.getAllData(this, "settings");
 		let member = this.MemberUtils.getMember(this.LastGuildStore.getGuildId(), info.id);
-		if (this.reseting || !member || !member.nick || info.id == BDFDB.myData.id && !settings.replaceOwn) return member.nick || info.username;
+		if (this.reseting || !member || !member.nick || info.id == BDFDB.myData.id && !settings.replaceOwn) return member && member.nick ? member.nick : info.username;
 		return settings.addNickname ? (settings.swapPositions ? (member.nick + " (" + info.username + ")") : (info.username + " (" + member.nick + ")")) : info.username;
 	}
 
