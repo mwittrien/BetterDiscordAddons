2f2dec9a01bd9e4d3fb674e07854f267e7fd039e
Mirco Wittrien <mirco.wittrien@gmail.com>
2019-05-24 11:46:22 +0200

    stuff

--- Plugins/CharCounter/CharCounter.plugin.js
+++ Plugins/CharCounter/CharCounter.plugin.js
@@ -3,7 +3,7 @@
 class CharCounter {
 	getName () {return "CharCounter";}
 
-	getVersion () {return "1.3.3";}
+	getVersion () {return "1.3.4";}
 
 	getAuthor () {return "DevilBro";}
 
@@ -11,7 +11,7 @@ class CharCounter {
 
 	initConstructor () {
 		this.changelog = {
-			"fixed":[["Parsed Length","The character counter show the parsed length (which determines if a message is over 2000 chars) again like it used to, instead of the literal length"]]
+			"fixed":[["Context Menu","Fixed the issue where the plugin would break the copy/paste actions via the textarea contextmenu"]]
 		};
 		
 		this.patchModules = {
@@ -163,9 +163,13 @@ class CharCounter {
 			BDFDB.addEventListener(this, document, "mouseup", () => {
 				BDFDB.removeEventListener(this, document);
 				if (input.selectionEnd - input.selectionStart) setImmediate(() => {BDFDB.addEventListener(this, document, "click", () => {
-					input.selectionStart = 0;
-					input.selectionEnd = 0;
-					updateCounter();
+					var contexttype = BDFDB.getReactValue(document.querySelector(BDFDB.dotCN.contextmenu), "return.stateNode.props.type");
+					if (!contexttype || !contexttype.startsWith("CHANNEL_TEXT_AREA")) {
+						input.selectionStart = 0;
+						input.selectionEnd = 0;
+						updateCounter();
+					}
+					else setTimeout(() => {updateCounter();},100);
 					BDFDB.removeEventListener(this, document);
 				});});
 			});
