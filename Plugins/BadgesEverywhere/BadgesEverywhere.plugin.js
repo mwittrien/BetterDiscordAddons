e1928ec6cd6f73b91521d680f729082c3bc66de8
Mirco Wittrien <mirco.wittrien@gmail.com>
2019-05-31 10:31:30 +0200

    Update BadgesEverywhere.plugin.js

--- Plugins/BadgesEverywhere/BadgesEverywhere.plugin.js
+++ Plugins/BadgesEverywhere/BadgesEverywhere.plugin.js
@@ -19,8 +19,6 @@ class BadgesEverywhere {
 			"MessageUsername":"componentDidMount",
 			"StandardSidebarView":"componentWillUnmount"
 		};
-		
-		this.chatBoostBadge = `<svg name="PremiumGuildSubscriberBadge" class="BE-badge BE-badge-GuildSubscriptionChat ${BDFDB.disCNS.memberpremiumicon + BDFDB.disCN.membericon}" aria-hidden="false" width="24" height="24" viewBox="0 0 8 12"><path d="M4 0L0 4V8L4 12L8 8V4L4 0ZM7 7.59L4 10.59L1 7.59V4.41L4 1.41L7 4.41V7.59Z" fill="currentColor"></path><path d="M2 4.83V7.17L4 9.17L6 7.17V4.83L4 2.83L2 4.83Z" fill="currentColor"></path></svg>`;
 
 		this.css = ` 
 			.BE-badge {
