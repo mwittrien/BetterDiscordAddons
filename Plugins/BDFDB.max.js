2fc52ca7fa99bac3508abb7de745740e34d60ae1
Mirco Wittrien <mirco.wittrien@gmail.com>
2019-09-04 16:52:59 +0200

    stuff

--- Plugins/BDFDB.max.js
+++ Plugins/BDFDB.max.js
@@ -5230,6 +5230,14 @@ var BDFDB = {myPlugins: BDFDB && BDFDB.myPlugins ? BDFDB.myPlugins : {}, BDv2Api
 		#bd-settingspane-container ${BDFDB.dotCN._repodescription} {
 			white-space: pre-line !important;
 		}
+		.BDFDB-versionchangelog {
+			display: inline-block;
+			background: currentColor;
+			-webkit-mask: url('data:image/svg+xml; utf8, <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 510 510"><path fill="currentColor" d="M267.75,12.75c-89.25,0-168.3,48.45-209.1,122.4L0,76.5v165.75h165.75 l-71.4-71.4c33.15-63.75,96.9-107.1,173.4-107.1C372.3,63.75,459,150.45,459,255s-86.7,191.25-191.25,191.25 c-84.15,0-153-53.55-181.05-127.5H33.15c28.05,102,122.4,178.5,234.6,178.5C402.9,497.25,510,387.6,510,255 C510,122.4,400.35,12.75,267.75,12.75z M229.5,140.25V270.3l119.85,71.4l20.4-33.15l-102-61.2v-107.1H229.5z"></path></svg>') center/contain no-repeat;
+			cursor: pointer;
+			margin: 0 4px 0 3px;
+		}
+		
 		${BDFDB.dotCNS.themedark + BDFDB.dotCN.popoutthemedpopout + BDFDB.notCN.messagespopoutwrap} {
 			-webkit-box-shadow: 0 2px 10px 0 rgba(0,0,0,20%);
 			background-color: #2f3136;
@@ -6013,7 +6021,7 @@ var BDFDB = {myPlugins: BDFDB && BDFDB.myPlugins ? BDFDB.myPlugins : {}, BDv2Api
 				let version = wrapper.querySelector(BDFDB.dotCN._repoversion);
 				if (version && data.changelog) {
 					BDFDB.removeEles(version.querySelectorAll('.BDFDB-versionchangelog'));
-					let changelogicon = BDFDB.htmlToElement(`<svg class="BDFDB-versionchangelog" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 510 510" style="margin: 0 2px -3px 6px;"><path fill="currentColor" d="M267.75,12.75c-89.25,0-168.3,48.45-209.1,122.4L0,76.5v165.75h165.75 l-71.4-71.4c33.15-63.75,96.9-107.1,173.4-107.1C372.3,63.75,459,150.45,459,255s-86.7,191.25-191.25,191.25 c-84.15,0-153-53.55-181.05-127.5H33.15c28.05,102,122.4,178.5,234.6,178.5C402.9,497.25,510,387.6,510,255 C510,122.4,400.35,12.75,267.75,12.75z M229.5,140.25V270.3l119.85,71.4l20.4-33.15l-102-61.2v-107.1H229.5z"></path></svg>`);
+					let changelogicon = BDFDB.htmlToElement(`<span class="BDFDB-versionchangelog" style="white-space: pre !important;">   </span>`);
 					version.appendChild(changelogicon);
 					changelogicon.addEventListener('click', () => {BDFDB.openChangeLogModal(data);});
 					changelogicon.addEventListener('mouseenter', () => {
