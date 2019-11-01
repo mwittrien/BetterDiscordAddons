c011c671683efdb8238b2e7e890769c57ad51906
Mirco Wittrien <mirco.wittrien@gmail.com>
2019-11-01 10:32:04 +0100

    stuff

--- Plugins/BDFDB.js
+++ Plugins/BDFDB.js
@@ -26,7 +26,7 @@ var BDFDB = {myPlugins: BDFDB && BDFDB.myPlugins || {}, cleanUps: BDFDB && BDFDB
 		var url = typeof plugin.getRawUrl == "function" && typeof plugin.getRawUrl() == "string" ? plugin.getRawUrl() : `https://mwittrien.github.io/BetterDiscordAddons/Plugins/${plugin.name}/${plugin.name}.plugin.js`;
 		BDFDB.PluginUtils.checkUpdate(plugin.name, url);
 
-		if (typeof plugin.initConstructor === "function") BDFDB.TimeUtils.suppress(plugin.initConstructor.bind(plugin), "Could not initiate constructor!", plugin.name);
+		if (typeof plugin.initConstructor === "function") setImmediate(() => {BDFDB.TimeUtils.suppress(plugin.initConstructor, "Could not initiate constructor!", plugin.name)();});
 		if (typeof plugin.css === "string") BDFDB.DOMUtils.appendLocalStyle(plugin.name, plugin.css);
 
 
