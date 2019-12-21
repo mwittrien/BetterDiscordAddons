9f49c6d846ed1a7eec994eaa35420d0d8b44c39b
Mirco Wittrien <mirco.wittrien@gmail.com>
2019-12-21 11:45:18 +0100

    stuff

--- Plugins/BDFDB.js
+++ Plugins/BDFDB.js
@@ -7237,11 +7237,15 @@
 						defaultValue: [this.props.defaultValue].flat(10).filter(n => n).map(keycode => [BDFDB.DiscordConstants.KeyboardDeviceTypes.KEYBOARD_KEY, keycode, BDFDB.DiscordConstants.KeyboardEnvs.BROWSER]),
 						onChange: this.handleChange.bind(this)
 					}), "reset", "onReset")),
-					this.props.reset || this.props.onReset ? BDFDB.ReactUtils.createElement(LibraryComponents.Clickable, {
-						className: BDFDB.disCN.hotkeyresetbutton,
-						onClick: this.handleReset.bind(this),
-						children: BDFDB.ReactUtils.createElement(LibraryComponents.SvgIcon, {
-							iconSVG: `<svg height="20" width="20" viewBox="0 0 20 20"><path fill="currentColor" d="M 14.348 14.849 c -0.469 0.469 -1.229 0.469 -1.697 0 l -2.651 -3.030 -2.651 3.029 c -0.469 0.469 -1.229 0.469 -1.697 0 -0.469 -0.469 -0.469 -1.229 0 -1.697l2.758 -3.15 -2.759 -3.152 c -0.469 -0.469 -0.469 -1.228 0 -1.697 s 1.228 -0.469 1.697 0 l 2.652 3.031 2.651 -3.031 c 0.469 -0.469 1.228 -0.469 1.697 0 s 0.469 1.229 0 1.697l -2.758 3.152 2.758 3.15 c 0.469 0.469 0.469 1.229 0 1.698 z"></path></svg>`,
+					this.props.reset || this.props.onReset ? BDFDB.ReactUtils.createElement(LibraryComponents.TooltipContainer, {
+						text: BDFDB.LanguageUtils.LanguageStrings.REMOVE_KEYBIND,
+						tooltipConfig: {type: "top"},
+						children: BDFDB.ReactUtils.createElement(LibraryComponents.Clickable, {
+							className: BDFDB.disCN.hotkeyresetbutton,
+							onClick: this.handleReset.bind(this),
+							children: BDFDB.ReactUtils.createElement(LibraryComponents.SvgIcon, {
+								iconSVG: `<svg height="20" width="20" viewBox="0 0 20 20"><path fill="currentColor" d="M 14.348 14.849 c -0.469 0.469 -1.229 0.469 -1.697 0 l -2.651 -3.030 -2.651 3.029 c -0.469 0.469 -1.229 0.469 -1.697 0 -0.469 -0.469 -0.469 -1.229 0 -1.697l2.758 -3.15 -2.759 -3.152 c -0.469 -0.469 -0.469 -1.228 0 -1.697 s 1.228 -0.469 1.697 0 l 2.652 3.031 2.651 -3.031 c 0.469 -0.469 1.228 -0.469 1.697 0 s 0.469 1.229 0 1.697l -2.758 3.152 2.758 3.15 c 0.469 0.469 0.469 1.229 0 1.698 z"></path></svg>`,
+							})
 						})
 					}) : null
 				].filter(n => n)
