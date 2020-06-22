e43c0c35520da37a353d7cee4fd4c4168aa9e048
Mirco Wittrien <mirco.wittrien@gmail.com>
2020-06-22 12:30:52 +0200

    stuff

--- Plugins/BDFDB.js
+++ Plugins/BDFDB.js
@@ -8626,6 +8626,14 @@
 			opacity: 0;
 		}
 		
+		${BDFDB.dotCN.app} {
+			position: fixed;
+			top: 0;
+			right: 0;
+			left: 0;
+			bottom: 0;
+		}
+		
 		${BDFDB.dotCN.loadingiconwrapper} {
 			position: absolute;
 			bottom: 0;
