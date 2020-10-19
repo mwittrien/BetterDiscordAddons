5ecf15557569cb082f435631986ffefa23c7f410
Mirco Wittrien <mirco.wittrien@gmail.com>
2020-10-19 14:01:22 +0200

    Update 0BDFDB.plugin.js

--- Library/0BDFDB.plugin.js
+++ Library/0BDFDB.plugin.js
@@ -6578,7 +6578,7 @@ module.exports = (_ => {
 						defaultProps: {
 							foreground: ""
 						},
-						icon: `<svg name="Download" aria-hidden="false" width="%%width" height="%%height" viewBox="0 0 24 24"><path class="%%foreground" fill="%%color" fill-rule="evenodd" clip-rule="evenodd" d="M16.293 9.293L17.707 10.707L12 16.414L6.29297 10.707L7.70697 9.293L11 12.586V2H13V12.586L16.293 9.293ZM18 20V18H20V20C20 21.102 19.104 22 18 22H6C4.896 22 4 21.102 4 20V18H6V20H18Z" aria-hidden="true"></path></svg>`
+						icon: `<svg name="Download" fill="%%color" aria-hidden="false" width="%%width" height="%%height" viewBox="0 0 24 24"><path class="%%foreground" fill-rule="evenodd" clip-rule="evenodd" d="M16.293 9.293L17.707 10.707L12 16.414L6.29297 10.707L7.70697 9.293L11 12.586V2H13V12.586L16.293 9.293ZM18 20V18H20V20C20 21.102 19.104 22 18 22H6C4.896 22 4 21.102 4 20V18H6V20H18Z" aria-hidden="true"></path></svg>`
 					},
 					DROPPER: {
 						defaultProps: {
