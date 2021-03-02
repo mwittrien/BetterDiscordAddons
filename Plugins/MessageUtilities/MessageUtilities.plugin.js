1f28135f717f52970649bc1ae7a6cd81bdf81c8f
Mirco Wittrien <mirco.wittrien@gmail.com>
2021-03-02 17:03:55 +0100

    covers

--- Plugins/MessageUtilities/MessageUtilities.plugin.js
+++ Plugins/MessageUtilities/MessageUtilities.plugin.js
@@ -250,7 +250,7 @@ module.exports = (_ => {
 					if (group && group.type == BDFDB.LibraryComponents.MenuItems.MenuGroup && BDFDB.ArrayUtils.is(group.props.children)) for (let item of group.props.children) {
 						if (item && item.props && item.props.id && !item.props.hint && !item.props.children) {
 							let hint, action;
-							if (item.props.id == "mark-unread") hint = settings.addHints && `${BDFDB.LibraryModules.KeyCodeUtils.getString(18)}+${clickMap.CLICK}`;
+							if (item.props.id == "mark-unread") hint = settings.addHints && `${BDFDB.LibraryModules.KeyCodeUtils.getString(18)}+CLICK`;
 							else {
 								switch (item.props.id) {
 									case "copy-link":
