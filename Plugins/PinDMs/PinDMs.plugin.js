b0126c9f8d303d32d509c390647006e38ce4c3d9
Mirco Wittrien <mirco.wittrien@gmail.com>
2021-04-22 22:08:37 +0200

    Update PinDMs.plugin.js

--- Plugins/PinDMs/PinDMs.plugin.js
+++ Plugins/PinDMs/PinDMs.plugin.js
@@ -2,7 +2,7 @@
  * @name PinDMs
  * @author DevilBro
  * @authorId 278543574059057154
- * @version 1.8.0
+ * @version 1.8.1
  * @description Allows you to pin DMs, making them appear at the top of your DMs/ServerList
  * @invite Jx3TjNS
  * @donate https://www.paypal.me/MircoWittrien
@@ -17,7 +17,7 @@ module.exports = (_ => {
 		"info": {
 			"name": "PinDMs",
 			"author": "DevilBro",
-			"version": "1.8.0",
+			"version": "1.8.1",
 			"description": "Allows you to pin DMs, making them appear at the top of your DMs/ServerList"
 		},
 		"changeLog": {
