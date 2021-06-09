575e7cc43d7960b01178ba8f481984fc3b1a61e7
Mirco Wittrien <mirco.wittrien@gmail.com>
2021-06-09 21:05:02 +0200

    Update EditUsers.plugin.js

--- Plugins/EditUsers/EditUsers.plugin.js
+++ Plugins/EditUsers/EditUsers.plugin.js
@@ -2,7 +2,7 @@
  * @name EditUsers
  * @author DevilBro
  * @authorId 278543574059057154
- * @version 4.2.9
+ * @version 4.2.8
  * @description Allows you to locally edit Users
  * @invite Jx3TjNS
  * @donate https://www.paypal.me/MircoWittrien
@@ -17,7 +17,7 @@ module.exports = (_ => {
 		"info": {
 			"name": "EditUsers",
 			"author": "DevilBro",
-			"version": "4.2.9",
+			"version": "4.2.8",
 			"description": "Allows you to locally edit Users"
 		},
 		"changeLog": {
