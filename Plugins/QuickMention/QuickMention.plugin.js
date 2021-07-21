16a809abb37ab63ecb128ba7d6d977722c37559f
Mirco Wittrien <mirco.wittrien@gmail.com>
2021-07-21 06:51:17 +0100

    Update QuickMention.plugin.js

--- Plugins/QuickMention/QuickMention.plugin.js
+++ Plugins/QuickMention/QuickMention.plugin.js
@@ -2,7 +2,7 @@
  * @name QuickMention
  * @author DevilBro
  * @authorId 278543574059057154
- * @version 1.0.2
+ * @version 1.0.3
  * @description Adds a Mention Button to the Message Options Bar
  * @invite Jx3TjNS
  * @donate https://www.paypal.me/MircoWittrien
@@ -17,7 +17,7 @@ module.exports = (_ => {
 		"info": {
 			"name": "QuickMention",
 			"author": "DevilBro",
-			"version": "1.0.2",
+			"version": "1.0.3",
 			"description": "Adds a Mention Button to the Message Options Bar"
 		}
 	};
@@ -76,7 +76,7 @@ module.exports = (_ => {
 			onStop () {}
 		
 			onMessageOptionToolbar (e) {
-				if (!e.instance.props.expanded && e.instance.props.message.author.id != BDFDB.UserUtils.me.id && (BDFDB.UserUtils.can("SEND_MESSAGES") || e.instance.props.channel && (e.instance.props.channel.isDM() || e.instance.props.channel.isGroupDM()))) {
+				if (e.instance.props.message.author.id != BDFDB.UserUtils.me.id && (BDFDB.UserUtils.can("SEND_MESSAGES") || e.instance.props.channel && (e.instance.props.channel.isDM() || e.instance.props.channel.isGroupDM()))) {
 					e.returnvalue.props.children.splice(1, 0, BDFDB.ReactUtils.createElement(BDFDB.LibraryComponents.TooltipContainer, {
 						key: "mention",
 						text: BDFDB.LanguageUtils.LanguageStrings.MENTION,
