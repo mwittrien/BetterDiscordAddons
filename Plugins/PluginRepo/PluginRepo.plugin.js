ef2f3d2028fe21c4d35312ca77ed613ee40316f6
Mirco Wittrien <mirco.wittrien@gmail.com>
2021-08-14 18:28:17 +0200

    Update PluginRepo.plugin.js

--- Plugins/PluginRepo/PluginRepo.plugin.js
+++ Plugins/PluginRepo/PluginRepo.plugin.js
@@ -695,7 +695,7 @@ module.exports = (_ => {
 			
 			onStop () {
 				BDFDB.TimeUtils.clear(updateInterval);
-				BDFDB.TimeUtils.clear(loading.timeout);
+				BDFDB.TimeUtils.clear(loading && loading.timeout);
 
 				this.forceUpdateAll();
 
