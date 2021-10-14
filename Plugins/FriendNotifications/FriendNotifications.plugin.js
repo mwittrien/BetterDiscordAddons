fa92b96c4d5d9e097cac91eac311ef57f16493ab
Mirco Wittrien <mirco.wittrien@gmail.com>
2021-10-14 16:34:32 +0200

    Update FriendNotifications.plugin.js

--- Plugins/FriendNotifications/FriendNotifications.plugin.js
+++ Plugins/FriendNotifications/FriendNotifications.plugin.js
@@ -2,7 +2,7 @@
  * @name FriendNotifications
  * @author DevilBro
  * @authorId 278543574059057154
- * @version 1.8.0
+ * @version 1.8.1
  * @description Shows a Notification when a Friend or a User, you choose to observe, changes their Status
  * @invite Jx3TjNS
  * @donate https://www.paypal.me/MircoWittrien
@@ -17,7 +17,7 @@ module.exports = (_ => {
 		"info": {
 			"name": "FriendNotifications",
 			"author": "DevilBro",
-			"version": "1.8.0",
+			"version": "1.8.1",
 			"description": "Shows a Notification when a Friend or a User, you choose to observe, changes their Status"
 		},
 		"changeLog": {
