0f6d1a4ca17c4e1258893dc7d7a427f729d7e136
Mirco Wittrien <mirco.wittrien@gmail.com>
2022-04-03 16:34:43 +0200

    Update CustomQuoter.plugin.js

--- Plugins/CustomQuoter/CustomQuoter.plugin.js
+++ Plugins/CustomQuoter/CustomQuoter.plugin.js
@@ -2,7 +2,7 @@
  * @name CustomQuoter
  * @author DevilBro
  * @authorId 278543574059057154
- * @version 1.2.8a
+ * @version 1.2.8
  * @description Brings back the Quote Feature and allows you to set your own Quote Formats
  * @invite Jx3TjNS
  * @donate https://www.paypal.me/MircoWittrien
