f28a4b9917d2d83e523cfd3094f213b0e6bd8dd1
Mirco Wittrien <mirco.wittrien@gmail.com>
2022-07-30 16:31:27 +0200

    Update ImageUtilities.plugin.js

--- Plugins/ImageUtilities/ImageUtilities.plugin.js
+++ Plugins/ImageUtilities/ImageUtilities.plugin.js
@@ -2,7 +2,7 @@
  * @name ImageUtilities
  * @author DevilBro
  * @authorId 278543574059057154
- * @version 4.7.8
+ * @version 4.7.9
  * @description Adds several Utilities for Images/Videos (Gallery, Download, Reverse Search, Zoom, Copy, etc.)
  * @invite Jx3TjNS
  * @donate https://www.paypal.me/MircoWittrien
@@ -17,7 +17,7 @@ module.exports = (_ => {
 		"info": {
 			"name": "ImageUtilities",
 			"author": "DevilBro",
-			"version": "4.7.8",
+			"version": "4.7.9",
 			"description": "Adds several Utilities for Images/Videos (Gallery, Download, Reverse Search, Zoom, Copy, etc.)"
 		}
 	};
@@ -1574,7 +1574,8 @@ module.exports = (_ => {
 					height: videoData.size.height,
 					naturalWidth: viewedImage.width,
 					naturalHeight: viewedImage.height,
-					play: true
+					play: true,
+					playOnHover: !!BDFDB.LibraryModules.LocalSettingsStore.useReducedMotion
 				}));
 				BDFDB.ReactUtils.forceUpdate(modalInstance);
 			}
