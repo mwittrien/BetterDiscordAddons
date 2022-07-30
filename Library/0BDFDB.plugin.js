1f7de1b34b2b8fa6fe8f666f567b672685668985
Mirco Wittrien <mirco.wittrien@gmail.com>
2022-07-30 16:26:26 +0200

    themes

--- Library/0BDFDB.plugin.js
+++ Library/0BDFDB.plugin.js
@@ -2,7 +2,7 @@
  * @name BDFDB
  * @author DevilBro
  * @authorId 278543574059057154
- * @version 2.4.7
+ * @version 2.4.8
  * @description Required Library for DevilBro's Plugins
  * @invite Jx3TjNS
  * @donate https://www.paypal.me/MircoWittrien
@@ -19,7 +19,7 @@ module.exports = (_ => {
 		"info": {
 			"name": "BDFDB",
 			"author": "DevilBro",
-			"version": "2.4.7",
+			"version": "2.4.8",
 			"description": "Required Library for DevilBro's Plugins"
 		},
 		"rawUrl": "https://mwittrien.github.io/BetterDiscordAddons/Library/0BDFDB.plugin.js"
@@ -8024,6 +8024,7 @@ module.exports = (_ => {
 					muted: true,
 					loop: true,
 					autoPlay: props.play,
+					playOnHover: props.playOnHover,
 					preload: "none"
 				}) : BDFDB.ReactUtils.createElement("img", {
 					alt: "",
