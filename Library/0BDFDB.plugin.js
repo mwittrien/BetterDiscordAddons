1464036d360c9f808d5cbe5b1116edb09ddcd8da
Mirco Wittrien <mirco.wittrien@gmail.com>
2023-03-02 15:44:04 +0100

    Update 0BDFDB.plugin.js

--- Library/0BDFDB.plugin.js
+++ Library/0BDFDB.plugin.js
@@ -2,7 +2,7 @@
  * @name BDFDB
  * @author DevilBro
  * @authorId 278543574059057154
- * @version 3.1.1
+ * @version 3.1.2
  * @description Required Library for DevilBro's Plugins
  * @invite Jx3TjNS
  * @donate https://www.paypal.me/MircoWittrien
