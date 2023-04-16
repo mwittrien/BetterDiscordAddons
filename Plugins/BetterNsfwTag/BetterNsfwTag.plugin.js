977d93b4468a6a7874e9bed2f63e2827dd615663
Luna Komorebi <lunax@mlunax.com>
2023-04-16 22:08:38 +0200

    Update BetterNsfwTag plugin to version 1.4.2 with a more noticeable NSFW tag for channels

--- Plugins/BetterNsfwTag/BetterNsfwTag.plugin.js
+++ Plugins/BetterNsfwTag/BetterNsfwTag.plugin.js
@@ -2,7 +2,7 @@
  * @name BetterNsfwTag
  * @author DevilBro
  * @authorId 278543574059057154
- * @version 1.3.2
+ * @version 1.4.2
  * @description Adds a more noticeable Tag to NSFW Channels
  * @invite Jx3TjNS
  * @donate https://www.paypal.me/MircoWittrien
