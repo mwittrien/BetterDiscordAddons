8e1ac4c0562ffc3a92e08940e95e25c0d788c8eb
Mirco Wittrien <mirco.wittrien@gmail.com>
2024-04-02 19:16:52 +0200

    Update ServerFolders.plugin.js

--- Plugins/ServerFolders/ServerFolders.plugin.js
+++ Plugins/ServerFolders/ServerFolders.plugin.js
@@ -2,7 +2,7 @@
  * @name ServerFolders
  * @author DevilBro
  * @authorId 278543574059057154
- * @version 7.2.1
+ * @version 7.2.2
  * @description Changes Discord's Folders, Servers open in a new Container, also adds extra Features to more easily organize, customize and manage your Folders
  * @invite Jx3TjNS
  * @donate https://www.paypal.me/MircoWittrien
