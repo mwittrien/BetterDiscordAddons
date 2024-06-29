230caef000f084f5c68885833a09ac12330ec7a5
Mirco Wittrien <mirco.wittrien@gmail.com>
2024-06-29 13:35:02 +0200

    Update CharCounter.plugin.js

--- Plugins/CharCounter/CharCounter.plugin.js
+++ Plugins/CharCounter/CharCounter.plugin.js
@@ -2,7 +2,7 @@
  * @name CharCounter
  * @author DevilBro
  * @authorId 278543574059057154
- * @version 1.6.4
+ * @version 1.6.5
  * @description Adds a Character Counter to most Inputs
  * @invite Jx3TjNS
  * @donate https://www.paypal.me/MircoWittrien
@@ -94,6 +94,7 @@ module.exports = (_ => {
 				this.css = `
 					${BDFDB.dotCN._charcountercounteradded} {
 						position: relative !important;
+						width: 100%;
 					}
 					${BDFDB.dotCN._charcountercounter} {
 						display: block;
