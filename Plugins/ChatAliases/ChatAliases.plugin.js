e9261537a29a6e2fa3706fb4913913b4eb30560b
Mirco Wittrien <mirco.wittrien@gmail.com>
2024-07-07 22:12:50 +0200

    Update ChatAliases.plugin.js

--- Plugins/ChatAliases/ChatAliases.plugin.js
+++ Plugins/ChatAliases/ChatAliases.plugin.js
@@ -14,8 +14,8 @@
 
 module.exports = (_ => {
 	const changeLog = {
-		"improved": {
-			"Alias Order": "Automatically sets a higher priority for longer aliases, to avoid short aliases from overwritting longer ones"
+		"fixed": {
+			"Alias Length": "No longer cuts of Aliases after 1200 Characters"
 		}
 	};
 
