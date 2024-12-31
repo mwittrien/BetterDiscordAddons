01b17da8da7f134ab75610d1a2515d9c350d2b68
CuantumQ <96323084+CuantumQ@users.noreply.github.com>
2024-12-30 18:49:26 -0600

    Adding BlankLine Support for SplitByLine for SplitLargeMessages
    Added an option to SplitByLine's functionality so that if a user formats their message like this:
    A
    
    B
    (internally "A\n\nB")
    
    and the ByLine mode splits on that empty line, the resulting two messages will be formatted as this:
    A
     
    B
    (internally "A\n** **", "B")
    
    instead of
    A
    B
    (internally "A", "B")
    
    This change makes it so that the user won't experience an unexpected change in formatting that the ByLine option seems to be trying to protect.

--- Plugins/SplitLargeMessages/SplitLargeMessages.plugin.js
+++ Plugins/SplitLargeMessages/SplitLargeMessages.plugin.js
@@ -3,11 +3,13 @@
  * @author DevilBro
  * @authorId 278543574059057154
  * @version 1.8.5
- * @description Allows you to enter larger Messages, which will automatically split into several smaller Messages.
+ * @description Allows you to enter larger Messages, which will automatically split into several smaller Messages
  * @invite Jx3TjNS
  * @donate https://www.paypal.me/MircoWittrien
  * @patreon https://www.patreon.com/MircoWittrien
  * @website https://mwittrien.github.io/
+ * @source https://github.com/mwittrien/BetterDiscordAddons/tree/master/Plugins/SplitLargeMessages/
+ * @updateUrl https://mwittrien.github.io/BetterDiscordAddons/Plugins/SplitLargeMessages/SplitLargeMessages.plugin.js
  */
 
 module.exports = (_ => {
