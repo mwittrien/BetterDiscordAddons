f0e7fb4246aa19eadc2804ea8e69177b3f0fd248
Mirco Wittrien <mirco.wittrien@gmail.com>
2025-04-12 12:06:36 +0200

    Update PersonalPins.plugin.js

--- Plugins/PersonalPins/PersonalPins.plugin.js
+++ Plugins/PersonalPins/PersonalPins.plugin.js
@@ -553,6 +553,7 @@ module.exports = (_ => {
 									className: BDFDB.disCNS.messagetoolbaricon + BDFDB.disCN.messagetoolbarbuttoncontent,
 									children: BDFDB.ReactUtils.createElement(BDFDB.LibraryComponents.SvgIcon, {
 										className: BDFDB.disCN.messagetoolbaricon,
+										nativeClass: true,
 										iconSVG: note ? pinIconDelete : pinIcon
 									})
 								})
