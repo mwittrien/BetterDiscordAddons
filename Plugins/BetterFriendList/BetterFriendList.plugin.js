0e520d6d49398f0556e9b49fba02645a4941db04
Mirco Wittrien <mirco.wittrien@gmail.com>
2025-10-22 22:23:57 +0200

    Update BetterFriendList.plugin.js

--- Plugins/BetterFriendList/BetterFriendList.plugin.js
+++ Plugins/BetterFriendList/BetterFriendList.plugin.js
@@ -14,9 +14,7 @@
 
 module.exports = (_ => {
 	const changeLog = {
-		"added": {
-			"Relationship Age": "Added Info when the relationship was added, sortable"
-		}
+		
 	};
 
 	return !window.BDFDB_Global || (!window.BDFDB_Global.loaded && !window.BDFDB_Global.started) ? class {
