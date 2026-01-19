dade850216f0539ea0728655fba5168a65a71196
Mirco Wittrien <mirco.wittrien@gmail.com>
2026-01-19 18:35:39 +0100

    stuff

--- Plugins/BetterSearchPage/BetterSearchPage.plugin.js
+++ Plugins/BetterSearchPage/BetterSearchPage.plugin.js
@@ -2,7 +2,7 @@
  * @name BetterSearchPage
  * @author DevilBro
  * @authorId 278543574059057154
- * @version 1.2.8
+ * @version 1.2.9
  * @description Makes the Controls in the Search Results Page sticky
  * @invite Jx3TjNS
  * @donate https://www.paypal.me/MircoWittrien
@@ -79,7 +79,7 @@ module.exports = (_ => {
 					${BDFDB.dotCNS.searchresultspagination + BDFDB.dotCN.paginationcontainer} {
 						margin-top: 0;
 					}
-					${BDFDB.dotCN.searchresultswrap} [role="list"] ~ ${BDFDB.dotCN.searchresultspagination} {
+					${BDFDB.dotCNS.searchresultsscroller + BDFDB.dotCN.searchresultspagination} {
 						display: none !important;
 					}
 				`;
@@ -99,7 +99,7 @@ module.exports = (_ => {
 				if (onPageChange) children.splice(index + 1, 0, BDFDB.ReactUtils.createElement(BDFDB.LibraryComponents.SearchResultsPagination, {
 					onPageChange: newPage => !e.instance.props.search.searching && onPageChange(newPage),
 					offset: e.instance.props.search.offset,
-					totalCount: e.instance.props.search.totalResults,
+					totalCount: e.instance.props.search.totalResults > 9976 ? 9976 : e.instance.props.search.totalResults,
 					pageSize: BDFDB.DiscordConstants.SEARCH_PAGE_SIZE
 				}));
 			}
