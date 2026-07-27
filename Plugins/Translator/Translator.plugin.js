8a70c9ad761e24c3a101f64dfd53734907d1de80
Mirco Wittrien <mirco.wittrien@gmail.com>
2026-07-27 13:02:24 +0200

    Update Translator.plugin.js

--- Plugins/Translator/Translator.plugin.js
+++ Plugins/Translator/Translator.plugin.js
@@ -2,7 +2,7 @@
  * @name Translator
  * @author DevilBro
  * @authorId 278543574059057154
- * @version 2.8.2
+ * @version 2.8.3
  * @description Allows you to translate incoming and your outgoing Messages within Discord
  * @invite Jx3TjNS
  * @donate https://www.paypal.me/MircoWittrien
